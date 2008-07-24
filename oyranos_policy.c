c407a722b663bed68e609869eae833d1c1c19c9c
Kai-Uwe Behrmann <ku.b@gmx.de>
2008-07-24 12:36:42 +0200

    * extended options set and documentation

--- oyranos_policy.c
+++ oyranos_policy.c
@@ -33,12 +33,50 @@
 
 void* oyAllocFunc(size_t size) {return malloc (size);}
 
+void  printfHelp (int argc, char** argv)
+{
+  char * version = oyVersionString(1,0);
+  char * id = oyVersionString(2,0);
+  char * cfg_date =  oyVersionString(3,0);
+  char * devel_time = oyVersionString(4,0);
+
+  fprintf( stderr, "\n");
+  fprintf( stderr, "oyranos-policy %s\n",
+                                _("is a policy administration tool"));
+  fprintf( stderr, "  Oyranos v%s config: %s devel period: %s\n",
+                  oyNoEmptyName_m_(version),
+                  oyNoEmptyName_m_(cfg_date), oyNoEmptyName_m_(devel_time) );
+  if(id)
+  fprintf( stderr, "  Oyranos git id %s\n", oyNoEmptyName_m_(id) );
+  fprintf( stderr, "\n");
+  fprintf( stderr, "%s\n",                 _("Usage"));
+  fprintf( stderr, "  %s\n",               _("Dump out the actual settings:"));
+  fprintf( stderr, "      %s\n",           argv[0]);
+  fprintf( stderr, "  %s\n",               _("Set new policy:"));
+  fprintf( stderr, "      %s %s\n",           argv[0], _("policy filename"));
+  fprintf( stderr, "  %s\n",               _("List available policies:"));
+  fprintf( stderr, "      %s -l\n",        argv[0]);
+  fprintf( stderr, "  %s\n",               _("Currently active policy:"));
+  fprintf( stderr, "      %s -c\n",        argv[0]);
+  fprintf( stderr, "  %s\n",               _("Save and install to a new policy:"));
+  fprintf( stderr, "      %s -s %s\n",        argv[0], _("policy name"));
+  fprintf( stderr, "\n");
+  fprintf( stderr, "\n");
+
+  if(version) oyDeAllocateFunc_(version);
+  if(id) oyDeAllocateFunc_(id);
+  if(cfg_date) oyDeAllocateFunc_(cfg_date);
+  if(devel_time) oyDeAllocateFunc_(devel_time);
+}
+
 int main( int argc , char** argv )
 {
   int error = 0;
-  const char* fileName = NULL;
+  const char* fileName = NULL,
+            * new_policy = NULL;
   size_t size = 0;
   char *xml = NULL;
+  int current_policy = 0, list_policies = 0;
 
   if(getenv("OYRANOS_DEBUG"))
   {
@@ -64,18 +102,19 @@ int main( int argc , char** argv )
         case '-':
             switch (argv[pos][1])
             {
+              case 's': if( pos + 1 < argc )
+                        { new_policy = argv[pos+1];
+                          if( !oyStrlen_(new_policy) )
+                            wrong_arg = "-s";
+                        } else wrong_arg = "-s";
+                        if(oy_debug) fprintf(stderr,"new_policy=0\n"); ++pos;
+                        break;
+              case 'c': current_policy = 1; break;
+              case 'l': list_policies = 1; break;
+              case 'v': oy_debug += 1; break;
               case 'h':
               default:
-                        fprintf( stderr, "\n");
-                        fprintf( stderr, "oyranos-policy v%d.%d.%d %s\n",
-                        OYRANOS_VERSION_A,OYRANOS_VERSION_B,OYRANOS_VERSION_C,
-                                _("is a policy administration tool"));
-                        fprintf( stderr, "%s\n",                 _("Usage"));
-                        fprintf( stderr, "  %s\n",               _("Set new policy:"));
-                        fprintf( stderr, "      %s\n",           argv[0]);
-                        fprintf( stderr, "            %s\n",     _("policy filename"));
-                        fprintf( stderr, "\n");
-                        fprintf( stderr, "\n");
+                        printfHelp(argc, argv);
                         exit (0);
                         break;
             }
@@ -111,6 +150,32 @@ int main( int argc , char** argv )
     return 1;
   }
 
+  if(new_policy)
+  {
+    error = oyPolicySaveActual( oyGROUP_ALL, new_policy );
+    if(!error)
+      fprintf( stdout, "%s \"%s\"\n",
+               _("installed new policy"), new_policy);
+    else
+      fprintf( stdout, "\"%s\" %s %d\n", new_policy,
+               _("installation of new policy file failed with error:"), error);
+
+  } else
+  if(current_policy || list_policies)
+  {
+    const char ** names = NULL;
+    int count = 0, i, current;
+    oyOptionChoicesGet( oyWIDGET_POLICY, &count, &names, &current );
+
+    if(list_policies)
+      for(i = 0; i < count; ++i)
+        fprintf(stdout, "%s\n", names[i]);
+
+    if(current_policy)
+      fprintf( stdout, "%s \"%s\"\n",
+               _("Currently active policy"), names[current]);
+
+  } else
   if(!fileName)
   {
     size = 0;
