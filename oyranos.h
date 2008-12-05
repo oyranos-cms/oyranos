98d893ae6ac105924c318bcc87cc3a9a3aa9551a
Kai-Uwe Behrmann <ku.b@gmx.de>
2008-12-05 23:06:18 +0100

    * [docu]: formatting

--- oyranos.h
+++ oyranos.h
@@ -4,13 +4,10 @@
  *
  *  Copyright (C) 2004-2008  Kai-Uwe Behrmann
  *
- */
-
-/**
  *  @brief    public Oyranos API's
- *  @internal
  *  @author   Kai-Uwe Behrmann <ku.b@gmx.de>
- *  @par License:\n new BSD <http://www.opensource.org/licenses/bsd-license.php>
+ *  @par License:\n
+ *  new BSD <http://www.opensource.org/licenses/bsd-license.php>
  *  @since    2004/11/25
  */
 
