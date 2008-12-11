9fe0c6020306d1160224df13f149755415eaff57
Kai-Uwe Behrmann <ku.b@gmx.de>
2008-12-11 18:28:09 +0100

    * [conf]: switch to v0.1.10

--- configure.sh
+++ configure.sh
@@ -756,7 +756,7 @@ if [ -n "$DEBUG" ] && [ $DEBUG -gt 0 ]; then
         test -f "$i/makefile".in && echo "DEBUG = $DEBUG_"  >> "$i/makefile"
         test -f "$i/makefile".in && echo "DEBUG_SWITCH = -v"  >> "$i/makefile"
       fi
-      if [ "$static" -eq "0" ]; then
+      if [ "$static" = "0" ]; then
         test -f "$i/makefile".in && echo "STATIC = \$(RM)"  >> "$i/makefile"
       fi
       if [ "$verbose" -eq "0" ]; then
