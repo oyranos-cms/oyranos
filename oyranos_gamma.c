87bb8bdf2bb8dd2f95a06011d09ddd19ccd2d5f6
Kai-Uwe Behrmann <ku.b@gmx.de>
2008-12-13 21:06:30 +0100

    * [and]: clearer expression same logic

--- oyranos_gamma.c
+++ oyranos_gamma.c
@@ -172,9 +172,9 @@ int main( int argc , char** argv )
     }
 
     /* make shure the display name is correct including the screen */
-    if(monitor_profile || erase)
-      oySetMonitorProfile (oy_display_name, monitor_profile);
     if(monitor_profile)
+      oySetMonitorProfile (oy_display_name, monitor_profile);
+    if(monitor_profile || erase)
       oySetMonitorProfile (oy_display_name, 0);
   }
 
