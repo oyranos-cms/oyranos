oyranos-cms/oyranos

commit 335775a7e9bb31c2d41cd2e52cee15bfeaee9cc1

* [oyX1]: improve command line parsing in oyranos-monitor
On letter options can be grouped, e.g. -lv

oyranos_gamma.c
@@ -80,14 +80,15 @@ int main( int argc , char** argv )
 
   if(argc != 1)
   {
-    int pos = 1;
+    int pos = 1, i;
     char *wrong_arg = 0;
     while(pos < argc)
     {
       switch(argv[pos][0])
       {
         case '-':
-            switch (argv[pos][1])
+            for(i = 1; i < strlen(argv[pos]); ++i)
+            switch (argv[pos][i])
             {
               case 'e': erase = 1; monitor_profile = 0; break;
               case 'b': database = 1; monitor_profile = 0; break;

commit 5e711f8eaa26e27d7e228c31854e07016f604ca5

* [oyX1]: fix cli parsing for -x/y args
This bug was introduced with 335775a7 and a2777e390.

oyranos_gamma.c
@@ -86,6 +86,7 @@ int main( int argc , char** argv )
                           if( opt == 0 && strcmp(argv[pos+1],"0") ) \
                             wrong_arg = "-" #opt; \
                           ++pos; \
+                          i = 1000; \
                         } else if(argv[pos][i+1] == '=') \
                         { opt = atoi( &argv[pos][i+2] ); \
                           if( opt == 0 && strcmp(&argv[pos][i+2],"0") ) \