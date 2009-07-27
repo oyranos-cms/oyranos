94f6e3b48f6dfd09a8ac838090984698aeec979a
Yiannis Belias <jonnyb@hol.gr>
2009-07-27 10:35:25 +0300

    [oytest] Select tests from cmdline

--- oyTest.cc
+++ oyTest.cc
@@ -6,61 +6,38 @@
 using namespace oyranos;
 
 int test_backend( const char * name );
+void help(char* progname);
 
-int main()
+int main(int argc, char** argv)
 {
-	test_backend( "monitor" );
-	test_backend( "scanner" );
+	if (argc < 2) {
+		help(argv[0]);
+		return 0;
+	}
+
+	for (int c=1; c<argc; ++c) {
+		int num = atoi(argv[c])-1;
+		if (num>3)
+			help(argv[0]);
+		switch (num) {
+			case 0:
+				test_backend( "monitor" );
+				break;
+			case 1:
+				test_backend( "scanner" );
+				break;
+			case 2:
+				test_backend( "raw-image" );
+				break;
+			case 3:
+				test_backend( "printer" );
+				break;
+		}
+	}
 
 	return 0;
 }
-#if 0
-	 /**Select SANE**/
-	 options = oyOptions_New( 0 );
-    error = oyDeviceGet( OY_TYPE_STD, "scanner", "pnm:0", options, 0 );
-	 /*Select SANE device TODO*/
-	 /*Command SANE backend to say hi*/
-	 oyOptions_SetFromText(
-			 &options,
-			 "//" OY_TYPE_STD "/config.scanner.SANE/command",
-			 "say_hello",
-			 OY_CREATE_NEW );
-    error = oyDeviceGet( OY_TYPE_STD, "scanner", "dev0", options, 0 );
-/*	 oyOptions_SetFromText(
-			 &options,
-			 "//" OY_TYPE_STD "/config.scanner.SANE/command",
-			 "list",
-			 OY_CREATE_NEW );
-    error = oyDeviceGet( OY_TYPE_STD, "scanner", "v4l:/dev/video0", options, 0 );
-*/
-#endif
-#if 0
-    oyConfig_s * device = 0; /* initialise with zero to avoid errors */
-    oyProfile_s * profile = 0;
-    oyOptions_s * options = 0;  /* not used in this example */
-    char * text = 0;
-    /* Call into the device backends oyCMMapi8_s::oyConfigs_FromPattern
-     * function with the "list" command.
-     * "monitor" is to be exchanged by the actual device class and
-     * ":0.0" by the actual device nick name.
-     */
 
-    int error = oyDeviceGet( 0, "monitor", ":0.0", options, &device );
-    /* obtain a expensive list of key/value pairs from the device backend */
-    error = oyDeviceGetInfo( device, oyNAME_DESCRIPTION, 0, &text,
-                                     malloc );
-
-	 printf("%s\n", text);
-    /* assign a profile to the device/driver */
-    error = oyDeviceSetProfile( device, "some_real_profile_in_your_path.icc" );
-
-    /* Ask Oyranos to lookup its DB for a matching profile. It is only useful
-     * if a profile is already set for the devices keys. The set devices can be
-     * viewed at the command line with:
-     * ls -d ~/.kdb/user/shared/freedesktop.org/imaging/config.*
-     */
-    error = oyDeviceAskProfile( device, &profile );
-#endif
 
 //All backends should understand the following calls
 int test_backend( const char * name )
@@ -168,3 +145,13 @@ int test_backend( const char * name )
 	 oyConfigs_Release( &sane_devices );
 }
 
+void help(char* progname)
+{
+	const char* usage =
+		"Usage: %s [[int] [int]...]\n"
+		"1:\tmonitor\n"
+		"2:\tscanner\n"
+		"3:\traw-image\n"
+		"4:\tprinter\n";
+	printf(usage,progname);
+}
