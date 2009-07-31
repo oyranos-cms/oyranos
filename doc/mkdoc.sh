39af50185756a8afb1b84b901f966f62359a5225
Yiannis Belias <jonnyb@hol.gr>
2009-07-31 20:44:30 +0300

    [online help] Handle elinks not present case.
    * Set a warning message.
    * Create an empty valid *_help.c file

--- doc/mkdoc.sh
+++ doc/mkdoc.sh
@@ -7,4 +7,4 @@ C_FILE=$(echo ${BASE}.c)
 cat $HTML |
 sed 's/FRAME=VOID.*BORDER=0/CELLSPACING=2 COLS=8 BORDER=2/g' |
 elinks -dump |
-awk '{print "\" "$0"\\n\""} BEGIN { print "static const char help_message[] =" } END { print ";" }' > $C_FILE
+awk '{print "\" "$0"\\n\""} BEGIN { print "static const char help_message[] = \"\"" } END { print ";" }' > $C_FILE
