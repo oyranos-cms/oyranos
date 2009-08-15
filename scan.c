0d60be249422deb5d2d051dc327592fa10ea37ac
Yiannis Belias <jonnyb@hol.gr>
2009-08-15 20:13:38 +0300

    [scan] Fix missing OY_SLASH

--- scan.c
+++ scan.c
@@ -80,9 +80,9 @@ void init()
       printf("Found %d SANE device%s\n", num_devices, num_devices > 1 ? "s" : "");
 
    /*Get the SANE version from the first found device*/
-   device = oyConfigs_Get(devices, 1);
+   device = oyConfigs_Get(devices, 0);
    assert(device != NULL);
-   error = oyOptions_FindInt(device->data, CMM_BASE_REG "driver_version", 0, &version);
+   error = oyOptions_FindInt(device->data, CMM_BASE_REG OY_SLASH "driver_version", 0, &version);
    assert(error == 0);
    oyConfig_Release(&device);
    print_sane_version();
