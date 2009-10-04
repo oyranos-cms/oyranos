ed97708b2b77e806c02db3f661a135aa8639efed
Kai-Uwe Behrmann <ku.b@gmx.de>
2009-10-04 10:10:56 +0200

    * [oforms]: move handlers to Oyranos core ...
    add help text and better options parser.
    Adapt oyXFORMsFromModelAndUi to Oyranos API change.
    Write XML file on request.

--- oyranos_xforms.c
+++ oyranos_xforms.c
@@ -11,30 +11,35 @@
 
 
 
+extern oyUiHandler_s * oy_ui_cmd_line_handlers[];
 
-int        oyXML2XFORMsSelect1Handler( xmlNodePtr          cur,
-                                       oyOptions_s       * collected_elements,
-                                       oyPointer           user_data );
 
-oyUiHandler_s oy_ui_handler_xf_select1_ =
-  {oyOBJECT_UI_HANDLER_S,0,0,0,        /**< oyStruct_s members */
-   "oyFORMS",                          /**< dialect */
-   "libxml2",                          /**< parser_type */
-   "xf:select1",                       /**< element_type; Wanted XML element. */
-   (oyUiHandler_f)oyXML2XFORMsSelect1Handler, /**< oyUiHandler_f handler*/
-   "dummy",                            /**< handler_type */
-   "xf:choices/xf:item/xf:label.xf:value" /**< element_search */
-  };
-
-oyUiHandler_s * oy_ui_handlers[] = {
-  &oy_ui_handler_xf_select1_,
-  0
-};
+void usage(int argc, char ** argv)
+{
+                        printf("\n");
+                        printf("oyranos-xforms v%d.%d.%d %s\n",
+                        OYRANOS_VERSION_A,OYRANOS_VERSION_B,OYRANOS_VERSION_C,
+                                _("is a Oyranos module options tool"));
+                        printf("%s\n",                 _("Usage"));
+                        printf("  %s\n",               _("Show options"));
+                        printf("      %s -n \"module_name\"\n", argv[0]);
+                        printf("\n");
+                        printf("  %s\n",               _("Get XFORMS:"));
+                        printf("      %s -n \"module_name\" -x \"xhtml_file\"\n", argv[0]);
+                        printf("\n");
+                        printf("  %s\n",               _("General options:"));
+                        printf("      %s\n",           _("-v verbose"));
+                        printf("\n");
+                        printf(_("For more informations read the man page:"));
+                        printf("\n");
+                        printf("      man oyranos-xforms\n");
+}
 
 int main (int argc, char ** argv)
 {
-  const char * node_name = argc > 1 ? argv[1] : "//imaging/icc.lcms";
-  oyFilterNode_s * node = oyFilterNode_NewWith( node_name, 0,0 );
+  const char * node_name = 0;
+  const char * xml_file = 0;
+  oyFilterNode_s * node = 0;
   char * ui_text = 0,
       ** namespaces = 0,
        * text = 0;
@@ -43,6 +48,75 @@ int main (int argc, char ** argv)
       i;
   oyOptions_s * opts = 0;
 
+#ifdef USE_GETTEXT
+  setlocale(LC_ALL,"");
+#endif
+  oyI18NInit_();
+
+
+/* allow "-opt val" and "-opt=val" syntax */
+#define OY_PARSE_STRING_ARG( opt ) \
+                        if( pos + 1 < argc && argv[pos][i+1] == 0 ) \
+                        { opt = argv[pos+1]; \
+                          if( opt == 0 && strcmp(argv[pos+1],"0") ) \
+                            wrong_arg = "-" #opt; \
+                          ++pos; \
+                          i = 1000; \
+                        } else if(argv[pos][i+1] == '=') \
+                        { opt = &argv[pos][i+2]; \
+                          if( opt == 0 && strcmp(&argv[pos][i+2],"0") ) \
+                            wrong_arg = "-" #opt; \
+                          i = 1000; \
+                        } else wrong_arg = "-" #opt; \
+                        if(oy_debug) printf(#opt "=%s\n",opt)
+
+  if(argc != 1)
+  {
+    int pos = 1, i;
+    char *wrong_arg = 0;
+    while(pos < argc)
+    {
+      switch(argv[pos][0])
+      {
+        case '-':
+            for(i = 1; i < strlen(argv[pos]); ++i)
+            switch (argv[pos][i])
+            {
+              case 'n': OY_PARSE_STRING_ARG( node_name ); break;
+              case 'x': OY_PARSE_STRING_ARG( xml_file ); break;
+              case 'v': oy_debug += 1; break;
+              case 'h':
+              case '-':
+                        if(strcmp(&argv[pos][2],"verbose") == 0)
+                        { oy_debug += 1; i=100; break;
+                        }
+              default:
+                        usage(argc, argv);
+                        exit (0);
+                        break;
+            }
+            break;
+        default:
+            wrong_arg = argv[pos];
+      }
+      if( wrong_arg )
+      {
+        printf("%s %s\n", _("wrong argument to option:"), wrong_arg);
+        exit(1);
+      }
+      ++pos;
+    }
+    if(oy_debug) printf( "%s\n", argv[1] );
+
+  }
+
+  if(!node_name)
+  {
+                        usage(argc, argv);
+                        exit (0);
+  }
+
+  node = oyFilterNode_NewWith( node_name, 0,0 );
   oyOptions_Release( &node->core->options_ );
   /* First call for options ... */
   opts = oyFilterNode_OptionsGet( node, 
@@ -54,14 +128,17 @@ int main (int argc, char ** argv)
   oyFilterNode_Release( &node );
 
   data = oyOptions_GetText( opts, oyNAME_NAME );
-  text = oyXFORMsFromModelAndUi( data, ui_text, (const char**)namespaces,
+  text = oyXFORMsFromModelAndUi( data, ui_text, (const char**)namespaces, 0,
                                  malloc );
 
   if(namespaces)
   {
     i = 0;
     while(namespaces[i])
+    {
+      printf("namespaces[%d]: %s\n", i, namespaces[i]);
       free( namespaces[i++] );
+    }
     free(namespaces);
   }
   if(ui_text) free(ui_text); ui_text = 0;
@@ -70,7 +147,10 @@ int main (int argc, char ** argv)
   if(oy_debug)
     printf("%s\n", text);
 
-  error = oyXFORMsRenderUi( text, oy_ui_handlers, 0 );
+  error = oyXFORMsRenderUi( text, oy_ui_cmd_line_handlers, 0 );
+
+  if(xml_file)
+    oyWriteMemToFile_( xml_file, text, strlen(text) );
 
   /* xmlParseMemory sollte der Ebenen gewahr werden wie oyOptions_FromText. */
   opts = oyOptions_FromText( data, 0,0 );
