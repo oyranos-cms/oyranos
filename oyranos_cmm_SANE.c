94eba6652178f33d9de98e6c40a14fc1e19e42fb
Yiannis Belias <jonnyb@hol.gr>
2010-03-01 18:02:30 +0200

    [SANE] Replace gamma tables with a gamma value
    This will be obtained with an lcms function that gets a
    gamma table and returns a floating point gamma value.

--- oyranos_cmm_SANE.c
+++ oyranos_cmm_SANE.c
@@ -15,6 +15,7 @@
 
 #include <oyranos/oyranos_cmm.h>
 #include <sane/sane.h>
+#include <lcms.h>
 
 #include <string.h>
 #include <stdarg.h>
@@ -1021,7 +1022,7 @@ int ColorInfoFromHandle(const SANE_Handle device_handle, oyOptions_s **options)
 
    for (opt_num = 1; opt_num < num_options; opt_num++) {
       opt = sane_get_option_descriptor(device_handle, opt_num);
-      if (opt->cap & SANE_CAP_COLOUR) {
+      if ((opt->cap & SANE_CAP_COLOUR) /*&& !(opt->cap & SANE_CAP_INACTIVE)*/) {
          void *value = malloc(opt->size);
          char *registration = malloc(sizeof(cmm_base_reg)+strlen(opt->name));
 
@@ -1038,8 +1039,18 @@ int ColorInfoFromHandle(const SANE_Handle device_handle, oyOptions_s **options)
                else {
                   int count = opt->size/sizeof(SANE_Word);
                   oyOption_s *option = oyOption_New(registration, 0);
-                  for (i=count-1; i>=0; --i)
-                     oyOption_SetFromInt(option, *(SANE_Int *) value+i, i, 0);
+                  /* If the option contains a gamma table, calculate the gamma value
+                   * as a float and save this instead */
+                //if (strstr(opt->name, "gamma-table")) {
+                //   LPGAMMATABLE lt = cmsAllocGamma(count);
+                //   for (i=0; i<count; ++i)
+                //      lt->GammaTable[i] = (WORD)(*(SANE_Int *) value+i);
+                //   oyOption_SetFromDouble(option, cmsEstimateGamma(lt), 0, 0);
+                //   cmsFreeGamma(lt);
+                //} else {
+                     for (i=count-1; i>=0; --i)
+                        oyOption_SetFromInt(option, *(SANE_Int *) value+i, i, 0);
+                //}
                   oyOptions_MoveIn(*options, &option, -1);
                }
                break;
