oyranos-cms/oyranos

commit 62fe5fe7d7af6bca6e79e4ad2c240eb014b9d452

* [alpha]: change oyDeviceGetProfile, removeoyDeviceAskProfile
... to support the "net_color_region_target" option.

"net_color_region_target" replaces "colour_server" from commit ab13ed0a.

modules/devices/oyranos_cmm_oyX1.c
@@ -630,13 +630,13 @@ int            oyX1Configs_Modify    ( oyConfigs_s       * devices,
           uint32_t flags = 0;
           char * data = 0;
 
-          if(oyOptions_FindString( options, "colour_server", 0 ))
+          if(oyOptions_FindString( options, "net_color_region_target", 0 ))
           {
             if(oy_debug)
               message( oyMSG_WARN, (oyStruct_s*)options, OY_DBG_FORMAT_ "\n  "
                      "Try %s(_xxx) from %s",
                      OY_DBG_ARGS_,
-                     oyOptions_FindString(options, "colour_server", 0) ? 
+                     oyOptions_FindString(options, "net_color_region_target", 0) ? 
                      OY_ICC_COLOUR_SERVER_TARGET_PROFILE_IN_X_BASE :
                      OY_ICC_V0_3_TARGET_PROFILE_IN_X_BASE,
                      device_name );
@@ -647,17 +647,22 @@ int            oyX1Configs_Modify    ( oyConfigs_s       * devices,
 
 
           has = 0;
-          o = oyConfig_Find( device, "icc_profile" );
+          /*  */
+          /*o = oyConfig_Find( device, "icc_profile" );
           if(o)
-            has = 1;
+            has = 1;*/
 
           if(data && size)
           {
             prof = oyProfile_FromMem( size, data, 0, 0 );
             free( data );
             if(has == 0)
-              o = oyOption_New( OYX1_MONITOR_REGISTRATION OY_SLASH
-                                "icc_profile", 0 );
+            {
+              const char * key = OYX1_MONITOR_REGISTRATION OY_SLASH "icc_profile";
+              if(oyOptions_FindString(options, "net_color_region_target", 0))
+                key = OYX1_MONITOR_REGISTRATION OY_SLASH "icc_profile.net_color_region_target";
+              o = oyOption_New( key, 0 );
+            }
           } else if(oyOptions_FindString( options, "icc_profile.fallback", 0 ))
           {
             icHeader * header = 0;
@@ -756,7 +761,7 @@ int            oyX1Configs_Modify    ( oyConfigs_s       * devices,
             message( oyMSG_WARN, (oyStruct_s*)options, OY_DBG_FORMAT_ "\n  "
                      "Could not obtain %s(_xxx) information for %s",
                      OY_DBG_ARGS_,
-                     oyOptions_FindString(options, "colour_server", 0) ? 
+                     oyOptions_FindString(options, "net_color_region_target", 0) ? 
                      OY_ICC_COLOUR_SERVER_TARGET_PROFILE_IN_X_BASE :
                      OY_ICC_V0_3_TARGET_PROFILE_IN_X_BASE,
                      device_name );

examples/colour_desktop/colour_desktop.c
@@ -684,7 +684,14 @@ static void    getDeviceProfile      ( CompScreen        * s,
                                   oyOption_StructGet( o, oyOBJECT_PROFILE_S );
 
     if(!output->oy_profile)
-      oyDeviceGetProfile( device, &output->oy_profile );
+    {
+      oyOptions_s * options = 0;
+      oyOptions_SetFromText( &options,
+                               "//"OY_TYPE_STD"/config/net_color_region_target",
+                                       "yes", OY_CREATE_NEW );
+      oyDeviceGetProfile( device, options, &output->oy_profile );
+      oyOptions_Release( &options );
+    }
 
     if(!output->oy_profile)
     {

examples/image2pdf/image2pdf.c
@@ -142,7 +142,7 @@ int main (int argc, char ** argv)
    */
   error = oyDeviceGet( OY_TYPE_STD, "monitor", 0, 0,
                        &device );
-  error = oyDeviceGetProfile( device, &prof );
+  error = oyDeviceGetProfile( device, 0, &prof );
   data = oyProfile_GetMem( prof, &size, 0, malloc );
 
   if(!size || !data)

examples/xcmsevents/xcmsevents_common.c
@@ -366,8 +366,8 @@ int      xcmseContext_Setup          ( xcmseContext_s    * c,
     FILE * fp;
     size_t size = 256, i = 0;
     char * txt;
-    S( "running \"oyranos-monitor -l\":%s", "" );
-    fp = popen(  "oyranos-monitor -l", "r" );
+    S( "running \"oyranos-monitor -lc\":%s", "" );
+    fp = popen(  "oyranos-monitor -lc", "r" );
     if( fp )
     {
       txt = (char*) oyAllocateFunc_( size + 1 );

modules/oyranos_cmm_oydi.c
@@ -673,7 +673,12 @@ int      oydiFilterPlug_ImageDisplayRun(oyFilterPlug_s   * requestor_plug,
       /* set the device profile of all CMM's image data */
       if(init)
       {
-        error = oyDeviceGetProfile( c, &p );
+        oyOptions_s * options = 0;
+        error = oyOptions_SetFromText( &options,
+                               "//"OY_TYPE_STD"/config/net_color_region_target",
+                                       "yes", OY_CREATE_NEW );
+        error = oyDeviceGetProfile( c, options, &p );
+        oyOptions_Release( &options );
 
         if(p && image_input && !oyProfile_Equal( image_input->profile_, p ))
         {

oyranos_alpha.c
@@ -12604,7 +12604,7 @@ const char **oyConfDomain_GetTexts_  ( oyConfDomain_s_   * obj )
  *  All other functions return a handle to the device. With this handle it is
  *  possible to get informations (oyDeviceGetInfo()), query it's current,
  *  possibly remote profile (strong oyDeviceGetProfile() /
- *  weak oyDeviceAskProfile()) set the profile persistent
+ *  weak oyDeviceAskProfile2()) set the profile persistent
  *  (oyDeviceSetProfile()) or query the persistent stored profile
  *  (oyDeviceProfileFromDB()).
  *
@@ -13070,7 +13070,7 @@ OYAPI int  OYEXPORT
 
   {
     /* 1. ask for the profile the device is setup with */
-    error = oyDeviceAskProfile( device, &p );
+    error = oyDeviceAskProfile2( device, 0, &p );
     if(p)
     {
       oyProfile_Release( &p );
@@ -13395,9 +13395,10 @@ OYAPI int  OYEXPORT
  *  get at least one basic profile.
  *
  *  For a basic and thus weaker call to the device use
- *  oyDeviceAskProfile() instead.
+ *  oyDeviceAskProfile2() instead.
  *
- *  @param         device          the device
+ *  @param         device              the device
+ *  @param         options             options passed to the backend
  *  @param         profile             the device's ICC profile
  *  @return                            error
  *
@@ -13407,6 +13408,7 @@ OYAPI int  OYEXPORT
  */
 OYAPI int  OYEXPORT
            oyDeviceGetProfile        ( oyConfig_s        * device,
+                                       oyOptions_s       * options,
                                        oyProfile_s      ** profile )
 {
   int error = !device,
@@ -13416,7 +13418,7 @@ OYAPI int  OYEXPORT
   oyCheckType__m( oyOBJECT_CONFIG_S, return 1 )
 
 
-  l_error = oyDeviceAskProfile( device, profile ); OY_ERR
+  l_error = oyDeviceAskProfile2( device, options, profile ); OY_ERR
 
   /** This function does a device setup in case no profile is delivered
    *  by the according module. */
@@ -13424,7 +13426,7 @@ OYAPI int  OYEXPORT
     l_error = oyDeviceSetup( device ); OY_ERR
 
   if(error <= 0) 
-    l_error = oyDeviceAskProfile( device, profile ); OY_ERR
+    l_error = oyDeviceAskProfile2( device, options, profile ); OY_ERR
 
   /** As a last means oyASSUMED_WEB is delivered. */
   if(!*profile)
@@ -13523,90 +13525,6 @@ OYAPI int  OYEXPORT
 
   if(own_options)
     oyOptions_Release( &options );
-  oyOption_Release( &o );
-
-  return error;
-}
-
-/** Function oyDeviceAskProfile
- *  @brief   ask for the device profile
- *
- *  Ask for a profile associated with the device. A device capable to
- *  hold a profile only the held profile will be checked and returned.
- *  In case this profile is not found a "icc_profile" of oyVAL_STRUCT should be
- *  included.
- *
- *  The device might not be able to hold a profile, then just the DB profile
- *  will be returned from here without an issue. For interessted users, the
- *  source of the profile keeps transparent, as it can be checked if the
- *  device contains a "icc_profile" option which contains a oyProfile_s object.
- *
- *  @param[in]     device              the device
- *  @param[out]    profile             the device's ICC profile
- *  @return                            0 - good, 1 >= error, -1 <= issue(s)
- *
- *  @version Oyranos: 0.1.10
- *  @since   2009/02/10 (Oyranos: 0.1.10)
- *  @date    2009/08/15
- */
-OYAPI int  OYEXPORT
-           oyDeviceAskProfile        ( oyConfig_s        * device,
-                                       oyProfile_s      ** profile )
-{
-  int error = !device;
-  oyOptions_s * options = 0;
-  oyOption_s * o = 0;
-  oyConfig_s * s = device;
-
-  oyCheckType__m( oyOBJECT_CONFIG_S, return 1 )
-
-
-  if(!options)
-  {
-    options = oyOptions_New( 0 );
-
-    error = !options;
-  }
-
-  if(error <= 0)
-  {
-    /* add "list" call to module arguments */
-    error = oyOptions_SetRegistrationTextKey_( options,
-                                               device->registration,
-                                               "command", "list" );
-  }
-
-  if(error <= 0)
-  {
-    error = oyOptions_SetRegistrationTextKey_( options,
-                                               device->registration,
-                                               "icc_profile", "true" );
-  }
-
-  if(error <= 0)
-    error = oyDeviceBackendCall( device, options );
-
-  if(error <= 0)
-    o = oyConfig_Find( device, "icc_profile" );
-
-  if(o && o->value_type == oyVAL_STRUCT &&
-     o->value)
-  {
-    if(o->value->oy_struct && 
-       o->value->oy_struct->type_ == oyOBJECT_PROFILE_S)
-      *profile = oyProfile_Copy( (oyProfile_s*) o->value->oy_struct, 0 );
-    else if(!error)
-      error = -1;
-  }
-  else
-  {
-    char * profile_name = 0;
-    oyDeviceProfileFromDB( device, &profile_name, 0 );
-    *profile = oyProfile_FromFile( profile_name, 0,0 );
-    oyDeAllocateFunc_( profile_name );
-  }
-
-  oyOptions_Release( &options );
   oyOption_Release( &o );
 
   return error;
@@ -28243,7 +28161,7 @@ char *   oyGetMonitorProfile         ( const char        * device_name,
   {
     error = oyDeviceGet( device_type, device_class, device_name,
                              options, &device );
-    error = oyDeviceGetProfile    ( device, &p );
+    error = oyDeviceGetProfile( device, 0, &p );
     oyConfig_Release( &device );
   }
 

oyranos_alpha.h
@@ -1557,9 +1557,7 @@ OYAPI int  OYEXPORT
                                        const char        * profile_name );
 OYAPI int  OYEXPORT
            oyDeviceGetProfile        ( oyConfig_s        * device,
-                                       oyProfile_s      ** profile );
-OYAPI int  OYEXPORT
-           oyDeviceAskProfile        ( oyConfig_s        * device,
+                                       oyOptions_s       * options,
                                        oyProfile_s      ** profile );
 OYAPI int  OYEXPORT
            oyDeviceAskProfile2       ( oyConfig_s        * device,

oyranos_gamma.c
@@ -52,7 +52,7 @@ int main( int argc , char** argv )
   char * format = 0;
   char * output = 0;
   int server = 0;
-  int colour_server = 0;
+  int net_color_region_target = 0;
 
   char *ptr = NULL;
   int x = 0, y = 0;
@@ -137,7 +137,7 @@ int main( int argc , char** argv )
             {
               case 'e': erase = 1; monitor_profile = 0; break;
               case 'b': database = 1; monitor_profile = 0; break;
-              case 'c': colour_server = 1; monitor_profile = 0; break;
+              case 'c': net_color_region_target = 1; monitor_profile = 0; break;
               case 'f': OY_PARSE_STRING_ARG(format); monitor_profile = 0; break;
               case 'l': list = 1; monitor_profile = 0; break;
               case 'o': OY_PARSE_STRING_ARG(output); monitor_profile = 0; break;
@@ -151,8 +151,8 @@ int main( int argc , char** argv )
                         {
                              if(strcmp(&argv[pos][2],"unset") == 0)
                         { erase = 1; monitor_profile = 0; i=100; break; }
-                        else if(strcmp(&argv[pos][2],"colour_server") == 0)
-                        { colour_server = 1; i=100; break; }
+                        else if(strcmp(&argv[pos][2],"net_color_region_target") == 0)
+                        { net_color_region_target = 1; i=100; break; }
                         else if(strcmp(&argv[pos][2],"setup") == 0)
                         { setup = 1; i=100; break; }
                         else if(strcmp(&argv[pos][2],"format") == 0)
@@ -232,6 +232,10 @@ int main( int argc , char** argv )
 
       error = oyDeviceGet( OY_TYPE_STD, "monitor", oy_display_name, 0,
                            &device );
+      if(net_color_region_target)
+        error = oyOptions_SetFromText( &options,
+                                       "//"OY_TYPE_STD"/config/net_color_region_target",
+                                       "yes", OY_CREATE_NEW );
 
       if(database)
       {
@@ -239,7 +243,7 @@ int main( int argc , char** argv )
         prof = oyProfile_FromFile( fn, 0, 0 );
         filename = fn;
       } else {
-        error = oyDeviceGetProfile    ( device, &prof );
+        error = oyDeviceGetProfile( device, options, &prof );
         filename = oyProfile_GetFileName( prof, -1 );
       }
       data = oyProfile_GetMem( prof, &size, 0, oyAllocFunc);
@@ -255,6 +259,7 @@ int main( int argc , char** argv )
       if(fn) oyDeAllocFunc( fn ); fn = 0;
       oyProfile_Release( &prof );
       oyConfig_Release( &device );
+      oyOptions_Release( &options );
     }
 
     if(format &&
@@ -265,7 +270,6 @@ int main( int argc , char** argv )
       oyConfigs_s * devices = 0;
       oyConfig_s * c = 0;
       icHeader * header = 0;
-      oyOptions_s * options = 0;
       oyOption_s * o = 0;
       char * out_name = 0;
 
@@ -342,11 +346,11 @@ int main( int argc , char** argv )
           if(strcmp(format,"icc") == 0)
           {
             oyOptions_s * cs_options = 0;
-            if(colour_server)
+            if(net_color_region_target)
             {
               /* get OY_ICC_COLOUR_SERVER_TARGET_PROFILE_IN_X_BASE */
               error = oyOptions_SetFromText( &cs_options,
-              "//"OY_TYPE_STD"/config/colour_server", "yes", OY_CREATE_NEW );
+              "//"OY_TYPE_STD"/config/net_color_region_target", "yes", OY_CREATE_NEW );
             }
             oyDeviceAskProfile2( c, cs_options, &prof );
             oyOptions_Release( &cs_options );
@@ -375,6 +379,7 @@ int main( int argc , char** argv )
         }
       }
       oyConfigs_Release( &devices );
+      oyOptions_Release( &options );
     }
 
 
@@ -387,11 +392,11 @@ int main( int argc , char** argv )
       oyOption_s * o = 0;
       oyOptions_s * cs_options = 0;
 
-      if(colour_server)
+      if(net_color_region_target)
       {
         /* get OY_ICC_COLOUR_SERVER_TARGET_PROFILE_IN_X_BASE */
         error = oyOptions_SetFromText( &cs_options,
-              "//"OY_TYPE_STD"/config/colour_server", "yes", OY_CREATE_NEW );
+              "//"OY_TYPE_STD"/config/net_color_region_target", "yes", OY_CREATE_NEW );
       }
       error = oyOptions_SetFromText( &options,
                                      "//" OY_TYPE_STD "/config/command",
@@ -438,7 +443,7 @@ int main( int argc , char** argv )
             if(text) oyDeAllocFunc( text ); text = 0;
             oyOption_Release( &o );
 
-            oyDeviceAskProfile( c, &prof );
+            oyDeviceAskProfile2( c, cs_options, &prof );
             data = oyProfile_GetMem( prof, &size, 0, oyAllocFunc);
             if(size && data)
               oyDeAllocFunc( data );

commit d4e77ab0e68376200e066c0a7f5cf5d132977ca0

* [oyX1]: revert icc_profile option omitting
The bug was introduced with 62fe5fe7.

modules/devices/oyranos_cmm_oyX1.c
@@ -648,9 +648,9 @@ int            oyX1Configs_Modify    ( oyConfigs_s       * devices,
 
           has = 0;
           /*  */
-          /*o = oyConfig_Find( device, "icc_profile" );
+          o = oyConfig_Find( device, "icc_profile" );
           if(o)
-            has = 1;*/
+            has = 1;
 
           if(data && size)
           {