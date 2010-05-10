ae291db940be9bf3315d0155c31b82d65b4dba9b
Yiannis Belias <jonnyb@hol.gr>
2010-05-10 13:35:29 +0300

    Add a new ClassMethods block & fix indentation

--- templates/Struct.template.h
+++ templates/Struct.template.h
@@ -2,15 +2,19 @@
 #define OY_{{ class_name|upper }}_H
 
 typedef struct {
-   {% block BaseMembers %}
-   /* Struct base class start */
-   oyOBJECT_e           type_;          /**< struct type oyOBJECT_{{ class_name|upper  }}_S */ 
-   oyStruct_Copy_f      copy;           /**< copy function */
-   oyStruct_Release_f   release;        /**< release function */
-   oyObject_s           oy_;            /**< base object */
-   /* Struct base class stop */
-   {% endblock %}
-   {% block ChildMembers %}{% endblock %}
-} {% block ClassName %}oy{{ class_name }}_s{% endblock %}
+  {% block BaseMembers %}
+  /* Struct base class start */
+  oyOBJECT_e           type_;          /**< @private struct type */
+  oyStruct_Copy_f      copy;           /**< copy function */
+  oyStruct_Release_f   release;        /**< release function */
+  oyObject_s           oy_;            /**< @private features name and hash */
+  /* Struct base class stop */
+  {% endblock %}
+  {% block ChildMembers %}{% endblock %}
+} {% block ClassName %}oy{{ class_name }}_s{% endblock %};
+
+{% block ClassMethods %}
+const char * oy{{ class_name }}_GetText( oyStruct_s * obj, oyNAME_e name_type, uint32_t flags );
+{% endblock %}
 
 #endif /* OY_{{ class_name|upper }}_H */
