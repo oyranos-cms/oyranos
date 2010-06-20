826be3d47026561429758694f55a5ea357f63275
Yiannis Belias <jonnyb@hol.gr>
2010-06-21 01:32:09 +0300

    [templates] Also print the parent child members

--- templates/Class_s_.h
+++ templates/Class_s_.h
@@ -1,7 +1,10 @@
 {% extends "Base_s_.h" %}
 
 {% block ChildMembers %}
+{{ block.super }}
+  /* {{ class.privName }} members start */
 {% include "Class.members.h" %}
+  /* {{ class.privName }} members stop */
 {% endblock %}
 
 {% block SpecificPrivateMethodsDeclarations %}
