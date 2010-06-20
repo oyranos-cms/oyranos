559d4f9d0858fe5ae37165421429625325d256a1
Yiannis Belias <jonnyb@hol.gr>
2010-06-20 11:53:31 +0300

    [Name] Add the doxygen doc

--- templates/Name_s.template.h
+++ templates/Name_s.template.h
@@ -1,5 +1,8 @@
 {% extends "Base_s.h" %}
 
+{% block doxygenPublicClass %}
+{% include "Name.dox" %}
+{% endblock %}
 {% block SpecificPublicMethodsDeclarations %}
 {% include "Name.public_methods_declarations.h" %}
 {% endblock SpecificPublicMethodsDeclarations %}
