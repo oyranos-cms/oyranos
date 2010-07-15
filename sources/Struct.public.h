df24a242c578f6c48a5c2c8783a00f77f9e3c3d6
Yiannis Belias <jonnyb@hol.gr>
2010-07-15 17:55:18 +0300

    [sources] Put the oyranos message functions declarations
    in the sources of the oyStruct_s object.

--- sources/Struct.public.h
+++ sources/Struct.public.h
@@ -1,3 +1,18 @@
 typedef oyStruct_s * (*oyStruct_Copy_f ) ( oyStruct_s *, oyPointer );
 typedef int       (*oyStruct_Release_f ) ( oyStruct_s ** );
 typedef oyPointer (*oyStruct_LockCreate_f)(oyStruct_s * obj );
+
+/* Message function declarations { */
+typedef enum {
+  oyMSG_ERROR = 300,
+  oyMSG_WARN,
+  oyMSG_DBG
+} oyMSG_e;
+
+typedef int  (*oyMessage_f)          ( int/*oyMSG_e*/      code,
+                                       const oyStruct_s  * context,
+                                       const char        * format,
+                                       ... );
+int            oyMessageFuncSet      ( oyMessage_f         message_func );
+extern         oyMessage_f             oyMessageFunc_p;
+/* } Message function declarations */
