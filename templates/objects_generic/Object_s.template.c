a06a9617263a7e9eab9c01727773cbd98f9738d3
Yiannis Belias <jonnyb@hol.gr>
2010-07-17 14:34:43 +0300

    [templates] Add #includes for compiling oyObject_s

--- templates/objects_generic/Object_s.template.c
+++ templates/objects_generic/Object_s.template.c
@@ -5,6 +5,8 @@
 #include "{{ class.name }}.h"
 #include "{{ class.privName }}.h"
 
+#include "oyName_s_.h"
+
 {% block SpecificPublicMethodsDefinitions %}
 /* Include "{{ class.public_methods_definitions_c }}" { */
 {% include class.public_methods_definitions_c %}
