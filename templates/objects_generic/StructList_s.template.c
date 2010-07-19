b03c9907c4556d39368867722ae745a33202d1e2
Yiannis Belias <jonnyb@hol.gr>
2010-07-20 00:54:46 +0300

    [templates] Add proper #include files

--- templates/objects_generic/StructList_s.template.c
+++ templates/objects_generic/StructList_s.template.c
@@ -1,1 +1,6 @@
 {% extends "Base_s.c" %}
+
+{% block IncludePrivateHeader %}
+{{ block.super }}
+#include <stdio.h>
+{% endblock %}
--- templates/objects_generic/StructList_s.template.h
+++ templates/objects_generic/StructList_s.template.h
@@ -1,1 +1,6 @@
 {% extends "Base_s.h" %}
+
+{% block ProjectIncludes %}
+{{ block.super }}
+#include "oyObserver_s.h"
+{% endblock %}
