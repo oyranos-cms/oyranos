8e86c58d4ab4611c54cb20a886149ceb6d263db5
Yiannis Belias <jonnyb@hol.gr>
2010-07-24 14:11:32 +0300

    [sources] Remove oyOption_FromStatic_(). It is deprecated

--- sources/Option.private_methods_declarations.h
+++ sources/Option.private_methods_declarations.h
@@ -1,5 +1,3 @@
-oyOption_s *   oyOption_FromStatic_  ( oyOption_t_       * opt,
-                                       oyObject_s          object );
 int            oyOption_Match_       ( oyOption_s_       * option_a,
                                        oyOption_s_       * option_b );
 void         oyOption_UpdateFlags_   ( oyOption_s_       * o );
--- sources/Option.private_methods_definitions.c
+++ sources/Option.private_methods_definitions.c
@@ -1,56 +1,5 @@
 /**
  *  @internal
- *  Function oyOption_FromStatic_
- *  @memberof oyOption_s
- *  @brief   provide the current state of Oyranos behaviour settings
- *
- *  @todo harmonise the xml key names to the elektra ones to form one namespace
- *  @deprecated we move to the oyCMMapi5_s for static stuff
- *
- *  @version Oyranos: 0.1.9
- *  @since   2008/11/06 (Oyranos: 0.1.9)
- *  @date    2008/11/13
- */
-oyOption_s *   oyOption_FromStatic_  ( oyOption_t_       * opt,
-                                       oyObject_s          object )
-{
-  oyOption_s_ * s = 0;
-  int error = !opt;
-
-  if(error)
-    return s;
-
-  s = (oyOption_s_*)oyOption_New( 0, object );
-  if(!s)
-    return s;
-
-  /*s->id = opt->id;*/
-  s->registration = oyStringAppend_( opt->config_string, opt->config_string_xml,
-                                     s->oy_->allocateFunc_ );
-  s->value = s->oy_->allocateFunc_(sizeof(oyValue_u));
-  memset( s->value, 0, sizeof(oyValue_u) );
-
-  if(oyWIDGET_BEHAVIOUR_START < opt->id && opt->id < oyWIDGET_BEHAVIOUR_END)
-  {
-    s->value_type = oyVAL_DOUBLE;
-    s->value->dbl = opt->default_value;
-    s->source = oyOPTIONSOURCE_FILTER;
-  } else
-  {
-    s->value_type = oyVAL_STRING;
-    s->value->string = oyGetDefaultProfileName( (oyPROFILE_e)opt->id,
-                                                s->oy_->allocateFunc_ );
-    if(!s->value->string)
-      s->value->string = oyStringCopy_( opt->default_string, s->oy_->allocateFunc_);
-    s->source = oyOPTIONSOURCE_FILTER;
-    oyOption_UpdateFlags_( s );
-  }
-
-  return s;
-}
-
-/**
- *  @internal
  *  Function oyOption_Match_
  *  @memberof oyOption_s
  *  @brief   two option key name matches
