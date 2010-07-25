0a389cc0d78cc958b1be9535050b5b8dd77e08fb
Yiannis Belias <jonnyb@hol.gr>
2010-07-25 19:08:25 +0300

    [tempaltes] Import oyOPTIONATTRIBUTE_e enum

--- templates/oyranos_object.template.h
+++ templates/oyranos_object.template.h
@@ -55,6 +55,64 @@ typedef enum {
   oyNAME_DESCRIPTION                   /**< compatible to oyName_s/oyObject_s */
 } oyNAME_e;
 
+/** @brief   select from filter
+ *  @ingroup objects_value
+ *
+ *  @version Oyranos: 0.1.10
+ *  @since   2009/07/27 (Oyranos: 0.1.10)
+ *  @date    2009/07/27
+ */
+#define OY_SELECT_FILTER         2048
+/** @brief   select from policy
+ *  @ingroup objects_value
+ *
+ *  Select typical from a associatable oyCMMapi9_s type of filter for providing
+ *  common options.
+ *
+ *  @version Oyranos: 0.1.10
+ *  @since   2009/07/27 (Oyranos: 0.1.10)
+ *  @date    2009/07/27
+ */
+#define OY_SELECT_COMMON         4096
+/** @enum    oyOPTIONATTRIBUTE_e
+ *  @brief   usage type
+ *  @ingroup objects_value
+ *
+ *  The types of Oyranos default settings to include into the options set.
+ *  So proofing makes no sense for toolkit widgets, but for advanced
+ *  graphics displaying. All other Oyranos behaviour settings should be taken
+ *  over untouched.
+ *
+ *  The ".invisible" attribute says a option shall not be displayed in a GUI.
+ *  The ".readonly" attribute alllows for unchangeable, static options. They
+ *  might be used for passing additional informations.
+ *  The ".font" and ".advanced" attributes are described for the according
+ *  enum values.
+ *
+ *  See as well @ref registration
+ *
+ *  @version Oyranos: 0.1.9
+ *  @since   2008/10/08 (Oyranos: 0.1.8)
+ *  @date    2008/11/27
+ */
+typedef enum {
+  /** basic settings, as typical for toolkits and office/web applications,
+   *  e.g. disable proofing or disable mark out of gamut colours */
+  oyOPTIONATTRIBUTE_BASIC = 0,
+  /** user modified, e.g. after oyOption_SetFromText() */
+  oyOPTIONATTRIBUTE_EDIT = 32,
+  /** automatically modified, e.g. by a CMM */
+  oyOPTIONATTRIBUTE_AUTOMATIC = 64,
+  /** advanced settings, as typical for editing, e.g. include proofing
+   *  (options appended with ".advanced") */
+  oyOPTIONATTRIBUTE_ADVANCED = 128,
+  /** front end options, handled by the framework or policy module,
+   *  e.g. ".front" */
+  oyOPTIONATTRIBUTE_FRONT = 256,
+  /** tell this options is included twice*/
+  oyOPTIONATTRIBUTE_DOUBLE = 512
+} oyOPTIONATTRIBUTE_e;
+
 {% include "cpp_end.h" %}
 
 #endif /* OYRANOS_OBJECT_H */
