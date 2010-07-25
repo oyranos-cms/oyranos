3eaa18030e6c0d770ec883151c306d278ef7ffec
Yiannis Belias <jonnyb@hol.gr>
2010-07-25 23:11:15 +0300

    [templates] Fix typo for previous commit

--- templates/objects_value/Options_s.template.c
+++ templates/objects_value/Options_s.template.c
@@ -2,7 +2,7 @@
 
     {% block MoveIn_Observed %}{
       if(oyStruct_IsObserved((oyStruct_s*)s, 0))
-        oyStruct_ObserverAdd( (oyStruct_s*)*obj, (oyStruct_s*)s->list, 0, 0 );
-      error = oyStructList_MoveIn( s->list, (oyStruct_s**)obj, pos,
+        oyStruct_ObserverAdd( (oyStruct_s*)*obj, (oyStruct_s*)s->list_, 0, 0 );
+      error = oyStructList_MoveIn( s->list_, (oyStruct_s**)obj, pos,
                                    OY_OBSERVE_AS_WELL );
     }{% endblock %}
