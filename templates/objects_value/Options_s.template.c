7fb1e6dfea8da6c93d80739fabfad9dc935c3319
Yiannis Belias <jonnyb@hol.gr>
2010-07-26 21:37:22 +0300

    [templates] Add oyranos_sentinel.h to Options_s.c

--- templates/objects_value/Options_s.template.c
+++ templates/objects_value/Options_s.template.c
@@ -1,5 +1,10 @@
 {% extends "BaseList_s.c" %}
 
+{% block LocalIncludeFiles %}
+{{ block.super }}
+#include "oyranos_sentinel.h"
+{% endblock %}
+
     {% block MoveIn_Observed %}{
       if(oyStruct_IsObserved((oyStruct_s*)s, 0))
         oyStruct_ObserverAdd( (oyStruct_s*)*obj, (oyStruct_s*)s->list_, 0, 0 );
