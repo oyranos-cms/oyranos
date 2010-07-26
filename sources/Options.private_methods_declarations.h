d0f33bf820a95b5d79a1a02e5e0eb0a629dfa1bd
Yiannis Belias <jonnyb@hol.gr>
2010-07-26 19:38:58 +0300

    [sources] Ifdef oyOptions_ForFilter_ to only be used when oyFilterCore is imported.

--- sources/Options.private_methods_declarations.h
+++ sources/Options.private_methods_declarations.h
@@ -1,6 +1,8 @@
+#ifdef OY_FILTER_CORE_S_H
 oyOptions_s *  oyOptions_ForFilter_  ( oyFilterCore_s    * filter,
                                        uint32_t            flags,
                                        oyObject_s          object );
+#endif /* OY_FILTER_CORE_S_H */
 int    oyOptions_SetDeviceTextKey_   ( oyOptions_s       * options,
                                        const char        * device_type,
                                        const char        * device_class,
