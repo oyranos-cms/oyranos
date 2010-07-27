a7ff8c1af1fd6946692e5ad625bb0e251e9a19c2
Yiannis Belias <jonnyb@hol.gr>
2010-07-27 09:50:11 +0300

    Create skeleton files for oyBlob_s

---
+++ sources/Blob.members.h
@@ -0,0 +1,4 @@
+  size_t               size;           /**< data size */
+  oyPointer            ptr;            /**< data */
+  int                  flags;          /**< 0x01 - static ptr */
+  char                 type[8];        /**< the type of data, e.g. oyCOLOUR_ICC_DEVICE_LINK / "oyDL" */
