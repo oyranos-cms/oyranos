62ae8d6caef0515d5a803d723d541e9278ebb6a2
Yiannis Belias <jonnyb@hol.gr>
2010-07-29 17:40:27 +0300

    [sources] Import private methods for oyCMMptr_s

---
+++ sources/CMMptr.private_methods_declarations.h
@@ -0,0 +1,6 @@
+int                oyCMMptr_Set_     ( oyCMMptr_s        * cmm_ptr,
+                                       const char        * lib_name,
+                                       const char        * resource,
+                                       oyPointer           ptr,
+                                       const char        * func_name,
+                                       oyPointer_release_f ptrRelease );
