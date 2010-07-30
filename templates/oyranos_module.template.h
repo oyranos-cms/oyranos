51452e615c89ed1fd89fcdb5f7be09676a5e0f29
Yiannis Belias <jonnyb@hol.gr>
2010-07-30 14:16:02 +0300

    [templates] Add a new header file "oyranos_module.h"
    This is the new oyranos_cmm.h, with different name.

---
+++ templates/oyranos_module.template.h
@@ -0,0 +1,55 @@
+{% include "source_file_header.txt" %}
+
+#ifndef OYRANOS_MODULE_H
+#define OYRANOS_MODULE_H
+
+{% include "cpp_begin.h" %}
+
+
+//#include "oyStruct_s.h"
+/** @brief   CMM capabilities query enum
+ *  @ingroup module_api
+ *
+ *  @since: 0.1.8
+ */
+typedef enum {
+  oyQUERY_OYRANOS_COMPATIBILITY,       /*!< provides the Oyranos version and expects the CMM compiled or compatibility Oyranos version back */
+  oyQUERY_PROFILE_FORMAT = 20,         /*!< value 1 == ICC */
+  oyQUERY_PROFILE_TAG_TYPE_READ,       /**< value a icTagTypeSignature (ICC) */
+  oyQUERY_PROFILE_TAG_TYPE_WRITE,      /**< value a icTagTypeSignature (ICC) */
+  oyQUERY_MAX
+} oyCMMQUERY_e;
+
+/**
+ *  typedef oyCMMCanHandle_f
+ *  @brief   CMM feature declaration function
+ *  @ingroup module_api
+ *  @memberof oyCMMapi_s
+ */
+typedef int      (*oyCMMCanHandle_f) ( oyCMMQUERY_e        type,
+                                       uint32_t            value );
+
+/**
+ *  typedef oyCMMInit_f
+ *  @brief   optional CMM init function
+ *  @ingroup module_api
+ *  @memberof oyCMMapi_s
+ */
+typedef int      (*oyCMMInit_f)      ( oyStruct_s        * filter );
+
+/**
+ *  typedef oyCMMMessageFuncSet_f
+ *  @brief   optionaly sets a CMM message function
+ *  @ingroup module_api
+ *  @memberof oyCMMapi_s
+ */
+typedef int      (*oyCMMMessageFuncSet_f)( oyMessage_f     message_func );
+
+#define oyCMM_PROFILE "oyPR"
+#define oyCMM_COLOUR_CONVERSION "oyCC"
+#define oyCOLOUR_ICC_DEVICE_LINK "oyDL"
+
+
+{% include "cpp_end.h" %}
+
+#endif /* OYRANOS_OBJECT_H */
