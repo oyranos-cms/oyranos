69e4f944f7e8033f47373503b761523dbc2cc92c
Yiannis Belias <jonnyb@hol.gr>
2010-07-31 00:17:45 +0300

    [build] Count new grantlee_extensions/ files

--- extract_sources.sh
+++ extract_sources.sh
@@ -6,7 +6,7 @@ function loc {
   echo "############# Lines Of Code ############# "
   find sources/ -name "*.c" -o -name "*.cc" -o -name "*.txt" -o -name "*.h" | xargs wc -l | grep total | sed 's/ *\([0-9]\+\).*/\1/' | xargs printf "Total %d lines of imported C code in sources/\n"
   find templates/ -name "*.c" -o -name "*.cc" -o -name "*.txt" -o -name "*.h" | xargs wc -l | grep total | sed 's/ *\([0-9]\+\).*/\1/' | xargs printf "Total %d lines of template C/C++ code in templates/\n"
-  wc -l *.cc *.h | grep total | sed 's/ *\([0-9]\+\).*/\1/' | xargs printf "Total %d lines of C++ code for oyAPIGenerator\n"
+  wc -l *.cc *.h grantlee_extensions/*.h grantlee_extensions/*.cpp | grep total | sed 's/ *\([0-9]\+\).*/\1/' | xargs printf "Total %d lines of C++ code for oyAPIGenerator\n"
   echo                                                                            "+------------------------------------------------------------+"
   wc -l API_generated/* | grep total | sed 's/ *\([0-9]\+\).*/\1/' | xargs printf "Total %d lines of C/C++ auto-generated code in API_generated/\n"
   echo "############# Lines Of Code ############# "
