1198fa252138f94d090d5b205b3555e937799735
Yiannis Belias <jonnyb@hol.gr>
2010-07-31 12:16:10 +0300

    [sources] Private methods take the hidden class pointer

--- sources/CMMapi.private_methods_declarations.h
+++ sources/CMMapi.private_methods_declarations.h
@@ -1,9 +1,9 @@
-oyOBJECT_e       oyCMMapi_Check_     ( oyCMMapi_s        * api );
-oyOBJECT_e   oyCMMapi_CheckWrap_     ( oyCMMInfo_s       * cmm_info,
-                                       oyCMMapi_s        * api,
+oyOBJECT_e       oyCMMapi_Check_     ( oyCMMapi_s_       * api );
+oyOBJECT_e   oyCMMapi_CheckWrap_     ( oyCMMInfo_s_      * cmm_info,
+                                       oyCMMapi_s_       * api,
                                        oyPointer           data,
                                        uint32_t          * rank );
-oyOBJECT_e   oyCMMapi_selectFilter_  ( oyCMMInfo_s       * info,
-                                       oyCMMapi_s        * api,
+oyOBJECT_e   oyCMMapi_selectFilter_  ( oyCMMInfo_s_      * info,
+                                       oyCMMapi_s_       * api,
                                        oyPointer           data,
                                        uint32_t          * rank );
