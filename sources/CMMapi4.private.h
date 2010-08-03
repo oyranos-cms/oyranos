f8265f608d1f15ddccbb8b7c3f26063707f6b9c3
Yiannis Belias <jonnyb@hol.gr>
2010-08-03 20:33:07 +0300

    [sources] Add oyRegistrationData_s private struct

---
+++ sources/CMMapi4.private.h
@@ -0,0 +1,10 @@
+/** @internal
+ *
+ *  @version Oyranos: 0.1.9
+ *  @since   2008/12/16 (Oyranos: 0.1.9)
+ *  @date    2008/12/16
+ */
+typedef struct {
+  oyOBJECT_e type;
+  const char        * registration;
+} oyRegistrationData_s;
