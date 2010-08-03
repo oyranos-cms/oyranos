774466fd824b652707ea58b6f92e8c2ec15e6c7d
Yiannis Belias <jonnyb@hol.gr>
2010-08-03 20:03:08 +0300

    * Create skeleton files for oyCMMapi9_s

---
+++ templates/module_api/CMMapi9_s_.template.h
@@ -0,0 +1,8 @@
+{% extends "CMMapiFilter_s_.template.h" %}
+
+{% block ChildMembers %}
+{{ block.super }}
+/* Include "CMMapi9.members.h" { */
+{% include "CMMapi9.members.h" %}
+/* } Include "CMMapi9.members.h" */
+{% endblock %}
