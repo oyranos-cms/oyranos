0c9e52e219ebd9cc941df48356e3e291135bf818
Yiannis Belias <jonnyb@hol.gr>
2010-08-09 02:44:06 +0300

    [sources] Import private methods for oyCMMapi3_s

---
+++ sources/CMMapi3.private_methods_declarations.h
@@ -0,0 +1,4 @@
+oyOBJECT_e   oyCMMapi3_Query_        ( oyCMMInfo_s       * cmm_info,
+                                       oyCMMapi_s        * api,
+                                       oyPointer           data,
+                                       uint32_t          * rank );
