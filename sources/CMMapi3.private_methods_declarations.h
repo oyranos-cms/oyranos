c1de4ea5e7dcb12222acdb6a117ebd7a993274ac
Yiannis Belias <jonnyb@hol.gr>
2010-08-09 02:58:07 +0300

    [review] [sources] Adopt oyCMMapi3_Query_() to "hidden struct" interface.

--- sources/CMMapi3.private_methods_declarations.h
+++ sources/CMMapi3.private_methods_declarations.h
@@ -1,4 +1,4 @@
-oyOBJECT_e   oyCMMapi3_Query_        ( oyCMMInfo_s       * cmm_info,
-                                       oyCMMapi_s        * api,
+oyOBJECT_e   oyCMMapi3_Query_        ( oyCMMInfo_s_      * cmm_info,
+                                       oyCMMapi_s_       * api,
                                        oyPointer           data,
                                        uint32_t          * rank );
