80a7c690c0fda676a9a266f230afae3722eab86e
Yiannis Belias <jonnyb@hol.gr>
2010-08-10 19:26:08 +0300

    [templates] Include files in oyProfile_s_.h

--- templates/objects_profile/Profile_s_.template.h
+++ templates/objects_profile/Profile_s_.template.h
@@ -1,5 +1,15 @@
 {% extends "Base_s_.h" %}
 
+{% block GlobalIncludeFiles %}
+{{ block.super }}
+#include <icc34.h>
+{% endblock %}
+
+{% block LocalIncludeFiles %}
+{{ block.super }}
+#include "oyStructList_s.h"
+{% endblock %}
+
 {% block ChildMembers %}
 {{ block.super }}
 /* Include "Profile.members.h" { */
