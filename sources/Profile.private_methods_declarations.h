a3c50a6fb0f5a29ec8a53ee9e5e5e499e95a3f8e
Yiannis Belias <jonnyb@hol.gr>
2010-08-11 01:02:43 +0300

    [sources] Import private methods for oyProfile_s

---
+++ sources/Profile.private_methods_declarations.h
@@ -0,0 +1,37 @@
+oyProfile_s* oyProfile_FromMemMove_  ( size_t              size,
+                                       oyPointer         * block,
+                                       int                 flags,
+                                       oyObject_s          object);
+oyProfile_s *  oyProfile_FromFile_   ( const char        * name,
+                                       uint32_t            flags,
+                                       oyObject_s          object );
+oyPointer    oyProfile_TagsToMem_    ( oyProfile_s       * profile,
+                                       size_t            * size,
+                                       oyAlloc_f           allocateFunc );
+int          oyProfile_ToFile_       ( oyProfile_s       * profile,
+                                       const char        * file_name );
+int32_t      oyProfile_Match_        ( oyProfile_s       * pattern,
+                                       oyProfile_s       * profile );
+int32_t      oyProfile_Hashed_       ( oyProfile_s       * s );
+char *       oyProfile_GetFileName_r ( oyProfile_s       * profile,
+                                       oyAlloc_f           allocateFunc );
+oyProfileTag_s * oyProfile_GetTagByPos_( oyProfile_s     * profile,
+                                       int                 pos );
+int                oyProfile_GetTagCount_ (
+                                       oyProfile_s       * profile );
+int          oyProfile_TagMoveIn_    ( oyProfile_s       * profile,
+                                       oyProfileTag_s   ** obj,
+                                       int                 pos );
+int          oyProfile_TagReleaseAt_ ( oyProfile_s       * profile,
+                                       int                 pos );
+int                oyProfile_DeviceAdd(oyProfile_s       * profile,
+                                       oyConfig_s        * config );
+oyPointer    oyProfile_WriteHeader_  ( oyProfile_s       * profile,
+                                       size_t            * size );
+oyPointer    oyProfile_WriteTags_    ( oyProfile_s       * profile,
+                                       size_t            * size,
+                                       oyPointer           icc_header,
+                                       oyPointer           icc_list,
+                                       oyAlloc_f           allocateFunc );
+oyPointer    oyProfile_WriteTagTable_( oyProfile_s       * profile,
+                                       size_t            * size );
