8b79d36fc2dea775d024a0a0b6076af9c3129458
Yiannis Belias <jonnyb@hol.gr>
2010-08-10 15:27:09 +0300

    [templates] Remove from oyranos_devices_internal.* 2 functions.
    oyOptions_SetDeviceTextKey_() and oyOptions_SetRegistrationTextKey_()
    are defined in oyOptions_s_.h

--- templates/oyranos_devices_internal.template.h
+++ templates/oyranos_devices_internal.template.h
@@ -3,20 +3,11 @@
 #ifndef {{ file_name|underscores|upper|tr:". _" }}
 #define {{ file_name|underscores|upper|tr:". _" }}
 
+#include "oyOptions_s_.h"
+
 char * oyDeviceRegistrationCreate_   ( const char        * device_type,
                                        const char        * device_class,
                                        const char        * key,
                                        char              * old_text );
-int    oyOptions_SetDeviceTextKey_   ( oyOptions_s_      * options,
-                                       const char        * device_type,
-                                       const char        * device_class,
-                                       const char        * key,
-                                       const char        * value );
-int    oyOptions_SetRegistrationTextKey_(
-                                       oyOptions_s_      * options,
-                                       const char        * registration,
-                                       const char        * key,
-                                       const char        * value );
-
 
 #endif /* {{ file_name|underscores|upper|tr:". _" }} */
