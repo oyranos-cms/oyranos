e7b2e15f92315374a1f756ffcb11feca7b51903d
Yiannis Belias <jonnyb@hol.gr>
2010-08-10 19:08:44 +0300

    * Create skeleton files for oyProfile_s

---
+++ sources/Profile.members.h
@@ -0,0 +1,9 @@
+  char               * file_name_;     /*!< @private file name for loading on request */
+  size_t               size_;          /*!< @private ICC profile size */
+  void               * block_;         /*!< @private ICC profile data */
+  icColorSpaceSignature sig_;          /*!< @private ICC profile signature */
+  oyPROFILE_e          use_default_;   /*!< @private if > 0 : take from settings */
+  oyObject_s         * names_chan_;    /*!< @private user visible channel description */
+  int                  channels_n_;    /*!< @private number of channels */
+  oyStructList_s     * tags_;          /**< @private list of header + tags */
+  int                  tags_modified_; /**< @private note manipulation */
---
+++ sources/Profile.private_custom_definitions.c
@@ -0,0 +1,85 @@
+/** Function    oyProfile_Release__Members
+ *  @memberof   oyProfile_s
+ *  @brief      Custom Profile destructor
+ *  @internal
+ *
+ *  This function will free up all memmory allocated by the
+ *  input object. First all object members witch have their
+ *  own release method are deallocated. Then the deallocateFunc_
+ *  of the oy_ object is used to release the rest of the members
+ *  that were allocated with oy_->allocateFunc_.
+ *
+ *  @param[in]  profile  the Profile object
+ *
+ *  @version Oyranos: x.x.x
+ *  @since   YYYY/MM/DD (Oyranos: x.x.x)
+ *  @date    YYYY/MM/DD
+ */
+void oyProfile_Release__Members( oyProfile_s_ * profile )
+{
+  /* Deallocate members here
+   * E.g: oyXXX_Release( &profile->member );
+   */
+
+  if(profile->oy_->deallocateFunc_)
+  {
+    oyDeAlloc_f deallocateFunc = profile->oy_->deallocateFunc_;
+
+    /* Deallocate members of basic type here
+     * E.g.: deallocateFunc( profile->member );
+     */
+  }
+}
+
+/** Function    oyProfile_Init__Members
+ *  @memberof   oyProfile_s
+ *  @brief      Custom Profile constructor 
+ *  @internal
+ *
+ *  This function will allocate all memmory for the input object.
+ *  For the basic member types this is done using the allocateFunc_
+ *  of the attatced (oyObject_s)oy_ object.
+ *
+ *  @param[in]  profile  the Profile object
+ *
+ *  @version Oyranos: x.x.x
+ *  @since   YYYY/MM/DD (Oyranos: x.x.x)
+ *  @date    YYYY/MM/DD
+ */
+int oyProfile_Init__Members( oyProfile_s_ * profile )
+{
+  return 0;
+}
+
+/** Function    oyProfile_Copy__Members
+ *  @memberof   oyProfile_s
+ *  @brief      Custom Profile copy constructor
+ *  @internal
+ *
+ *  This function makes a copy of all values from the input
+ *  to the output object. The destination object and all of its
+ *  members should already be allocated.
+ *
+ *  @param[in]   src  the oyProfile_s_ input object
+ *  @param[out]  dst  the output oyProfile_s_ object
+ *
+ *  @version Oyranos: x.x.x
+ *  @since   YYYY/MM/DD (Oyranos: x.x.x)
+ *  @date    YYYY/MM/DD
+ */
+int oyProfile_Copy__Members( oyProfile_s_ * dst, oyProfile_s_ * src)
+{
+  int error = 0;
+  oyAlloc_f allocateFunc_ = 0;
+  oyDeAlloc_f deallocateFunc_ = 0;
+
+  if(!dst || !src)
+    return 1;
+
+  allocateFunc_ = dst->oy_->allocateFunc_;
+  deallocateFunc_ = dst->oy_->deallocateFunc_;
+
+  /* Copy each value of src to dst here */
+
+  return error;
+}
