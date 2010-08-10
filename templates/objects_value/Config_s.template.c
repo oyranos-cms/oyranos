baee3d07e95e1183055e8160e8d2e005d4b28b1b
Yiannis Belias <jonnyb@hol.gr>
2010-08-10 10:18:26 +0300

    [templates] Add include files to oyConfig_s.c

--- templates/objects_value/Config_s.template.c
+++ templates/objects_value/Config_s.template.c
@@ -1,1 +1,10 @@
 {% extends "Base_s.c" %}
+
+{% block LocalIncludeFiles %}
+{{ block.super }}
+#include "oyConfigs_s.h"
+#include "oyCMMapiFilters_s.h"
+
+#include "oyOption_s_.h"
+#include "oyCMMapi8_s_.h"
+{% endblock %}
