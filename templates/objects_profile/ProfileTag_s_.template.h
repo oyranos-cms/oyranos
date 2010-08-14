eb63349990c6a4c93f14d6f12f59cb33a17cd0ab
Yiannis Belias <jonnyb@hol.gr>
2010-08-14 20:55:39 +0300

    [templates] Add icc34.h to oyProfileTag_s_.h

--- templates/objects_profile/ProfileTag_s_.template.h
+++ templates/objects_profile/ProfileTag_s_.template.h
@@ -1,5 +1,10 @@
 {% extends "Base_s_.h" %}
 
+{%block LocalIncludeFiles %}
+{{ block.super }}
+#include <icc34.h>
+{% endblock %}
+
 {% block ChildMembers %}
 {{ block.super }}
 /* Include "ProfileTag.members.h" { */
