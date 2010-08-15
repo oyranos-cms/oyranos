fd382fbaebb179a84a9afd1e1e590eff151ca60b
Yiannis Belias <jonnyb@hol.gr>
2010-08-15 22:27:24 +0300

    [templates] Include icc34.h in oyProfile_s

--- templates/objects_profile/Profile_s.template.h
+++ templates/objects_profile/Profile_s.template.h
@@ -1,5 +1,10 @@
 {% extends "Base_s.h" %}
 
+{% block GlobalIncludeFiles %}
+{{ block.super }}
+#include <icc34.h>
+{% endblock %}
+
 {% block LocalIncludeFiles %}
 {{ block.super }}
 #include "oyranos_profile.h"
