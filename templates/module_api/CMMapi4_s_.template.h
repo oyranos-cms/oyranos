8d361f89b393d1013f5657fba5932c0e7c96c24c
Yiannis Belias <jonnyb@hol.gr>
2010-09-02 14:22:24 +0300

    [templates] Add include files to CMMapi4_s_.h

--- templates/module_api/CMMapi4_s_.template.h
+++ templates/module_api/CMMapi4_s_.template.h
@@ -1,5 +1,12 @@
 {% extends "CMMapiFilter_s_.template.h" %}
 
+{% block LocalIncludeFiles %}
+{{ block.super }}
+#include "oyCMMui_s.h"
+#include "oyCMMapi5_s.h"
+#include "oyFilterNode_s.h"
+{% endblock %}
+
 {% block ChildMembers %}
 {{ block.super }}
 /* Include "CMMapi4.members.h" { */
