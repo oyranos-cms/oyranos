a3c24f0b735e300caa926788fd1aee2437add464
Kai-Uwe Behrmann <ku.b@gmx.de>
2011-02-12 14:41:40 +0100

    * [test]: add compile test for liboyranos_object

---
+++ test_object.c
@@ -0,0 +1,20 @@
+#include "oyBlob_s.h"
+#include "oyOption_s.h"
+
+int main( int argc, char ** argv)
+{
+  oyBlob_s * blob = oyBlob_New(0);
+  oyOption_s * o;
+
+  if(!blob)
+    oyMessageFunc_p(oyMSG_WARN,0,"No oyBlob_s generated");
+
+  oyBlob_Release(&blob);
+
+  o = oyOption_FromRegistration("/sw/freedesktop.org/imaging/key",0);
+  oyOption_SetFromDouble( o, 0.25, 2, 0 );
+  oyOption_Release( &o );
+
+  return 0;
+}
+
