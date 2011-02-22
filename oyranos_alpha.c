16ec5d1bdee6b64d55fb5fab62a9880f96ae3e7f
Kai-Uwe Behrmann <ku.b@gmx.de>
2011-02-22 13:54:28 +0100

    * [alpha]: free unused memory in oyOptions_Handle

--- oyranos_alpha.c
+++ oyranos_alpha.c
@@ -22678,6 +22678,8 @@ int             oyOptions_Handle     ( const char        * registration,
     apis = oyCMMsGetFilterApis_( 0,0, api_reg, oyOBJECT_CMM_API10_S, 
                                  oyFILTER_REG_MODE_STRIP_IMPLEMENTATION_ATTR,
                                  &rank_list, &api_n );
+    if(rank_list) oyDeAllocateFunc_(rank_list); rank_list = 0;
+
     apis_n = oyCMMapiFilters_Count( apis );
     if(test)
       for(i = 0; i < apis_n; ++i)
