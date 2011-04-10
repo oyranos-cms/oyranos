0f3e1e8d0a9c8c53ea4e1dd3357ca6e88d72d1e0
Kai-Uwe Behrmann <ku.b@gmx.de>
2011-04-10 14:35:17 +0200

    * [exmpl]: add "--set" option to oymd5icc
    This allows to rewrite a ICC profile and thus set the Oyranos computed
    ICC profile ID. It will be used to fix the upcomming profile package release.
    
    The output is made somewhat similiar to the md5sum command.
    
    The return value comes from the oyProfile_GetMD5() function. In my
    environment 255 means -1: no internal ICC profile ID.

--- examples/oymd5icc/oymd5icc.c
+++ examples/oymd5icc/oymd5icc.c
@@ -2,25 +2,48 @@
 #include <oyranos_alpha.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 
 int main( int argc, char ** argv )
 {
   oyProfile_s * p = 0;
   uint32_t * i;
-  uint32_t md5[4];
+  uint32_t md5[4],
+           id[4];
+  int profile_pos = 1,
+      set_md5 = 0,
+      md5_good = 0,
+      error;
 
   if(argc == 1)
   {
-    printf("Usage: %s icc_profile.icc\n", argv[0]);
+    printf("Usage: %s [--set] icc_profile.icc\n", argv[0]);
     exit(0);
   }
 
-  p = oyProfile_FromFile( argv[1], 0,0 );
-  oyProfile_GetMD5(p, md5);
-  i = (uint32_t*)md5;
-  printf( "profile hash[based on md5]: %x%x%x%x\n", i[0], i[1], i[2], i[3] );
+  if(strcmp(argv[1],"--set") == 0)
+  {
+    set_md5 = 1;
+    ++profile_pos;
+  }
 
-  return 0;
+  p = oyProfile_FromFile( argv[profile_pos], 0,0 );
+  error = oyProfile_GetMD5(p, OY_COMPUTE, md5);
+  if(-1 <= error && error < 1)
+    md5_good = 1;
+
+  oyProfile_GetMD5(p, OY_FROM_PROFILE, id);
+
+  if(set_md5)
+    oyProfile_ToFile_( p, argv[profile_pos]);
+
+  i = (uint32_t*)md5;
+  if(p)
+    printf( "%x%x%x%x[%x%x%x%x] %s\n",
+            i[0],i[1],i[2],i[3], id[0],id[1],id[2],id[3], argv[profile_pos] );
+  else
+    fprintf(stderr, "not found: %s\n", argv[profile_pos] );
+
+  return error;
 }
