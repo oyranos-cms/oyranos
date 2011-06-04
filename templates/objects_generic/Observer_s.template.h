eb31e3832fe0cf3a2f08fbb08811d4e0b860e757
Yiannis Belias <jonnyb@hol.gr>
2011-06-04 13:40:01 +0300

    * [sources] Remove oyObserver_s public members from template

--- templates/objects_generic/Observer_s.template.h
+++ templates/objects_generic/Observer_s.template.h
@@ -57,21 +57,9 @@ typedef  int      (*oyObserver_Signal_f) (
 /* } Include "{{ class.dox }}" */
 {% endblock %}
 struct {{ class.name }} {
-  oyOBJECT_e           type_;          /**< @private internal struct type oyOBJECT_OBSERVER_S */
-  oyStruct_Copy_f      copy;           /**< copy function */
-  oyStruct_Release_f   release;        /**< release function */
-  oyPointer            dummy;          /**< keep to zero */
-
-  /** a reference to the observing object */
-  oyStruct_s         * observer;
-  /** a reference to the to be observed model */
-  oyStruct_s         * model;
-  /** optional data; If no other user data is available this data will be
-   *  passed with the signal. */
-  oyStruct_s         * user_data;
-  oyObserver_Signal_f  signal;         /**< observers signaling function */ 
-  int                  disable_ref;    /**< disable signals reference counter
-                                            == 0 -> enabled; otherwise not */
+/* Include "Observer.members.h" { */
+{% include "Observer.members.h" %}
+/* } Include "Observer.members.h" */
 };
 
 /* {{ class.name }} common object functions { */
