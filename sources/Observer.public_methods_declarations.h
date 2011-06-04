87dca64dc769938085064877a0782586b5612ac3
Yiannis Belias <jonnyb@hol.gr>
2011-06-04 14:19:30 +0300

    * [sources] Remove oyObserver_s public methods declarations from template

---
+++ sources/Observer.public_methods_declarations.h
@@ -0,0 +1,41 @@
+OYAPI int  OYEXPORT
+           oyObserver_SignalSend     ( oyObserver_s      * observer,
+                                       oySIGNAL_e          signal_type,
+                                       oyStruct_s        * user_data );
+OYAPI int  OYEXPORT
+           oyStruct_ObserverAdd      ( oyStruct_s        * model,
+                                       oyStruct_s        * observer,
+                                       oyStruct_s        * user_data,
+                                       oyObserver_Signal_f signalFunc );
+OYAPI int  OYEXPORT
+           oyStruct_ObserverRemove   ( oyStruct_s        * model,
+                                       oyStruct_s        * observer,
+                                       oyObserver_Signal_f signalFunc );
+OYAPI int  OYEXPORT
+           oyStruct_ObserverSignal   ( oyStruct_s        * model,
+                                       oySIGNAL_e          signal_type,
+                                       oyStruct_s        * signal_data );
+OYAPI int  OYEXPORT
+           oyStruct_DisableSignalSend( oyStruct_s        * model );
+OYAPI int  OYEXPORT
+           oyStruct_EnableSignalSend ( oyStruct_s        * model );
+OYAPI int  OYEXPORT
+           oyStruct_ObserversCopy    ( oyStruct_s        * object,
+                                       oyStruct_s        * pattern,
+                                       uint32_t            flags );
+OYAPI int  OYEXPORT
+           oyStruct_ObserverCopyModel( oyStruct_s        * model,
+                                       oyStruct_s        * pattern,
+                                       uint32_t            flags );
+OYAPI int  OYEXPORT
+           oyStruct_ObserverCopyObserver (
+                                       oyStruct_s        * observer,
+                                       oyStruct_s        * pattern,
+                                       uint32_t            flags );
+OYAPI int  OYEXPORT
+           oyStruct_IsObserved       ( oyStruct_s        * model,
+                                       oyStruct_s        * observer );
+OYAPI uint32_t OYEXPORT
+           oyObserverFlagsGet        ( void );
+OYAPI int  OYEXPORT
+           oyObserverFlagsSet        ( uint32_t            flags );
