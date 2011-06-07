9dc48b181c4108c334f8fb070ed1dc06ad6c5e5f
Yiannis Belias <jonnyb@hol.gr>
2011-06-07 13:52:32 +0300

    [sources] Import public methods for oyProfileTag_s

--- oyranos_alpha.h
+++ oyranos_alpha.h
@@ -904,49 +904,11 @@ struct oyProfileTag_s {
 OYAPI oyProfileTag_s * OYEXPORT
                oyProfileTag_New      ( oyObject_s          object );
 OYAPI oyProfileTag_s * OYEXPORT
-               oyProfileTag_Create   ( oyStructList_s    * list,
-                                       icTagTypeSignature  tag_type,
-                                       uint32_t            version,
-                                       const char        * required_cmm,
-                                       oyObject_s          object );
-OYAPI oyProfileTag_s * OYEXPORT
-               oyProfileTag_CreateFromText (
-                                       const char        * text,
-                                       icTagTypeSignature  tag_type,
-                                       icTagSignature      tag_usage,
-                                       oyObject_s          object );
-OYAPI oyProfileTag_s * OYEXPORT
-               oyProfileTag_CreateFromData ( 
-                                       icTagSignature      sig,
-                                       icTagTypeSignature  type,
-                                       oySTATUS_e          status,
-                                       size_t              tag_size,
-                                       oyPointer           tag_block,
-                                       oyObject_s          object );
-OYAPI oyProfileTag_s * OYEXPORT
                oyProfileTag_Copy     ( oyProfileTag_s    * obj,
                                        oyObject_s          object);
 OYAPI int  OYEXPORT
                oyProfileTag_Release  ( oyProfileTag_s   ** obj );
 
-OYAPI int  OYEXPORT
-               oyProfileTag_Set      ( oyProfileTag_s    * tag,
-                                       icTagSignature      sig,
-                                       icTagTypeSignature  type,
-                                       oySTATUS_e          status,
-                                       size_t              tag_size,
-                                       oyPointer           tag_block );
-char   **      oyProfileTag_GetText  ( oyProfileTag_s    * tag,
-                                       int32_t           * n,
-                                       const char        * language,
-                                       const char        * country,
-                                       int32_t           * tag_size,
-                                       oyAlloc_f           allocateFunc );
-oyStructList_s*oyProfileTag_Get      ( oyProfileTag_s    * tag );
-int            oyProfileTag_GetBlock ( oyProfileTag_s    * tag,
-                                       oyPointer         * tag_block,
-                                       size_t            * tag_size,
-                                       oyAlloc_f           allocateFunc );
 
 OYAPI int  OYEXPORT
            oyOptions_SetDriverContext( oyOptions_s      ** options,
