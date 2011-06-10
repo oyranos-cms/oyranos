0f62b8fa7e1a256b9556164bc6011572b5a35b4d
Yiannis Belias <jonnyb@hol.gr>
2011-06-10 14:54:42 +0300

    * [templates] Create new file oyranos_generic.template.c

---
+++ templates/oyranos_generic.template.c
@@ -0,0 +1,10 @@
+{% include "source_file_header.txt" %}
+
+#include "oyranos_generic.h"
+
+/** Public function definitions { */
+/** } Public function definitions */
+
+/** Private function definitions { */
+/** } Private function definitions */
+
