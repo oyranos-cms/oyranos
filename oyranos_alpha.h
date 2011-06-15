60c9480874e55b6b25443064cd976c61092465bc
Yiannis Belias <jonnyb@hol.gr>
2011-06-15 12:22:10 +0300

    * [alpha] Remove oyStructList*() from oyranos_alpha.h
    They've been imported to the new tempalte system.

--- oyranos_alpha.h
+++ oyranos_alpha.h
@@ -205,59 +205,7 @@ int          oyName_boolean          ( oyName_s          * name_a,
 
 #define OY_HASH_SIZE 16
 
-
-
-oyStructList_s * oyStructList_New    ( oyObject_s          object );
-oyStructList_s * oyStructList_Copy   ( oyStructList_s    * list,
-                                       oyObject_s          obj );
-int              oyStructList_Release( oyStructList_s   ** list );
-
 #define OY_OBSERVE_AS_WELL 0x01
-int              oyStructList_MoveIn ( oyStructList_s    * list,
-                                       oyStruct_s       ** ptr,
-                                       int                 pos,
-                                       uint32_t            flags );
-/*oyStruct_s **    oyStructList_GetRaw_( oyStructList_s    * list );*/
-oyStruct_s *     oyStructList_Get_   ( oyStructList_s    * list,
-                                       int                 pos );
-oyStruct_s *     oyStructList_GetRef ( oyStructList_s    * list,
-                                       int                 pos );
-oyStruct_s *     oyStructList_GetRefType( oyStructList_s * list,
-                                       int                 pos,
-                                       oyOBJECT_e          type );
-int              oyStructList_ReferenceAt_(oyStructList_s* list,
-                                       int                 pos );
-int              oyStructList_ReleaseAt( oyStructList_s  * list,
-                                       int                 pos );
-int              oyStructList_Count  ( oyStructList_s    * list );
-const char *     oyStructList_GetText( oyStructList_s    * s,
-                                       oyNAME_e            name_type,
-                                       int                 intent_spaces,
-                                       uint32_t            flags );
-const char *     oyStructList_GetID  ( oyStructList_s    * s,
-                                       int                 intent_spaces,
-                                       uint32_t            flags );
-int              oyStructList_Clear  ( oyStructList_s    * s );
-int              oyStructList_CopyFrom(oyStructList_s    * list,
-                                       oyStructList_s    * from,
-                                       oyObject_s          object );
-int              oyStructList_MoveTo ( oyStructList_s    * s,
-                                       int                 pos,
-                                       int                 new_pos );
-int              oyStructList_Sort   ( oyStructList_s    * s,
-                                       int32_t           * rank_map );
-int              oyStructList_ObserverAdd (
-                                       oyStructList_s    * list,
-                                       oyStruct_s        * observer,
-                                       oyStruct_s        * user_data,
-                                       oyObserver_Signal_f signalFunc );
-int              oyStructList_MoveInName (
-                                       oyStructList_s    * texts,
-                                       char             ** text,
-                                       int                 pos );
-int              oyStructList_AddName( oyStructList_s    * texts,
-                                       const char        * text,
-                                       int pos );
 
 oyHash_s *   oyCMMCacheListGetEntry_ ( const char        * hash_text );
 oyStructList_s** oyCMMCacheList_     ( void );
