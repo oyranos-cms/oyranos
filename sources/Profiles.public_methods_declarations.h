99f0e2ab5e4117213444d71a9c93276549fb58ec
Yiannis Belias <jonnyb@hol.gr>
2011-06-17 16:21:48 +0300

    * [sources] Remove duplicate oyProfiles_s functions
    They are added by the templates automaticly for every list object.

--- sources/Profiles.public_methods_declarations.h
+++ sources/Profiles.public_methods_declarations.h
@@ -5,14 +5,6 @@ OYAPI oyProfiles_s * OYEXPORT
                  oyProfiles_ForStd   ( oyPROFILE_e         std_profile_class,
                                        int               * current,
                                        oyObject_s          object);
-oyProfiles_s   * oyProfiles_MoveIn   ( oyProfiles_s      * list,
-                                       oyProfile_s      ** obj,
-                                       int                 pos );
-int              oyProfiles_ReleaseAt( oyProfiles_s      * list,
-                                       int                 pos );
-oyProfile_s *    oyProfiles_Get      ( oyProfiles_s      * list,
-                                       int                 pos );
-int              oyProfiles_Count    ( oyProfiles_s      * list );
 int              oyProfiles_DeviceRank ( oyProfiles_s    * list,
                                          oyConfig_s      * device,
                                          int32_t         * rank_list );
