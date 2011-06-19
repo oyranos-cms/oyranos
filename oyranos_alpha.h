acbde01e23ec3f2adb882ac273c0799222db41d7
Yiannis Belias <jonnyb@hol.gr>
2011-06-19 21:36:34 +0300

    * [alpha] Remove old function declarations

--- oyranos_alpha.h
+++ oyranos_alpha.h
@@ -596,50 +596,6 @@ OYAPI int  OYEXPORT
                                        const char        * driver_context_type,
                                        size_t              driver_context_size,
                                        oyObject_s          object);
-OYAPI int  OYEXPORT
-           oyDevicesGet              ( const char        * device_type,
-                                       const char        * device_class,
-                                       oyOptions_s       * options,
-                                       oyConfigs_s      ** devices );
-OYAPI int  OYEXPORT
-           oyDeviceGet               ( const char        * device_type,
-                                       const char        * device_class,
-                                       const char        * device_name,
-                                       oyOptions_s       * options,
-                                       oyConfig_s       ** device );
-OYAPI int  OYEXPORT
-           oyDeviceSetup             ( oyConfig_s        * device );
-OYAPI int  OYEXPORT
-           oyDeviceUnset             ( oyConfig_s        * device );
-OYAPI int  OYEXPORT
-           oyDeviceGetInfo           ( oyConfig_s        * device,
-                                       oyNAME_e            type,
-                                       oyOptions_s       * options,
-                                       char             ** info_text,
-                                       oyAlloc_f           allocateFunc );
-OYAPI int  OYEXPORT
-           oyDeviceBackendCall       ( oyConfig_s        * device,
-                                       oyOptions_s       * options );
-OYAPI int  OYEXPORT
-           oyDeviceSetProfile        ( oyConfig_s        * device,
-                                       const char        * profile_name );
-OYAPI int  OYEXPORT
-           oyDeviceGetProfile        ( oyConfig_s        * device,
-                                       oyOptions_s       * options,
-                                       oyProfile_s      ** profile );
-OYAPI int  OYEXPORT
-           oyDeviceAskProfile2       ( oyConfig_s        * device,
-                                       oyOptions_s       * options,
-                                       oyProfile_s      ** profile );
-OYAPI int  OYEXPORT
-           oyDeviceProfileFromDB     ( oyConfig_s        * device,
-                                       char             ** profile_name,
-                                       oyAlloc_f           allocateFunc );
-OYAPI int OYEXPORT oyDeviceSelectSimiliar
-                                     ( oyConfig_s        * pattern,
-                                       oyConfigs_s       * heap,
-                                       uint32_t            flags,
-                                       oyConfigs_s      ** matched_devices );
 
 
 /** @enum    oyDATALAYOUT_e
