bb8d02dd928f618f2015141036f968e48496fdf9
Yiannis Belias <jonnyb@hol.gr>
2011-06-21 14:17:27 +0300

    * [revert] Hide CMMs

--- templates/objects_value/Config_s.template.c
+++ templates/objects_value/Config_s.template.c
@@ -8,8 +8,12 @@
 {% block LocalIncludeFiles %}
 {{ block.super }}
 #include "oyConfigs_s.h"
+#ifdef UNHIDE_CMM
 #include "oyCMMapiFilters_s.h"
+#endif
 
 #include "oyOption_s_.h"
+#ifdef UNHIDE_CMM
 #include "oyCMMapi8_s_.h"
+#endif
 {% endblock %}
