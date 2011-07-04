9b396e7207262f527bb9a90e62204246e54f8762
Yiannis Belias <jonnyb@hol.gr>
2011-07-04 20:39:17 +0300

    * [alpha] Remove stale declarations for
    oyOption_SetFromText()
    oyOption_GetValueText()
    oyOption_SetFromInt()
    oyOption_GetValueInt()
    oyOption_SetFromDouble()
    oyOption_GetValueDouble()
    oyOption_SetFromData()
    oyOption_SetRegistration()
    oyOption_StructGet()
    oyOption_StructMoveIn()
    oyOption_Copy()
    oyOption_Release()
    
    They've been imported to the new tempalte system.

--- oyranos_alpha.h
+++ oyranos_alpha.h
@@ -239,42 +239,12 @@ int          oyRegistrationEraseFromDB(const char        * registration );
 
 oyOption_s *   oyOption_FromDB       ( const char        * registration,
                                        oyObject_s          object );
-oyOption_s *   oyOption_Copy         ( oyOption_s        * option,
-                                       oyObject_s          object );
-int            oyOption_Release      ( oyOption_s       ** option );
 
 
 #define OY_STRING_LIST                 0x01 /** create a oyVAL_STRING_LIST */
 /* decode */
 #define oyToStringList_m(r)            ((r)&1)
-int            oyOption_SetFromText  ( oyOption_s        * obj,
-                                       const char        * text, 
-                                       uint32_t            flags );
-char *         oyOption_GetValueText ( oyOption_s        * obj,
-                                       oyAlloc_f           allocateFunc );
-int            oyOption_SetFromInt   ( oyOption_s        * obj,
-                                       int32_t             integer,
-                                       int                 pos,
-                                       uint32_t            flags );
-int32_t        oyOption_GetValueInt  ( oyOption_s        * obj,
-                                       int                 pos );
-int            oyOption_SetFromDouble( oyOption_s        * obj,
-                                       double              floating_point,
-                                       int                 pos,
-                                       uint32_t            flags );
-double         oyOption_GetValueDouble(oyOption_s        * obj,
-                                       int                 pos );
-int            oyOption_SetFromData  ( oyOption_s        * option,
-                                       oyPointer           ptr,
-                                       size_t              size );
-int            oyOption_SetRegistration (
-                                       oyOption_s        * option,
-                                       const char        * registration );
 int            oyOption_SetValueFromDB(oyOption_s        * option );
-int            oyOption_StructMoveIn ( oyOption_s        * option,
-                                       oyStruct_s       ** s );
-oyStruct_s *   oyOption_StructGet    ( oyOption_s        * option,
-                                       oyOBJECT_e          type );
 
 
 oyOptions_s *  oyOptions_FromBoolean ( oyOptions_s       * pattern,
