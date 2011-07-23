17a55a7a7f9a2e9e7e3b59716907a918c6ef7b5e
Yiannis Belias <jonnyb@hol.gr>
2011-07-24 02:19:59 +0300

    * [sources] Import private methods for oyFilterCore_s

--- sources_weg/FilterCore.private_methods_declarations.h
+++ sources_weg/FilterCore.private_methods_declarations.h
@@ -1,3 +1,5 @@
 oyOptions_s *  oyOptions_ForFilter_  ( oyFilterCore_s    * filter,
                                        uint32_t            flags,
                                        oyObject_s          object );
+int          oyFilterCore_SetCMMapi4_( oyFilterCore_s    * s,
+                                       oyCMMapi4_s       * cmm_api4 );
--- sources_weg/FilterCore.private_methods_definitions.c
+++ sources_weg/FilterCore.private_methods_definitions.c
@@ -134,3 +134,54 @@ oyOptions_s *  oyOptions_ForFilter_  ( oyFilterCore_s    * filter,
   return s;
 }
 
+/**
+ *  @internal
+ *  Function oyFilterCore_SetCMMapi4_
+ *  @memberof oyFilterCore_s
+ *  @brief   lookup and initialise a new filter object
+ *
+ *  @version Oyranos: 0.1.10
+ *  @since   2008/11/27 (Oyranos: 0.1.9)
+ *  @date    2009/02/28
+ */
+int          oyFilterCore_SetCMMapi4_( oyFilterCore_s    * s,
+                                       oyCMMapi4_s       * cmm_api4 )
+{
+  int error = !s;
+  oyAlloc_f allocateFunc_ = 0;
+  static const char * lang = 0;
+  int update = 1;
+
+  if(error <= 0)
+    allocateFunc_ = s->oy_->allocateFunc_;
+
+  error = !(cmm_api4 && cmm_api4->type == oyOBJECT_CMM_API4_S);
+
+  if(error <= 0)
+  {
+    s->registration_ = oyStringCopy_( cmm_api4->registration,
+                                      allocateFunc_);
+
+    s->category_ = oyStringCopy_( cmm_api4->ui->category, allocateFunc_ );
+
+    /* we lock here as cmm_api4->oyCMMuiGet might not be thread save */
+    {
+      if(!lang)
+        lang = oyLanguage();
+
+      oyObject_Lock( s->oy_, __FILE__, __LINE__ );
+      if(lang &&
+         oyStrcmp_( oyNoEmptyName_m_(oyLanguage()), lang ) == 0)
+        update = 0;
+
+      oyObject_UnLock( s->oy_, __FILE__, __LINE__ );
+    }
+
+    s->api4_ = cmm_api4;
+  }
+
+  if(error && s)
+    oyFilterCore_Release( &s );
+
+  return error;
+}
