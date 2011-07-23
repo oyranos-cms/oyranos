fe346b81b61d0b6e73dea707c7447ee25f3c4ad2
Yiannis Belias <jonnyb@hol.gr>
2011-07-23 22:00:32 +0300

    * [sources] Fix oyFilterCore_s members

--- sources_weg/FilterCore.members.h
+++ sources_weg/FilterCore.members.h
@@ -1,11 +1,4 @@
-   char               * registration_;  /**< @private a registration name, e.g. "shared/oyranos.org/imaging/scale", see as well @ref registration */
-
-  char               * category_;      /**< @private the ui menue category for this filter, to be specified */
-
-  oyOptions_s        * options_;       /**< @private local options */
-
-  oyCMMapi4_s        * api4_;          /**< @private oyranos library interfaces */
- char               * registration_;  /**< @private a registration name, e.g. "shared/oyranos.org/imaging/scale", see as well @ref registration */
+  char               * registration_;  /**< @private a registration name, e.g. "shared/oyranos.org/imaging/scale", see as well @ref registration */
 
   char               * category_;      /**< @private the ui menue category for this filter, to be specified */
 
