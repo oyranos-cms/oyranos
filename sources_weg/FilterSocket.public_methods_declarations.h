61e90463d4e42ecf1b06091e4d388b40e731174b
Yiannis Belias <jonnyb@hol.gr>
2011-08-04 13:51:31 +0300

    * [sources] Import public methods for oyFilterSocket_s

---
+++ sources_weg/FilterSocket.public_methods_declarations.h
@@ -0,0 +1,8 @@
+OYAPI int  OYEXPORT
+                 oyFilterSocket_Callback (
+                                       oyFilterPlug_s    * c,
+                                       oyCONNECTOR_EVENT_e e );
+OYAPI int  OYEXPORT
+                 oyFilterSocket_SignalToGraph (
+                                       oyFilterSocket_s  * c,
+                                       oyCONNECTOR_EVENT_e e );
---
+++ sources_weg/FilterSocket.public_methods_definitions.c
@@ -0,0 +1,147 @@
+/** Function oyFilterSocket_Callback
+ *  @memberof oyFilterSocket_s
+ *  @brief   tell about a oyConversion_s event
+ *
+ *  @param[in,out] c                   the connector
+ *  @param         e                   the event type
+ *  @return                            0 on success, else error
+ *
+ *  @version Oyranos: 0.1.8
+ *  @since   2008/07/28 (Oyranos: 0.1.8)
+ *  @date    2009/02/19
+ */
+OYAPI int  OYEXPORT
+                 oyFilterSocket_Callback(
+                                       oyFilterPlug_s    * c,
+                                       oyCONNECTOR_EVENT_e e )
+{
+  int n, i;
+  oyFilterSocket_s * s;
+  oyFilterPlug_s * p;
+
+  if(e != oyCONNECTOR_EVENT_OK && oy_debug_signals)
+  {
+    WARNc5_S("\n  oyFilterNode_s[%d]->oyFilterSocket_s[%d]\n"
+             "  event: \"%s\" plug[%d/node%d]",
+            (c && c->remote_socket_ && c->remote_socket_->node) ?
+                   oyObject_GetId(c->remote_socket_->node->oy_) : -1,
+            (c && c->remote_socket_) ? oyObject_GetId(c->remote_socket_->oy_)
+                                     : -1,
+            oyConnectorEventToText(e),
+            c ? oyObject_GetId( c->oy_ ) : -1,
+            c ? (c->node ? oyObject_GetId( c->node->oy_ ) : -1) : -1
+          );
+  }
+
+  if(!c)
+    return 1;
+
+  s = c->remote_socket_;
+
+  if(!s)
+    return 0;
+
+  n = oyFilterPlugs_Count( s->requesting_plugs_ );
+
+  if(e == oyCONNECTOR_EVENT_RELEASED)
+    for(i = 0; i < n; ++i)
+    {
+      p = oyFilterPlugs_Get( s->requesting_plugs_, i );
+      if(p == c)
+      {
+        oyFilterPlugs_ReleaseAt( s->requesting_plugs_, i );
+        break;
+      }
+    }
+
+  return 0;
+}
+
+/** Function oyFilterSocket_SignalToGraph
+ *  @memberof oyFilterSocket_s
+ *  @brief   send a signal through the graph
+ *
+ *  The traversal direction is defined as from the starting node to the output.
+ *
+ *  @return                            1 if handled or zero
+ *
+ *  @version Oyranos: 0.3.2
+ *  @since   2009/10/27 (Oyranos: 0.1.10)
+ *  @date    2011/07/10
+ */
+OYAPI int  OYEXPORT
+                 oyFilterSocket_SignalToGraph (
+                                       oyFilterSocket_s  * c,
+                                       oyCONNECTOR_EVENT_e e )
+{
+  oySIGNAL_e sig = oySIGNAL_OK;
+  int n, i, j_n,j, k,k_n, handled = 0, pos;
+  oyFilterPlug_s * p, * sp;
+  oyFilterGraph_s * graph = 0;
+
+  switch(e)
+  {
+    case oyCONNECTOR_EVENT_OK:                /**< kind of ping */
+    case oyCONNECTOR_EVENT_CONNECTED:         /**< connection established */
+    case oyCONNECTOR_EVENT_RELEASED:          /**< released the connection */
+    case oyCONNECTOR_EVENT_DATA_CHANGED:      /**< call to update image views */
+    case oyCONNECTOR_EVENT_STORAGE_CHANGED:   /**< new data accessors */
+    case oyCONNECTOR_EVENT_INCOMPATIBLE_DATA: /**< can not process image */
+    case oyCONNECTOR_EVENT_INCOMPATIBLE_OPTION:/**< can not handle option */
+    case oyCONNECTOR_EVENT_INCOMPATIBLE_CONTEXT:/**< can not handle profile */
+    case oyCONNECTOR_EVENT_INCOMPLETE_GRAPH:  /**< can not completely process */
+         sig = (oySIGNAL_e) e; break;
+  }
+
+  handled = oyStruct_ObserverSignal( (oyStruct_s*) c->node, sig, 0 );
+  if(handled)
+    return handled;
+
+  if(e != oyCONNECTOR_EVENT_OK && oy_debug_signals)
+  {
+    WARNc4_S("oyFilterNode_s[%d]->oyFilterSocket_s[%d]\n"
+             "  event: \"%s\" socket[%d]",
+            (c && c->node) ? oyObject_GetId(c->node->oy_) : -1,
+            c ? oyObject_GetId(c->oy_) : -1,
+            oyConnectorEventToText(e),
+            (c) ? oyObject_GetId( c->oy_ ) : -1
+          );
+  }
+
+  n = oyFilterPlugs_Count( c->requesting_plugs_ );
+
+  for(i = 0; i < n; ++i)
+  {
+    p = oyFilterPlugs_Get( c->requesting_plugs_, i );
+    handled = oyStruct_ObserverSignal( (oyStruct_s*) p->node, sig, 0 );
+
+    /* get all nodes in the output direction */
+    graph = oyFilterGraph_FromNode( p->node, OY_INPUT );
+
+    j_n = oyFilterNodes_Count( graph->nodes );
+    for( j = 0; j < j_n; ++j )
+    {
+      oyFilterNode_s * node = oyFilterNodes_Get( graph->nodes, j );
+
+      /* iterate over all node outputs */
+      k_n = oyFilterNode_EdgeCount( node, 1, OY_FILTEREDGE_CONNECTED );
+      for(k = 0; k < k_n; ++k)
+      {
+        pos = oyFilterNode_GetConnectorPos( node, 1, "///", k,
+                                            OY_FILTEREDGE_CONNECTED );
+        sp = oyFilterNode_GetPlug( node, pos );
+        oyFilterSocket_Callback( sp, e );
+      }
+
+      oyFilterNode_Release( &node );
+      if(handled)
+        break;
+    }
+
+    oyFilterGraph_Release( &graph );
+    if(handled)
+      break;
+  }
+
+  return handled;
+}
