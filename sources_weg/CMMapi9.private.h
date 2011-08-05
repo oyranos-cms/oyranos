1dbfeeb7411764e380af6580a1be3b38529c7f36
Yiannis Belias <jonnyb@hol.gr>
2011-08-05 16:34:07 +0300

    * [sources] Update CMMapi9 from master.

--- sources_weg/CMMapi9.private.h
+++ sources_weg/CMMapi9.private.h
@@ -1,5 +1,5 @@
 /** typedef  oyConversion_Correct_f
- *  @brief   check for correctly adhering to policies
+ *  @brief   Check for correctly adhering to policies
  *  @ingroup module_api
  *  @memberof oyCMMapi9_s
  *
@@ -13,14 +13,21 @@
  *  - "verbose" - reporting changes as message
  *
  *  @param   conversion                the to be checked configuration
+ *  @param[in]     flags               for inbuild defaults |
+ *                                     oyOPTIONSOURCE_FILTER;
+ *                                     for options marked as advanced |
+ *                                     oyOPTIONATTRIBUTE_ADVANCED |
+ *                                     OY_SELECT_FILTER |
+ *                                     OY_SELECT_COMMON
  *  @param   options                   options to the policy module
  *  @return                            0 - indifferent, >= 1 - error
  *                                     + a message should be sent
  *
- *  @version Oyranos: 0.1.10
+ *  @version Oyranos: 0.1.13
  *  @since   2009/07/23 (Oyranos: 0.1.10)
- *  @date    2009/07/23
+ *  @date    2010/11/27
  */
 typedef int  (*oyConversion_Correct_f) (
                                        oyConversion_s    * conversion,
+                                       uint32_t            flags,
                                        oyOptions_s       * options );
