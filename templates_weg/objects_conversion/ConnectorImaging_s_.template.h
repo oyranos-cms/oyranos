878c10e2cc3c7fc361b525f4f9e8452f921b0fb9
Yiannis Belias <jonnyb@hol.gr>
2011-08-05 22:08:25 +0300

    * [templates] Add include files to Connector_s_.h

--- templates_weg/objects_conversion/ConnectorImaging_s_.template.h
+++ templates_weg/objects_conversion/ConnectorImaging_s_.template.h
@@ -1,5 +1,11 @@
 {% extends "Connector_s_.template.h" %}
 
+{% block LocalIncludeFiles %}
+{{ block.super }}
+#include "oyranos_module.h"
+#include "oyCMMapi5_s_.h"
+{% endblock %}
+
 {% block ChildMembers %}
 {{ block.super }}
 /* Include "ConnectorImaging.members.h" { */
