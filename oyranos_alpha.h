eccf6bca2fe4fc80d9bb27e2fcc04ceec92266cb
Yiannis Belias <jonnyb@hol.gr>
2011-08-06 16:40:35 +0300

    * [templates] Move oyDATATYPE_e to oyranos_image.h

--- oyranos_alpha.h
+++ oyranos_alpha.h
@@ -414,23 +414,6 @@ int            oyRectangle_SamplesFromImage (
 const char *   oyRectangle_Show      ( oyRectangle_s     * rectangle );
 
 
-/** @enum    oyDATATYPE_e
- *  @brief   basic data types for anonymous pointers
- *  @ingroup objects_image
- *
- *  @version Oyranos: 0.1.8
- *  @since   2007/00/00 (Oyranos: 0.1.8)
- *  @date    2007/00/00
- */
-typedef enum oyDATATYPE_e {
-  oyUINT8,     /*!<  8-bit integer */
-  oyUINT16,    /*!< 16-bit integer */
-  oyUINT32,    /*!< 32-bit integer */
-  oyHALF,      /*!< 16-bit floating point number */
-  oyFLOAT,     /*!< IEEE floating point number */
-  oyDOUBLE     /*!< IEEE double precission floating point number */
-} oyDATATYPE_e;
-
 int      oySizeofDatatype            ( oyDATATYPE_e        t );
 
 
--- templates/oyranos_image.template.h
+++ templates/oyranos_image.template.h
@@ -6,6 +6,23 @@
 
 {% include "cpp_begin.h" %}
 
+/** @enum    oyDATATYPE_e
+ *  @brief   basic data types for anonymous pointers
+ *  @ingroup objects_image
+ *
+ *  @version Oyranos: 0.1.8
+ *  @since   2007/00/00 (Oyranos: 0.1.8)
+ *  @date    2007/00/00
+ */
+typedef enum oyDATATYPE_e {
+  oyUINT8,     /*!<  8-bit integer */
+  oyUINT16,    /*!< 16-bit integer */
+  oyUINT32,    /*!< 32-bit integer */
+  oyHALF,      /*!< 16-bit floating point number */
+  oyFLOAT,     /*!< IEEE floating point number */
+  oyDOUBLE     /*!< IEEE double precission floating point number */
+} oyDATATYPE_e;
+
 {% include "cpp_end.h" %}
 
 #endif /* {{ file_name|underscores|upper|tr:". _" }} */
