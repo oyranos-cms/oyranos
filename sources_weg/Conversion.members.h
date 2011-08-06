d33fd46884c5a6bd31a8d6f64c614ae5236e4d8a
Yiannis Belias <jonnyb@hol.gr>
2011-08-06 21:24:07 +0300

    * Create skeleton files for oyConversion_s

---
+++ sources_weg/Conversion.members.h
@@ -0,0 +1,2 @@
+  oyFilterNode_s     * input;          /**< the input image filter; Most users will start logically with this pice and chain their filters to get the final result. */
+  oyFilterNode_s     * out_;           /**< @private the Oyranos output image. Oyranos will stream the filters starting from the end. This element will be asked on its first plug. */
