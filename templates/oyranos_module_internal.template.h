8a2861018309c9629b51c618e347afecd25588c6
Yiannis Belias <jonnyb@hol.gr>
2011-08-18 23:57:18 +0300

    Revert "* [templates]: deactiave Filter function"
    This reverts commit f411368666f410e98817164dad1587bfeafe17f3.
    
    The idea was to let oyCMMsGetFilterApis_ and its usage only appear in
    liboyranos_modules.so and higher abstraction. It can be resolved later.

--- templates/oyranos_module_internal.template.h
+++ templates/oyranos_module_internal.template.h
@@ -4,11 +4,11 @@
 #define {{ file_name|underscores|upper|tr:". _" }}
 
 
-/*oyCMMapiFilters_s * oyCMMsGetFilterApis_(const char        * cmm_required,
+oyCMMapiFilters_s * oyCMMsGetFilterApis_(const char        * cmm_required,
                                          const char        * registration,
                                          oyOBJECT_e          type,
                                          uint32_t         ** rank_list,
                                          uint32_t          * count );
-*/
+
 
 #endif /* {{ file_name|underscores|upper|tr:". _" }} */
