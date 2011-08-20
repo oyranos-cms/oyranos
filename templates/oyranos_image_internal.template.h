697b9997907c805c847ea383fdd22b8a2cc3a2e4
Yiannis Belias <jonnyb@hol.gr>
2011-08-20 08:42:46 +0300

    * [templates] Create new oyranos_image_internal.h file

---
+++ templates/oyranos_image_internal.template.h
@@ -0,0 +1,14 @@
+{% load gsoc_extensions %}
+{% include "source_file_header.txt" %}
+
+#ifndef {{ file_name|underscores|upper|tr:". _" }}
+#define {{ file_name|underscores|upper|tr:". _" }}
+
+{% include "cpp_begin.h" %}
+
+#include "oyranos_image.h"
+
+
+{% include "cpp_end.h" %}
+
+#endif /* {{ file_name|underscores|upper|tr:". _" }} */
