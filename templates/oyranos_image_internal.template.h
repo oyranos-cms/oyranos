85ca105a77a1acbc2d040614eeb74b288243e562
Yiannis Belias <jonnyb@hol.gr>
2011-08-20 08:49:47 +0300

    * [templates] Import pixel mask description to oyranos_image_internal.h

--- templates/oyranos_image_internal.template.h
+++ templates/oyranos_image_internal.template.h
@@ -9,6 +9,20 @@
 #include "oyranos_image.h"
 
 
+/** @internal
+ *  pixel mask description
+ */
+enum {
+  oyLAYOUT = 0, /* remembering the layout */
+  oyPOFF_X,     /* pixel count x offset */
+  oyPOFF_Y,     /* pixel count y offset */
+  oyCOFF,       /* channel offset */
+  oyDATA_SIZE,  /* sample size in byte */
+  oyCHANS,      /* number of channels */
+  oyCHAN0       /* first colour channel */
+};
+
+
 {% include "cpp_end.h" %}
 
 #endif /* {{ file_name|underscores|upper|tr:". _" }} */
