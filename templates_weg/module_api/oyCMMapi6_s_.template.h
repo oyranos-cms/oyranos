08b46d34b02cd0d846f9c5a07ec3318cc575bb2e
Yiannis Belias <jonnyb@hol.gr>
2011-08-21 11:17:09 +0300

    * [templates] Add include files to oyCMMapi6_s_.h

--- templates_weg/module_api/oyCMMapi6_s_.template.h
+++ templates_weg/module_api/oyCMMapi6_s_.template.h
@@ -1,5 +1,12 @@
 {% extends "oyCMMapiFilter_s_.template.h" %}
 
+{% block LocalIncludeFiles %}
+{{ block.super }}
+#include "oyPointer_s.h"
+#include "oyFilterNode_s.h"
+#include "oyCMMapi6_s_.h"
+{% endblock %}
+
 {% block ChildMembers %}
 {{ block.super }}
 /* Include "CMMapi6.members.h" { */
