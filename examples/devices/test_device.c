93d511c0fea9efa499f7f1e088a572d31d6c1593
Kai-Uwe Behrmann <ku.b@gmx.de>
2012-04-08 16:04:29 +0200

    * [exmpl]: list per device profiles
    ... with
    $ test-device --list-profiles device_class device_name

--- examples/devices/test_device.c
+++ examples/devices/test_device.c
@@ -179,7 +179,6 @@ int main(int argc, char *argv[])
             device_class, device_name, profile_name, error?"wrong":"good",
             tmp?"\n has already a profile: ":"", tmp?tmp:"" );
 
-
     if(!oy_device)
       exit(1);
 
@@ -189,6 +188,131 @@ int main(int argc, char *argv[])
     else
       error = oyDeviceUnset( oy_device );
 
+
+    oyConfig_Release( &oy_device );
+    exit(0);
+
+  } else
+  if(argc >= 3 && strcmp(argv[1], "--list-profiles" ) == 0)
+  {
+    oyConfig_s * oy_device = 0;
+    oyProfile_s * profile = 0;
+    const char * tmp = 0;
+    icSignature profile_class;
+    oyOptions_s * options = 0;
+    oyOptions_SetFromText( &options,
+                   "//"OY_TYPE_STD"/config/icc_profile.x_color_region_target",
+                         "yes", OY_CREATE_NEW );
+
+    device_class = argv[2];
+    device_name = argv[3];
+
+    {
+      oyConfDomain_s * d = oyConfDomain_FromReg( device_class, 0 );
+      const char * icc_profile_class = oyConfDomain_GetText( d,
+                                             "icc_profile_class", oyNAME_NICK );
+      if(icc_profile_class && strcmp(icc_profile_class,"display") == 0)
+        profile_class = icSigDisplayClass;
+      else if(icc_profile_class && strcmp(icc_profile_class,"output") == 0)
+        profile_class = icSigOutputClass;
+      else if(icc_profile_class && strcmp(icc_profile_class,"input") == 0)
+        profile_class = icSigInputClass;
+
+       printf("icc_profile_class: %s\n", icc_profile_class );
+       oyConfDomain_Release( &d );
+    }
+
+    error = oyDeviceGet( 0, device_class, device_name, 0, &oy_device );
+    if(oy_device)
+      error = oyDeviceGetProfile( oy_device, options, &profile );
+
+    if(profile)
+      tmp = oyProfile_GetFileName( profile, -1 );
+
+    printf( "%s %s %s%s%s\n",
+            device_class, device_name, error?"":"good",
+            tmp?"\n has a profile: ":"", tmp?tmp:"" );
+
+    if(!oy_device)
+      exit(1);
+
+    {
+    int size, i, current = -1, current_tmp = 0, pos = 0;
+    oyProfile_s * profile = 0, * temp_profile = 0;
+    oyProfiles_s * patterns = 0, * iccs = 0;
+    oyConfig_s * device = oy_device;
+    const char * profile_file_name = 0;
+    
+    profile = oyProfile_FromSignature( profile_class, oySIGNATURE_CLASS, 0 );
+    patterns = oyProfiles_MoveIn( patterns, &profile, -1 );
+    
+    iccs = oyProfiles_Create( patterns, 0 );
+    oyProfiles_Release( &patterns );
+    
+    
+    size = oyProfiles_Count(iccs);
+    int32_t * rank_list = (int32_t*) malloc( oyProfiles_Count(iccs) *           
+                                             sizeof(int32_t) );
+    oyProfiles_DeviceRank( iccs, device, rank_list );
+    
+    size = oyProfiles_Count(iccs);
+    
+    error = oyDeviceGetProfile( device, options, &profile );
+    profile_file_name = oyProfile_GetFileName( profile, 0 );
+    
+    int show_only_device_related = 1;
+    int empty_added = -1;                   
+    
+    for( i = 0; i < size; ++i)
+    {
+      const char * temp_profile_file_name, * description;
+      {
+         temp_profile = oyProfiles_Get( iccs, i );
+         description = oyProfile_GetText( temp_profile, oyNAME_DESCRIPTION );
+         temp_profile_file_name = oyProfile_GetFileName( temp_profile, 0);
+         
+         current_tmp = -1;
+         
+         if(profile_file_name && temp_profile_file_name &&                      
+            strcmp( profile_file_name, temp_profile_file_name ) == 0)           
+           current_tmp = pos;
+           
+         if(current == -1 && current_tmp != -1)
+           current = current_tmp;
+           
+         if(empty_added == -1 &&
+            rank_list[i] < 1) 
+         {
+           printf("\n");
+           empty_added = pos;
+           if(current != -1 &&                                                  
+              current == pos)
+             ++current;
+           ++pos;
+         } 
+
+         printf("[%d] %s (%s)\n", rank_list[i], description, temp_profile_file_name);
+         
+         if(show_only_device_related == 0 ||
+            rank_list[i] > 0 ||
+            current_tmp != -1)
+           ++pos;
+      }  
+      oyProfile_Release( &temp_profile );
+    } 
+    if(empty_added == -1)
+    {
+      ++pos;
+      if(current == -1 && current_tmp != -1)                                      
+        current = pos; 
+    }   
+    printf("current: %d\n", current);
+    oyConfig_Release( &device );
+    oyProfile_Release( &profile );                                                
+    oyProfiles_Release( &iccs );
+    oyOptions_Release( &options );
+
+    }
 
     oyConfig_Release( &oy_device );
     exit(0);
@@ -204,6 +328,8 @@ int main(int argc, char *argv[])
     printf( "Usage - set a device profile:\n  %s -s [device_class \"monitor\"] [device_name \":0.0\"] profilename.icc\n",
             strrchr(argv[0],'/') ? strrchr(argv[0],'/')+1 : argv[0] );
     printf( "Usage - tell verbosely about devices:\n  %s -v [device_class \"monitor\"]\n",
+            strrchr(argv[0],'/') ? strrchr(argv[0],'/')+1 : argv[0] );
+    printf( "Usage - list all profiles for a device:\n  %s --list-profiles \"device_class\" \"device_name\"\n",
             strrchr(argv[0],'/') ? strrchr(argv[0],'/')+1 : argv[0] );
     exit(1);
   }
