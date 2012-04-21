b1685606201b1884e8ea576e285bccf75c0cd788
Kai-Uwe Behrmann <ku.b@gmx.de>
2012-04-21 16:41:43 +0200

    * [alpha]: remove unneeded header

--- oyranos_convert.c
+++ oyranos_convert.c
@@ -21,7 +21,6 @@
 #include "oyranos.h"
 #include "oyranos_alpha.h"
 #include "oyranos_debug.h"
-//#include "oyranos_elektra.h"
 #include "oyranos_helper.h"
 #include "oyranos_helper_macros.h"
 #include "oyranos_internal.h"
