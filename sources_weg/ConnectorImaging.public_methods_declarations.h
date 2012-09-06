a177f496be0410c8af37b157bd161f13b9811bbd
Kai-Uwe Behrmann <ku.b@gmx.de>
2012-09-06 23:15:48 +0200

    * [sources]: add member accessors to oyConnectorImaging_s

---
+++ sources_weg/ConnectorImaging.public_methods_declarations.h
@@ -0,0 +1,42 @@
+OYAPI int  OYEXPORT
+                 oyConnectorImaging_GetDataTypes (
+                                       oyConnectorImaging_s  * c,
+                                       const oyDATATYPE_e   ** data_types,
+                                       int                   * data_types_n );
+OYAPI int  OYEXPORT
+                 oyConnectorImaging_GetChannelTypes (
+                                       oyConnectorImaging_s  * c,
+                                       const oyCHANNELTYPE_e** channel_types,
+                                       int                   * channel_types_n );
+
+/** @enum    oyCONNECTOR_IMAGING_CAP_e
+ *  @brief   Capabilities of oyConnectorImaging_s
+ *
+ *  @version Oyranos: 0.5.0
+ *  @since   2012/09/06 (Oyranos: 0.5.0)
+ *  @date    2012/09/06
+ */
+typedef enum {
+  oyCONNECTOR_IMAGING_CAP_IS_PLUG,
+  oyCONNECTOR_IMAGING_CAP_MAX_COLOUR_OFFSET,
+  oyCONNECTOR_IMAGING_CAP_MIN_CHANNELS_COUNT,
+  oyCONNECTOR_IMAGING_CAP_MAX_CHANNELS_COUNT,
+  oyCONNECTOR_IMAGING_CAP_MIN_COLOUR_COUNT,
+  oyCONNECTOR_IMAGING_CAP_MAX_COLOUR_COUNT,
+  oyCONNECTOR_IMAGING_CAP_CAN_PLANAR,        /**< can read separated channels */
+  oyCONNECTOR_IMAGING_CAP_CAN_INTERWOVEN,    /**< can read continuous channels */
+  oyCONNECTOR_IMAGING_CAP_CAN_SWAP,          /**< can swap colour channels (BGR)*/
+  oyCONNECTOR_IMAGING_CAP_CAN_SWAP_BYTES,    /**< non host byte order */
+  oyCONNECTOR_IMAGING_CAP_CAN_REVERT,        /**< revert 1 -> 0 and 0 -> 1 */
+  oyCONNECTOR_IMAGING_CAP_CAN_PREMULTIPLIED_ALPHA,
+  oyCONNECTOR_IMAGING_CAP_CAN_NONPREMULTIPLIED_ALPHA,
+  oyCONNECTOR_IMAGING_CAP_CAN_SUBPIXEL,      /**< understand subpixel order */
+  oyCONNECTOR_IMAGING_CAP_ID,                /**< relative to oyFilterCore_s, e.g. 1*/
+  /**< connector is mandatory or optional, important for modules */
+  oyCONNECTOR_IMAGING_CAP_IS_MANDATORY
+} oyCONNECTOR_IMAGING_CAP_e;
+OYAPI int  OYEXPORT
+                 oyConnectorImaging_GetCapability (
+                                       oyConnectorImaging_s    * c,
+                                       oyCONNECTOR_IMAGING_CAP_e type );
+
---
+++ sources_weg/ConnectorImaging.public_methods_definitions.c
@@ -0,0 +1,99 @@
+/** Function  oyConnectorImaging_GetDataTypes
+ *  @memberof oyConnectorImaging_s
+ *  @brief    Get capabilities of oyConnectorImaging_s
+ *
+ *  @version  Oyranos: 0.5.0
+ *  @date     2012/09/06
+ *  @since    2012/09/06 (Oyranos: 0.5.0)
+ */
+OYAPI int  OYEXPORT
+                 oyConnectorImaging_GetDataTypes (
+                                       oyConnectorImaging_s  * c,
+                                       const oyDATATYPE_e   ** data_types,
+                                       int                   * data_types_n )
+{
+  oyConnectorImaging_s_ * s = (oyConnectorImaging_s_*)c;
+
+  if(!s)
+    return 0;
+
+  oyCheckType__m( oyOBJECT_CONNECTOR_IMAGING_S, return 1 )
+
+  if(data_types)
+    *data_types = s->data_types;
+  if(data_types_n)
+    *data_types_n = s->data_types_n;
+
+  return 0;
+}
+/** Function  oyConnectorImaging_GetChannelTypes
+ *  @memberof oyConnectorImaging_s
+ *  @brief    Get capabilities of oyConnectorImaging_s
+ *
+ *  @version  Oyranos: 0.5.0
+ *  @date     2012/09/06
+ *  @since    2012/09/06 (Oyranos: 0.5.0)
+ */
+OYAPI int  OYEXPORT
+                 oyConnectorImaging_GetChannelTypes (
+                                       oyConnectorImaging_s  * c,
+                                       const oyCHANNELTYPE_e** channel_types,
+                                       int                   * channel_types_n )
+{
+  oyConnectorImaging_s_ * s = (oyConnectorImaging_s_*)c;
+
+  if(!s)
+    return 0;
+
+  oyCheckType__m( oyOBJECT_CONNECTOR_IMAGING_S, return 1 )
+
+  if(channel_types)
+    *channel_types = s->channel_types;
+  if(channel_types_n)
+    *channel_types_n = s->channel_types_n;
+
+  return 0;
+}
+
+/** Function  oyConnectorImaging_GetDataTypes
+ *  @memberof oyConnectorImaging_s
+ *  @brief    Get capabilities of oyConnectorImaging_s
+ *
+ *  @version  Oyranos: 0.5.0
+ *  @date     2012/09/06
+ *  @since    2012/09/06 (Oyranos: 0.5.0)
+ */
+OYAPI int  OYEXPORT
+                 oyConnectorImaging_GetCapability (
+                                       oyConnectorImaging_s    * c,
+                                       oyCONNECTOR_IMAGING_CAP_e type )
+{
+  oyConnectorImaging_s_ * s = (oyConnectorImaging_s_*)c;
+
+  if(!s)
+    return 0;
+
+  oyCheckType__m( oyOBJECT_CONNECTOR_IMAGING_S, return 0 )
+
+  switch(type)
+  {
+    case oyCONNECTOR_IMAGING_CAP_IS_PLUG: return s->is_plug;
+    case oyCONNECTOR_IMAGING_CAP_MAX_COLOUR_OFFSET: return s->max_colour_offset;
+    case oyCONNECTOR_IMAGING_CAP_MIN_CHANNELS_COUNT: return s->min_channels_count;
+    case oyCONNECTOR_IMAGING_CAP_MAX_CHANNELS_COUNT: return s->max_channels_count;
+    case oyCONNECTOR_IMAGING_CAP_MIN_COLOUR_COUNT: return s->min_colour_count;
+    case oyCONNECTOR_IMAGING_CAP_MAX_COLOUR_COUNT: return s->max_colour_count;
+    case oyCONNECTOR_IMAGING_CAP_CAN_PLANAR: return s->can_planar;
+    case oyCONNECTOR_IMAGING_CAP_CAN_INTERWOVEN: return s->can_interwoven;
+    case oyCONNECTOR_IMAGING_CAP_CAN_SWAP: return s->can_swap;
+    case oyCONNECTOR_IMAGING_CAP_CAN_SWAP_BYTES: return s->can_swap_bytes;
+    case oyCONNECTOR_IMAGING_CAP_CAN_REVERT: return s->can_revert;
+    case oyCONNECTOR_IMAGING_CAP_CAN_PREMULTIPLIED_ALPHA: return s->can_premultiplied_alpha;
+    case oyCONNECTOR_IMAGING_CAP_CAN_NONPREMULTIPLIED_ALPHA: return s->can_nonpremultiplied_alpha;
+    case oyCONNECTOR_IMAGING_CAP_CAN_SUBPIXEL: return s->can_subpixel;
+    case oyCONNECTOR_IMAGING_CAP_ID: return s->id;
+    case oyCONNECTOR_IMAGING_CAP_IS_MANDATORY: return s->is_mandatory;
+    default; return 0; 
+  }
+  return 0;
+}
