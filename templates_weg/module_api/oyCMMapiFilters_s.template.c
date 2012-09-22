289b04a1787a58b5c98576d80d1efc4885f557f1
Kai-Uwe Behrmann <ku.b@gmx.de>
2012-09-22 13:24:21 +0200

    * [build]: add CMMapiFilters
    fix build issue through include
    
    oyIsOfTypeCMMapiFilter can reside in oy-module.h ?

--- templates_weg/module_api/oyCMMapiFilters_s.template.c
+++ templates_weg/module_api/oyCMMapiFilters_s.template.c
@@ -1,3 +1,8 @@
 {% extends "BaseList_s.c" %}
 
+{% block LocalIncludeFiles %}
+{{ block.super }}
+#include "oyranos_alpha_internal.h"
+{% endblock %}
+
 {% block MoveIn_CheckTypes %}oyIsOfTypeCMMapiFilter( (*obj)->type_ ){% endblock %}
