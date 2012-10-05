81709c80266297ef8c109235c94c7406ddba2f1a
Kai-Uwe Behrmann <ku.b@gmx.de>
2012-10-05 12:34:03 +0200

    * [test]: adapt to generated object types
    file test.c - not so important

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
  * Oyranos is an open source Colour Management System 
  * 
  *  @par Copyright:
- *            2004-2009 (C) Kai-Uwe Behrmann
+ *            2004-2012 (C) Kai-Uwe Behrmann
  *
  *  @author   Kai-Uwe Behrmann <ku.b@gmx.de>
  *  @par License:
@@ -18,8 +18,12 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "oyArray2d_s.h"
+#include "oyConversion_s.h"
+#include "oyFilterNode_s.h"
+#include "oyImage_s.h"
+
 #include <oyranos.h>
-#include <oyranos_alpha.h>
 #include <oyranos_texts.h> /* oyStringListRelease_ */
 #include <oyranos_cmm.h>   /* for hacking into module API */
 
@@ -32,7 +36,7 @@ char**             oyStringSplit_    ( const char    * text,
                                        const char      delimiter,
                                        int           * count,
                                        oyAlloc_f       allocateFunc );
-int                oyWriteMemToFile_ ( const char*, void*, size_t );
+int                oyWriteMemToFile_ ( const char*, const void*, size_t );
 
 
 int
@@ -52,6 +56,7 @@ main(int argc, char** argv)
   oyProfile_s * prof = 0;
   int x,y,w,h, i,j;
   char * ptr = 0;
+  oyPointer_s * backend_data;
   uint32_t size = 0;
   int error = 0;
 
@@ -72,9 +77,11 @@ main(int argc, char** argv)
   oyImage_Release( &image_out );
   oyProfile_Release( &prof );
   /* create a very simple pixel iterator */
+  out = oyConversion_GetNode( conversion, OY_OUTPUT );
   pixel_access = oyPixelAccess_Create( 0,0,
-                                 oyFilterNode_GetPlug( conversion->out_, 0 ),
+                                 oyFilterNode_GetPlug( out, 0 ),
                                        oyPIXEL_ACCESS_IMAGE, 0 );
+  oyFilterNode_Release( &out );
 
   /* show the Oyranos graph with ghostview */
   ptr = oyConversion_ToText( conversion, "Oyranos simple Test Graph",0,malloc);
@@ -89,59 +96,45 @@ main(int argc, char** argv)
   x = y = 0;
   {
     double * p;
+    uint8_t ** ptrs;
+    oyArray2d_s * array;
 
-    pixel_access->start_xy[0] = x;
-    pixel_access->start_xy[1] = y;
+    oyPixelAccess_ChangeRectangle( pixel_access, x,y, 0 );
     result = oyConversion_RunPixels( conversion, pixel_access );
 
-    p = (double*)pixel_access->array->array2d[0];
+    array = oyPixelAccess_GetArray( pixel_access );
+    ptrs = oyArray2d_GetData( array );
+    p = (double*)ptrs[0];
 
     if(result == 0)
-    for(i = 0; i < pixel_access->array->height; ++i)
-    for(j = 0; j < pixel_access->array->width; j += 3)
     {
-      char br = (j != pixel_access->array->width - 3) ? ' ':'\n';
-      p = (double*)pixel_access->array->array2d[i];
-      fprintf( stdout, "%.03f %.03f %.03f %c",
-               p[j+0], p[j+1], p[j+2], br );
+      int array_width = oyArray2d_GetWidth( array );
+      int array_height = oyArray2d_GetHeight( array );
+      for(i = 0; i < array_height; ++i)
+      for(j = 0; j < array_width; j += 3)
+      {
+        char br = (j != array_width - 3) ? ' ':'\n';
+        p = (double*)ptrs[i];
+        fprintf( stdout, "%.03f %.03f %.03f %c",
+                 p[j+0], p[j+1], p[j+2], br );
+      }
     }
+    oyArray2d_Release( &array );
   }
 
-  /* itereate in chunks */
-  result = 0;
-  x = y = 0;
-  pixel_access->start_xy[0] = pixel_access->start_xy[1] = x;
-  while(result == 0 && 0)
-  {
-    double * p = 0;
+  in = oyConversion_GetNode( conversion, OY_INPUT );
+  backend_data = oyFilterNode_GetModuleData( in );
+  oyFilterNode_Release( &in );
+  oyPointer_Release( &backend_data );
 
-    result = oyConversion_RunPixels( conversion, pixel_access );
-    p = pixel;
-
-    if(result == 0)
-    for(i = 0; i < pixel_access->pixels_n*3; i += 3)
-      fprintf( stdout, "%.01f %.01f %.01f\n", p[i+0], p[i+1], p[i+2] );
-
-    x += 2;
-    /*if(x >= w)
-    {
-      pixel_access->start_xy[0] = 0;
-      pixel_access->start_xy[1] = ++y;
-      x = 0;
-    }*/
-  }
-
-
-  if(conversion->input->core->api4_->oyCMMFilterNode_ContextToMem)
-    ptr = conversion->input->core->api4_->oyCMMFilterNode_ContextToMem( conversion->input, &size, malloc );
-  free(ptr); ptr = 0;
-
-  if (0) /* dump the colour transformation */
+#if 0
+  /* dump the colour transformation */
   {
     oyFilterPlug_s * plug = oyFilterNode_GetPlug( conversion->out_, 0 );
     ptr = plug->remote_socket_->node->core->api4_->oyCMMFilterNode_ContextToMem(
              plug->remote_socket_->node, &size, malloc );
   }
+#endif
 
   if(ptr)
     oyWriteMemToFile_( "test_dbg.icc", ptr, size );
@@ -155,15 +148,17 @@ main(int argc, char** argv)
   in = oyFilterNode_NewWith( "//" OY_TYPE_STD "/input_ppm", 0, 0 );
   oyConversion_Set( conversion, in, 0 );
 
-  options = oyFilterNode_OptionsGet( in, OY_FILTER_GET_DEFAULT );
+  options = oyFilterNode_GetOptions( in, oyOPTIONSOURCE_FILTER );
   error = oyOptions_SetFromText( &options, "//" OY_TYPE_STD "/input_ppm/filename",
                                  "oyranos_logo.ppm", OY_CREATE_NEW );
   oyOptions_Release( &options );
 
   image_in = oyConversion_GetImage( conversion, OY_INPUT );;
   prof = oyProfile_FromFile( "Lab.icc", 0, 0 );
-  image_out = oyImage_Create( image_in->width, image_in->height, 0, 
-                              oyImage_PixelLayoutGet( image_in ), prof, 0 );
+  image_out = oyImage_Create( oyImage_GetWidth(image_in),
+                              oyImage_GetHeight(image_in), 0, 
+                              oyImage_GetPixelLayout( image_in, oyLAYOUT ),
+                              prof, 0 );
 
   out = oyFilterNode_NewWith( "//" OY_TYPE_STD "/icc", 0, 0 );
   error = oyFilterNode_Connect( in, "//" OY_TYPE_STD "/data",
@@ -173,15 +168,14 @@ main(int argc, char** argv)
   in = out; in = 0; 
  
   out = oyFilterNode_NewWith( "//" OY_TYPE_STD "/output_ppm", 0, 0 );
-  options = oyFilterNode_OptionsGet( out, OY_FILTER_GET_DEFAULT );
+  options = oyFilterNode_GetOptions( out, oyOPTIONSOURCE_FILTER );
   error = oyOptions_SetFromText( &options, "//" OY_TYPE_STD "/output_ppm/filename",
                                  "test_dbg.ppm", OY_CREATE_NEW );
   oyOptions_Release( &options );
   error = oyFilterNode_Connect( in, "//" OY_TYPE_STD "/data",
                                 out, "//" OY_TYPE_STD "/data", 0 );
 
-  pixel_access->start_xy[0] = 0;
-  pixel_access->start_xy[1] = 0;
+  oyPixelAccess_ChangeRectangle( pixel_access, 0,0, NULL );
   result = oyConversion_RunPixels( conversion, pixel_access );
 
   oyPixelAccess_Release( &pixel_access );
@@ -196,7 +190,8 @@ main(int argc, char** argv)
   free(ptr); ptr = 0;
 
   image_out = oyConversion_GetImage( conversion, OY_OUTPUT );
-  fprintf( stdout, "image: %dx%d\n", image_out->width, image_out->height );
+  fprintf( stdout, "image: %dx%d\n",
+           oyImage_GetWidth(image_out), oyImage_GetHeight(image_out) );
   oyImage_Release( &image_out );
 
   oyConversion_Release( &conversion );
