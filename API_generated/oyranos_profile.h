a079a7760f78e7c1dfbda88e34e49b9f45461f09
Kai-Uwe Behrmann <ku.b@gmx.de>
2012-10-09 09:30:35 +0200

    * [generated]: add oy_profile.h

---
+++ API_generated/oyranos_profile.h
@@ -0,0 +1,90 @@
+/** @file oyranos_profile.h
+
+   [Template file inheritance graph]
+   +-- oyranos_profile.template.h
+
+ *  Oyranos is an open source Colour Management System
+ *
+ *  @par Copyright:
+ *            2004-2012 (C) Kai-Uwe Behrmann
+ *
+ *  @author   Kai-Uwe Behrmann <ku.b@gmx.de>
+ *  @par License:
+ *            new BSD - see: http://www.opensource.org/licenses/bsd-license.php
+ *  @date     2012/10/08
+ */
+
+
+#ifndef OYRANOS_PROFILE_H
+#define OYRANOS_PROFILE_H
+
+#ifdef __cplusplus
+/*namespace oyranos {*/
+  extern "C" {
+#endif /* __cplusplus */
+
+
+
+/** \addtogroup objects_profile Profile API
+ *
+ *  To open a profile exist several methods in the oyProfile_Fromxxx APIs.
+ *  oyProfile_FromStd(), oyProfile_FromFile() and oyProfile_FromMem() are basic
+ *  profile open functions. oyProfile_FromSignature() is used for creating a
+ *  dummy profile during profile filtering.
+ *  oyProfile_FromMD5() is a lookup function from a profile hash, e.g. as
+ *  provided by a 'psid' tag. \n
+ *  Profile properties can be obtained from oyProfile_GetSignature(), like
+ *  colour spaces, date, magic number and so on.
+ *  oyProfile_GetChannelNames(), oyProfile_GetText() and oyProfile_GetID()
+ *  provide additional informations. \n
+ *  The profile element functions have Tag in their names. They work together
+ *  with the oyProfileTag_s APIs. \n
+ *  oyProfile_GetFileName is a reverse lookup to obtain the name of a installed
+ *  file from a profile, e.g. find the name of a reachable display profile.
+
+ *  @{
+ */
+
+
+
+
+/** @brief general profile infos
+ *  @ingroup objects_profile
+ *
+ *  use for oyProfile_GetSignature
+ *
+ *  @since Oyranos: version 0.1.8
+ *  @date  10 december 2007 (API 0.1.8)
+ */
+typedef enum {
+  oySIGNATURE_COLOUR_SPACE,            /**< colour space */
+  oySIGNATURE_PCS,                     /**< profile connection space */
+  oySIGNATURE_SIZE,                    /**< internal stored size */
+  oySIGNATURE_CMM,                     /**< prefered CMM */
+  oySIGNATURE_VERSION,                 /**< version */
+  oySIGNATURE_CLASS,                   /**< usage class, e.g. 'mntr' ... */
+  oySIGNATURE_MAGIC,                   /**< magic; ICC: 'acsp' */
+  oySIGNATURE_PLATFORM,                /**< operating system */
+  oySIGNATURE_OPTIONS,                 /**< various ICC header flags */
+  oySIGNATURE_MANUFACTURER,            /**< device manufacturer */
+  oySIGNATURE_MODEL,                   /**< device modell */
+  oySIGNATURE_INTENT,                  /**< seldom used profile claimed intent*/
+  oySIGNATURE_CREATOR,                 /**< profile creator ID */
+  oySIGNATURE_DATETIME_YEAR,           /**< creation time in UTC */
+  oySIGNATURE_DATETIME_MONTH,          /**< creation time in UTC */
+  oySIGNATURE_DATETIME_DAY,            /**< creation time in UTC */
+  oySIGNATURE_DATETIME_HOURS,          /**< creation time in UTC */
+  oySIGNATURE_DATETIME_MINUTES,        /**< creation time in UTC */
+  oySIGNATURE_DATETIME_SECONDS,        /**< creation time in UTC */
+  oySIGNATURE_MAX
+} oySIGNATURE_TYPE_e;
+
+/** @} *//* objects_profile */
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+/*}*/ /* namespace oyranos */
+#endif /* __cplusplus */
+
+#endif /* OYRANOS_PROFILE_H */
