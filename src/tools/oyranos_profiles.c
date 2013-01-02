96b207fd5f677a2dbb7d90b5a0e91331d51fc911
Kai-Uwe Behrmann <ku.b@gmx.de>
2013-01-02 15:53:07 +0100

    * [tools]: support download of ICC profiles in oy-profiles

--- src/tools/oyranos_profiles.c
+++ src/tools/oyranos_profiles.c
@@ -399,8 +399,25 @@ int main( int argc , char** argv )
       for(i = 0; i < install_n; ++i)
       {
         const char * file_name = install[i];
-        oyProfile_s * ip = oyProfile_FromFile( file_name, OY_NO_CACHE_WRITE, 0 );
+        oyProfile_s * ip = 0;
         char * show_text = 0;
+
+        if(file_name && strlen(file_name) > 10 && strcmp(file_name,"http://"))
+        {
+          size_t size = 0;
+          char * mem = NULL;
+
+          mem = oyReadUrlToMemf_( &size, "r", oyAllocateFunc_,
+                                  file_name );
+
+          if(mem && size)
+          {
+            ip = oyProfile_FromMem( size, mem, OY_NO_CACHE_WRITE, NULL );
+            oyFree_m_( mem ); size = 0;
+          }
+
+        } else
+          ip = oyProfile_FromFile( file_name, OY_NO_CACHE_WRITE, 0 );
 
         if(!ip)
         {
