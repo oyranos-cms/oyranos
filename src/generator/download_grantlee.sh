e8b2fc6f6895c2e25a77f7871862ff5962c6e5b5
Kai-Uwe Behrmann <ku.b@gmx.de>
2013-01-17 11:13:57 +0100

    * [conf]: alternate grantlee download with curl

--- src/generator/download_grantlee.sh
+++ src/generator/download_grantlee.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 version=0.2.0
+packet_file=grantlee-$version.tar.gz
+loc=http://downloads.grantlee.org/
 
 mkdir -p grantlee
 cd grantlee
 if [ -f grantlee-$version.tar.gz ]; then
   echo ""
 else
-  wget http://downloads.grantlee.org/grantlee-$version.tar.gz
+  which curl && curl -L $loc$packet_file -o $packet_file || wget $loc$packet_file
 fi
 tar xzf grantlee-$version.tar.gz
 cd grantlee-$version
