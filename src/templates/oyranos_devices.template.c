037a6e56a74a66eddf0570ebf340ca4af0df85ca
Kai-Uwe Behrmann <ku.b@gmx.de>
2013-03-12 19:45:42 +0100

    * [templates]: show node options in oyFilterGraph_ToText
    ... and move oyFilterGraph_ToText to oy_devices.c in order to link against
    some needed symbols.

--- src/templates/oyranos_devices.template.c
+++ src/templates/oyranos_devices.template.c
@@ -2572,6 +2572,287 @@ int            oyFilterNode_GetUi    ( oyFilterNode_s     * node,
   return error;
 }
 
+/** Function  oyFilterGraph_ToText
+ *  @memberof oyFilterGraph_s
+ *  @brief    Text description of a graph
+ *
+ *  @todo Should this function generate XFORMS compatible output? How?
+ *
+ *  @param[in]     graph               graphy object
+ *  @param[in]     input               start node of a oyConversion_s
+ *  @param[in]     output              end node and if present a switch
+ *                                     to interprete input and output as start
+ *                                     and end node of a oyConversion_s
+ *  @param[in]     head_line           text for inclusion
+ *  @param[in]     reserved            future format selector (dot, xml ...)
+ *  @param[in]     allocateFunc        allocation function
+ *  @return                            the graph description
+ *
+ *  @version Oyranos: 0.9.5
+ *  @date    2013/03/12
+ *  @since   2008/10/04 (Oyranos: 0.1.8)
+ */
+OYAPI char * OYEXPORT
+           oyFilterGraph_ToText      ( oyFilterGraph_s   * graph,
+                                       oyFilterNode_s    * input,
+                                       oyFilterNode_s    * output,
+                                       const char        * head_line,
+                                       int                 reserved,
+                                       oyAlloc_f           allocateFunc )
+{
+  char * text = 0,
+       * temp = oyAllocateFunc_(4096),
+       * temp2 = oyAllocateFunc_(4096),
+       * tmp = 0, * txt = 0, * t = 0, * t2 = 0;
+  oyFilterNode_s * node = 0;
+  char * save_locale = 0;
+  oyFilterGraph_s_ * s = (oyFilterGraph_s_*)graph;
+
+  oyFilterPlug_s_ * p = 0;
+  int i, j, n, len,
+      nodes_n = 0;
+  oyPointer_s * backend_data;
+
+  oyCheckType__m( oyOBJECT_FILTER_GRAPH_S, return 0 )
+
+  save_locale = oyStringCopy_( setlocale(LC_NUMERIC, 0 ), oyAllocateFunc_);
+
+
+  STRING_ADD( text, "digraph G {\n" );
+  STRING_ADD( text, "bgcolor=\"transparent\"\n" );
+  STRING_ADD( text, "  rankdir=LR\n" );
+  STRING_ADD( text, "  graph [fontname=Helvetica, fontsize=12];\n" );
+  STRING_ADD( text, "  node [shape=record, fontname=Helvetica, fontsize=10, style=\"filled,rounded\"];\n" );
+  STRING_ADD( text, "  edge [fontname=Helvetica, fontsize=10];\n" );
+  STRING_ADD( text, "\n" );
+  if(input && output)
+  {
+  STRING_ADD( text, "  conversion [shape=plaintext, label=<\n" );
+  STRING_ADD( text, "<table border=\"0\" cellborder=\"1\" cellspacing=\"0\">\n" );
+  STRING_ADD( text, "  <tr><td>oyConversion_s</td></tr>\n" );
+  STRING_ADD( text, "  <tr><td>\n" );
+  STRING_ADD( text, "     <table border=\"0\" cellborder=\"0\" align=\"left\">\n" );
+  STRING_ADD( text, "       <tr><td align=\"left\">...</td></tr>\n" );
+  STRING_ADD( text, "       <tr><td align=\"left\" port=\"in\">+input</td></tr>\n" );
+  STRING_ADD( text, "       <tr><td align=\"left\" port=\"out\">+out_</td></tr>\n" );
+  STRING_ADD( text, "       <tr><td align=\"left\">...</td></tr>\n" );
+  STRING_ADD( text, "     </table>\n" );
+  STRING_ADD( text, "     </td></tr>\n" );
+  STRING_ADD( text, "  <tr><td> </td></tr>\n" );
+  STRING_ADD( text, "</table>>,\n" );
+  STRING_ADD( text, "                    style=\"\", color=black];\n" );
+  }
+  STRING_ADD( text, "\n" );
+
+  /* add more node descriptions */
+  nodes_n = oyFilterNodes_Count( s->nodes );
+  for(i = 0; i < nodes_n; ++i)
+  {
+    oyFilterCore_s_ * node_core;
+    oyOptions_s * node_opts;
+    const char * tc;
+
+    node = oyFilterNodes_Get( s->nodes, i );
+    n = oyFilterNode_EdgeCount( node, 1, 0 );
+    node_core = (oyFilterCore_s_*)oyFilterNode_GetCore( node );
+
+    /** The function is more verbose with the oy_debug variable set. */
+    if(!oy_debug &&
+       oyStrchr_( ((oyCMMapi4_s_*)node_core->api4_)->id_, OY_SLASH_C ))
+    {
+      STRING_ADD( tmp, ((oyCMMapi4_s_*)node_core->api4_)->id_ );
+      t = oyStrrchr_( tmp, OY_SLASH_C );
+      *t = 0;
+      STRING_ADD( txt, t+1 );
+      oyFree_m_(tmp);
+    } else
+    {
+      STRING_ADD( txt, ((oyCMMapi4_s_*)node_core->api4_)->id_ );
+      if(oy_debug)
+      {
+        int node_sockets_n = oyFilterNode_EdgeCount( node, 0,
+                                                     OY_FILTEREDGE_CONNECTED );
+      for(j = 0; j < node_sockets_n; ++j)
+      {
+        oyFilterSocket_s * socket = oyFilterNode_GetSocket( node, j );
+        oyStruct_s * socket_data = oyFilterSocket_GetData( socket );
+        if(socket && socket_data)
+        {
+          const char * name = oyObject_GetName( socket_data->oy_, 1 );
+          int k, pos = 0;
+          len = strlen(name);
+          for(k = 0; k < len; ++k)
+            if(k && name[k] == '"' && name[k-1] != '\\')
+            {
+              sprintf( &temp2[pos], "\\\"" );
+              pos += 2;
+            } else if(name[k] == '<')
+            {
+              sprintf( &temp2[pos], "\\<" );
+              pos += 2;
+            } else if(name[k] == '>')
+            {
+              sprintf( &temp2[pos], "\\>" );
+              pos += 2;
+            } else if(name[k] == '[')
+            {
+              sprintf( &temp2[pos], "\\[" );
+              pos += 2;
+            } else if(name[k] == ']')
+            {
+              sprintf( &temp2[pos], "\\]" );
+              pos += 2;
+            } else if(name[k] == '\n')
+            {
+              sprintf( &temp2[pos], "\\n" );
+              pos += 2;
+            } else
+              temp2[pos++] = name[k];
+          temp2[pos] = 0;
+          if(oy_debug >= 4)
+          oyMessageFunc_p( oyMSG_DBG,(oyStruct_s*)node,
+                           OY_DBG_FORMAT_ "%s", OY_DBG_ARGS_, name );
+          if(oy_debug >= 4)
+          oyMessageFunc_p( oyMSG_DBG,(oyStruct_s*)node,
+                           OY_DBG_FORMAT_ "%s", OY_DBG_ARGS_, temp2 );
+          oySprintf_(temp, "  %d [ label=\"{<data> | Data %d\\n"
+                     " Type: \\\"%s\\\"\\n"
+                     " XML: \\\"%s\\\"|<socket>}\"];\n",
+                     oyObject_GetId( socket->oy_ ),
+                     j,
+                     oyStructTypeToText( socket_data->type_ ),
+                     temp2);
+          if(oy_debug >= 4)
+          oyMessageFunc_p( oyMSG_DBG,(oyStruct_s*)node,
+                           OY_DBG_FORMAT_ "%s", OY_DBG_ARGS_, temp );
+          STRING_ADD( text, temp );
+        }
+        oyFilterSocket_Release( &socket );
+      }
+      }
+    }
+
+    backend_data = oyFilterNode_GetModuleData( node );
+#if 0
+    tc = oyFilterNode_GetText( node, oyNAME_NICK );
+#else
+    node_opts = oyFilterNode_GetOptions( node, 0 );
+    tc = oyOptions_GetText( node_opts, oyNAME_NICK );
+    oyOptions_Release( &node_opts );
+#endif
+    t2 = oyStringReplace_( tc, "\n\n", "\n", oyAllocateFunc_ );
+    t = oyStringReplace_( t2, "\n", "\\n", oyAllocateFunc_ );
+    if(t2) oyFree_m_(t2);
+    t2 = oyStringReplace_( t, "\"", "\\\"", oyAllocateFunc_ );
+    if(t) oyFree_m_(t);
+    t = oyStringReplace_( t2, "<", "*", oyAllocateFunc_ );
+    if(t2) oyFree_m_(t2);
+    t2 = oyStringReplace_( t, ">", "*", oyAllocateFunc_ );
+    if(t) oyFree_m_(t);
+          oyMessageFunc_p( oyMSG_DBG,(oyStruct_s*)node,
+                           OY_DBG_FORMAT_ "%s", OY_DBG_ARGS_, t2 );
+    sprintf( temp,   "  %d [ label=\"{<plug> %d| Filter Node %d\\n"
+                     " Category: \\\"%s\\\"\\n CMM: \\\"%s\\\"\\n"
+                     " Type: \\\"%s\\\"\\n"
+                     " ResourceType: \\\"%s\\\" %d\\n"
+                     " Options: \\\"%s\\\"|<socket>}\"];\n",
+                     oyFilterNode_GetId( node ), n,
+                     node->oy_->id_,
+                     oyFilterCore_GetCategory( (oyFilterCore_s*)node_core, 1 ),
+                     txt,
+                     oyFilterNode_GetRegistration( node ),
+                     oyNoEmptyString_m_(oyPointer_GetResourceName( backend_data )),
+                     oyPointer_GetSize( backend_data ),
+                     oyNoEmptyString_m_(t2));
+
+    STRING_ADD( text, temp );
+    oyFree_m_(txt);
+    if(t2) oyFree_m_(t2);
+
+    oyFilterNode_Release( &node );
+    oyFilterCore_Release( (oyFilterCore_s**)&node_core );
+  }
+
+
+  STRING_ADD( text, "\n" );
+  STRING_ADD( text, "  subgraph cluster_0 {\n" );
+  STRING_ADD( text, "    label=\"" );
+  STRING_ADD( text, head_line );
+  STRING_ADD( text, "\"\n" );
+  STRING_ADD( text, "    color=gray;\n" );
+  STRING_ADD( text, "\n" );
+
+  /* add more node placements */
+  n = oyFilterPlugs_Count( s->edges );
+  for(i = 0; i < n; ++i)
+  {
+    p = (oyFilterPlug_s_*)oyFilterPlugs_Get( s->edges, i );
+
+    oySprintf_( temp,
+                "    %d:socket -> %d:plug [arrowhead=crow, arrowtail=box];\n",
+                oyFilterNode_GetId( (oyFilterNode_s*)p->remote_socket_->node ),
+                oyFilterNode_GetId( (oyFilterNode_s*)p->node ) );
+    STRING_ADD( text, temp );
+
+    oyFilterPlug_Release( (oyFilterPlug_s**)&p );
+  }
+  
+  for(i = 0; i < nodes_n; ++i)
+  {
+    node = oyFilterNodes_Get( s->nodes, i );
+    if(oy_debug)
+    {
+      int node_sockets_n = oyFilterNode_EdgeCount( node, 0,
+                                                   OY_FILTEREDGE_CONNECTED );
+      for(j = 0; j < node_sockets_n; ++j)
+      {
+        oyFilterSocket_s * socket = oyFilterNode_GetSocket( node, j );
+        oyStruct_s * socket_data = oyFilterSocket_GetData( socket );
+        if(socket && socket_data)
+        {
+          oySprintf_( temp,
+               "    %d:socket -> %d:data [arrowhead=crow, arrowtail=box];\n",
+                oyFilterNode_GetId( node ),
+                oyObject_GetId( socket->oy_ ));
+          STRING_ADD( text, temp );
+          if(socket_data->release)
+            socket_data->release( &socket_data );
+        }
+        oyFilterSocket_Release( &socket );
+          
+      }
+    }
+    oyFilterNode_Release( &node );
+  }
+
+  STRING_ADD( text, "\n" );
+  if(input && output)
+  {
+  oySprintf_( temp, "    conversion:in -> %d [arrowhead=none, arrowtail=normal];\n", oyFilterNode_GetId( input ) );
+  STRING_ADD( text, temp );
+  oySprintf_( temp, "    conversion:out -> %d;\n",
+                    oyFilterNode_GetId( output ) );
+  STRING_ADD( text, temp );
+  }
+  STRING_ADD( text, "  }\n" );
+  STRING_ADD( text, "\n" );
+  if(input && output)
+  STRING_ADD( text, "  conversion\n" );
+  STRING_ADD( text, "}\n" );
+  STRING_ADD( text, "\n" );
+
+  setlocale(LC_NUMERIC, "C");
+  /* sensible printing */
+  setlocale(LC_NUMERIC, save_locale);
+  oyFree_m_( save_locale );
+
+  STRING_ADD( text, "" );
+
+  oyFree_m_( temp );
+
+  return text;
+}
+
 /** \addtogroup misc Miscellaneous
 
  *  @{
