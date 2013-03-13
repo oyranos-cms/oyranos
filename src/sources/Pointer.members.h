93448f4ef139773e762f688c2cac6b3580c40d31
Kai-Uwe Behrmann <ku.b@gmx.de>
2013-03-13 09:17:25 +0100

    * [sources]: add oyPointer_s_::id member

--- src/sources/Pointer.members.h
+++ src/sources/Pointer.members.h
@@ -1,5 +1,6 @@
   char               * lib_name;       /*!< the CMM */
   char               * func_name;      /*!< optional the CMM's function name */
+  char               * id;             /*!< identification string */
   oyPointer            ptr;            /*!< a CMM's data pointer */
   int                  size;           /**< -1 && 0 - unknown, > 1 size of ptr*/
   char               * resource;       /**< the resource type, e.g. oyCMM_PROFILE, oyCMM_COLOUR_CONVERSION */
