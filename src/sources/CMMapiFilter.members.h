1ee910dd1f598141bcc9212087ee99e0d5441c84
Kai-Uwe Behrmann <ku.b@gmx.de>
2013-12-28 08:24:46 +0100

    * [template]: add CMMapiFilter:runtime_context
    ... and xxx_G/SetBackendContext() funtions to all filter classes.
    The context is backend specific per module loadtime.

--- src/sources/CMMapiFilter.members.h
+++ src/sources/CMMapiFilter.members.h
@@ -1,1 +1,2 @@
   oyCMMapi5_s_   * api5_;            /**< @private meta module; keep to zero */
+  oyPointer_s    * runtime_context;  /**< data needed to run the filter */
--- src/templates/module_api/oyCMMapiFilter_s.template.h
+++ src/templates/module_api/oyCMMapiFilter_s.template.h
@@ -1,1 +1,16 @@
 {% extends "oyCMMapi_s.template.h" %}
+
+{% block LocalIncludeFiles %}
+{{ block.super }}
+#include "oyPointer_s.h"
+{% endblock %}
+
+{% block GeneralPublicMethodsDeclarations %}
+{{ block.super }}
+OYAPI int  OYEXPORT
+           oy{{ class.baseName }}_SetBackendContext          ( {{ class.name }}       * api,
+                                       oyPointer_s       * ptr );
+OYAPI oyPointer_s * OYEXPORT
+           oy{{ class.baseName }}_GetBackendContext          ( {{ class.name }}       * api );
+{% endblock GeneralPublicMethodsDeclarations %}
+
