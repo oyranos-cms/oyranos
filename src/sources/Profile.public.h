b2eca8d84019c453f90bce56cff539c5533e47d4
Kai-Uwe Behrmann <ku.b@gmx.de>
2014-03-04 21:32:34 +0100

    * [sources]: add comments to OY_COMPUTE define

--- src/sources/Profile.public.h
+++ src/sources/Profile.public.h
@@ -9,11 +9,11 @@
     W  no cache write \n
 
  */
-#define OY_NO_CACHE_READ            0x01        /** read not from cache */
-#define OY_NO_CACHE_WRITE           0x02        /** write not from cache */
+#define OY_NO_CACHE_READ            0x01        /**< read not from cache */
+#define OY_NO_CACHE_WRITE           0x02        /**< write not from cache */
 /* decode */
 #define oyToNoCacheRead_m(r)        ((r)&1)
 #define oyToNoCacheWrite_m(w)       (((w) >> 1)&1)
 
-#define OY_COMPUTE      0x01
-#define OY_FROM_PROFILE 0x04
+#define OY_COMPUTE      0x01                    /**< compute newly */
+#define OY_FROM_PROFILE 0x04                    /**< use from profile */
