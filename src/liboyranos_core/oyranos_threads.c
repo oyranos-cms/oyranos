de48156260738993bf756af48fd5ddd62b70975d
Boris Samorodov <bsam@passap.ru>
2014-03-17 13:32:51 +0100

    * [core]: fix clang (no OPENMP) error

--- src/liboyranos_core/oyranos_threads.c
+++ src/liboyranos_core/oyranos_threads.c
@@ -181,9 +181,11 @@ void oyThreadsInit_(void)
     oyObject_Lock( oy_job_message_list_->oy_, __func__, __LINE__ );
     oyObject_UnLock( oy_job_message_list_->oy_, __func__, __LINE__ );
 
+#ifdef _OPENMP
     if((omp_get_num_procs() - 1) >= 1)
       oy_thread_count_ = omp_get_num_procs() - 1;
     else
+#endif
       oy_thread_count_ = 1;
 
     oy_threads_ = (oyThread_t*)calloc(sizeof(oyThread_t),oy_thread_count_+1);
