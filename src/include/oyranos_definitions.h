fd7238fe58d59865e02291f16e4dbe1e7bdb815a
Kai-Uwe Behrmann <ku.b@gmx.de>
2014-07-30 11:38:12 +0200

    * [API]: change key macros let them not end in a slash

--- src/include/oyranos_definitions.h
+++ src/include/oyranos_definitions.h
@@ -117,11 +117,11 @@
 /** Oyranos internal key path in the data base */
 #define OY_INTERNAL OY_TOP_INTERNAL OY_SLASH OY_DOMAIN_INTERNAL OY_SLASH OY_TYPE_STD
 /** Type path for standard settings */
-#define OY_PROFILE_STD           OY_STD OY_SLASH "profile" OY_SLASH
+#define OY_PROFILE_STD           OY_STD OY_SLASH "profile"
 /** Type path for standard settings */
-#define OY_BEHAVIOUR_STD         OY_STD OY_SLASH "behaviour" OY_SLASH
+#define OY_BEHAVIOUR_STD         OY_STD OY_SLASH "behaviour"
 /** Type path for standard settings */
-#define OY_CMM_STD               OY_STD OY_SLASH "icc_color" OY_SLASH
+#define OY_CMM_STD               OY_STD OY_SLASH "icc_color"
 
 /** Oyranos default profiles key path in the data base */
 #define OY_REGISTRED_PROFILES    OY_STD OY_SLASH "device_profiles"
@@ -140,64 +140,64 @@
 /* --- default profiles --- */
 
 /** key name for the default Editing profile */
-#define OY_DEFAULT_EDITING_RGB_PROFILE       OY_PROFILE_STD "editing_rgb"
+#define OY_DEFAULT_EDITING_RGB_PROFILE       OY_PROFILE_STD OY_SLASH "editing_rgb"
 /** key name for the default Editing profile */
-#define OY_DEFAULT_EDITING_CMYK_PROFILE      OY_PROFILE_STD "editing_cmyk"
+#define OY_DEFAULT_EDITING_CMYK_PROFILE      OY_PROFILE_STD OY_SLASH "editing_cmyk"
 /** key name for the default Editing profile */
-#define OY_DEFAULT_EDITING_XYZ_PROFILE       OY_PROFILE_STD "editing_xyz"
+#define OY_DEFAULT_EDITING_XYZ_PROFILE       OY_PROFILE_STD OY_SLASH "editing_xyz"
 /** key name for the default Editing profile */
-#define OY_DEFAULT_EDITING_LAB_PROFILE       OY_PROFILE_STD "editing_lab"
+#define OY_DEFAULT_EDITING_LAB_PROFILE       OY_PROFILE_STD OY_SLASH "editing_lab"
 /** key name for the default Editing profile */
-#define OY_DEFAULT_EDITING_GRAY_PROFILE      OY_PROFILE_STD "editing_gray"
+#define OY_DEFAULT_EDITING_GRAY_PROFILE      OY_PROFILE_STD OY_SLASH "editing_gray"
 /** key name for the untagged data assumed source XYZ profile */
-#define OY_DEFAULT_ASSUMED_XYZ_PROFILE       OY_PROFILE_STD "assumed_xyz"
+#define OY_DEFAULT_ASSUMED_XYZ_PROFILE       OY_PROFILE_STD OY_SLASH "assumed_xyz"
 /** key name for the untagged data assumed source Lab profile */
-#define OY_DEFAULT_ASSUMED_LAB_PROFILE       OY_PROFILE_STD "assumed_lab"
+#define OY_DEFAULT_ASSUMED_LAB_PROFILE       OY_PROFILE_STD OY_SLASH "assumed_lab"
 /** key name for the untagged data assumed source RGB profile */
-#define OY_DEFAULT_ASSUMED_RGB_PROFILE       OY_PROFILE_STD "assumed_rgb"
+#define OY_DEFAULT_ASSUMED_RGB_PROFILE       OY_PROFILE_STD OY_SLASH "assumed_rgb"
 /** name for the untagged data assumed source profile for www content */
-#define OY_DEFAULT_ASSUMED_WEB_PROFILE       OY_PROFILE_STD "assumed_web" /* static_profile */
+#define OY_DEFAULT_ASSUMED_WEB_PROFILE       OY_PROFILE_STD OY_SLASH "assumed_web" /* static_profile */
 /** key name for the untagged data assumed source Cmyk profile */
-#define OY_DEFAULT_ASSUMED_CMYK_PROFILE      OY_PROFILE_STD "assumed_cmyk"
+#define OY_DEFAULT_ASSUMED_CMYK_PROFILE      OY_PROFILE_STD OY_SLASH "assumed_cmyk"
 /** key name for the untagged data assumed source Gray profile */
-#define OY_DEFAULT_ASSUMED_GRAY_PROFILE      OY_PROFILE_STD "assumed_gray"
+#define OY_DEFAULT_ASSUMED_GRAY_PROFILE      OY_PROFILE_STD OY_SLASH "assumed_gray"
 /** key name for a standard proofing profile */
-#define OY_DEFAULT_PROOF_PROFILE             OY_PROFILE_STD "proof"
+#define OY_DEFAULT_PROOF_PROFILE             OY_PROFILE_STD OY_SLASH "proof"
 
 
 /* --- settings --- */
 
 /** key name for the non profile tagged content action [ dont assign | assign assumed Profile | promt ] */
-#define OY_ACTION_UNTAGGED_ASSIGN      OY_BEHAVIOUR_STD "action_untagged_assign"
+#define OY_ACTION_UNTAGGED_ASSIGN      OY_BEHAVIOUR_STD OY_SLASH "action_untagged_assign"
 /** key name for the rgb profile mismatch action */
-#define OY_ACTION_MISMATCH_RGB         OY_BEHAVIOUR_STD "action_missmatch_rgb"
+#define OY_ACTION_MISMATCH_RGB         OY_BEHAVIOUR_STD OY_SLASH "action_missmatch_rgb"
 /** key name for the cmyk profile mismatch action */
-#define OY_ACTION_MISMATCH_CMYK        OY_BEHAVIOUR_STD "action_missmatch_cmyk"
+#define OY_ACTION_MISMATCH_CMYK        OY_BEHAVIOUR_STD OY_SLASH "action_missmatch_cmyk"
 /** key name for the allow mixed color space switch */
-#define OY_CONVERT_MIXED_COLOR_SPACE_PRINT_DOCUMENT OY_BEHAVIOUR_STD "mixed_color_spaces_print_doc_convert"
+#define OY_CONVERT_MIXED_COLOR_SPACE_PRINT_DOCUMENT OY_BEHAVIOUR_STD OY_SLASH "mixed_color_spaces_print_doc_convert"
 /** key name for generating a mixed color space for internet warning */
-#define OY_CONVERT_MIXED_COLOR_SPACE_SCREEN_DOCUMENT OY_BEHAVIOUR_STD "mixed_color_spaces_screen_doc_convert"
+#define OY_CONVERT_MIXED_COLOR_SPACE_SCREEN_DOCUMENT OY_BEHAVIOUR_STD OY_SLASH "mixed_color_spaces_screen_doc_convert"
 /** key name for the default rendering intent */
-#define OY_DEFAULT_RENDERING_INTENT    OY_BEHAVIOUR_STD "rendering_intent"
+#define OY_DEFAULT_RENDERING_INTENT    OY_BEHAVIOUR_STD OY_SLASH "rendering_intent"
 /** key name for the default rendering with BPC */
-#define OY_DEFAULT_RENDERING_BPC       OY_BEHAVIOUR_STD "rendering_bpc"
+#define OY_DEFAULT_RENDERING_BPC       OY_BEHAVIOUR_STD OY_SLASH "rendering_bpc"
 /** key name for the default proofing rendering intent */
-#define OY_DEFAULT_RENDERING_INTENT_PROOF    OY_BEHAVIOUR_STD "rendering_intent_proof"
+#define OY_DEFAULT_RENDERING_INTENT_PROOF    OY_BEHAVIOUR_STD OY_SLASH "rendering_intent_proof"
 /** key name for the default gamut warning */
-#define OY_DEFAULT_RENDERING_GAMUT_WARNING  OY_BEHAVIOUR_STD "rendering_gamut_warning"
+#define OY_DEFAULT_RENDERING_GAMUT_WARNING  OY_BEHAVIOUR_STD OY_SLASH "rendering_gamut_warning"
 /** key name for soft proofing by default */
-#define OY_DEFAULT_PROOF_SOFT          OY_BEHAVIOUR_STD "proof_soft"
+#define OY_DEFAULT_PROOF_SOFT          OY_BEHAVIOUR_STD OY_SLASH "proof_soft"
 /** key name for hard proofing by default */
-#define OY_DEFAULT_PROOF_HARD          OY_BEHAVIOUR_STD "proof_hard"
+#define OY_DEFAULT_PROOF_HARD          OY_BEHAVIOUR_STD OY_SLASH "proof_hard"
 
 /** key name for context CMM */
-#define OY_DEFAULT_CMM_CONTEXT         OY_CMM_STD "context"
+#define OY_DEFAULT_CMM_CONTEXT         OY_CMM_STD OY_SLASH "context"
 /** key name for renderer CMM */
-#define OY_DEFAULT_CMM_RENDERER        OY_CMM_STD "renderer"
+#define OY_DEFAULT_CMM_RENDERER        OY_CMM_STD OY_SLASH "renderer"
 /** key name for context CMM fallback */
-#define OY_DEFAULT_CMM_CONTEXT_FALLBACK  OY_CMM_STD "context_fallback"
+#define OY_DEFAULT_CMM_CONTEXT_FALLBACK  OY_CMM_STD OY_SLASH "context_fallback"
 /** key name for renderer CMM fallback */
-#define OY_DEFAULT_CMM_RENDERER_FALLBACK OY_CMM_STD "renderer_fallback"
+#define OY_DEFAULT_CMM_RENDERER_FALLBACK OY_CMM_STD OY_SLASH "renderer_fallback"
 
 
 #endif /* OYRANOS_DEFINITIONS_H */
