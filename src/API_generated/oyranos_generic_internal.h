b997fb97445e02068c6d327872007eb717b22de0
Kai-Uwe Behrmann <ku.b@gmx.de>
2015-02-02 18:27:51 +0100

    * [templates]: C++ guards to oy_generic_internal.h

--- src/API_generated/oyranos_generic_internal.h
+++ src/API_generated/oyranos_generic_internal.h
@@ -21,9 +21,17 @@
 #include "oyHash_s.h"
 #include "oyStructList_s.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
 oyHash_s *   oyCacheListGetEntry_    ( oyStructList_s    * cache_list,
                                        uint32_t            flags,
                                        const char        * hash_text );
 
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
 
 #endif /* OYRANOS_GENERIC_INTERNAL_H */
