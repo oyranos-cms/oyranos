912d1567091e0ff6c5a9024e231a45d439eae70d
Kai-Uwe Behrmann <ku.b@gmx.de>
2015-02-08 07:48:42 +0100

    * [core]: declare all DB APIs with oySCOPE_e argument
    The layer above can now select the scope.

--- src/include_private/oyranos_elektra.h
+++ src/include_private/oyranos_elektra.h
@@ -3,7 +3,7 @@
  *  Oyranos is an open source Color Management System 
  *
  *  @par Copyright:
- *            2004-2009 (C) Kai-Uwe Behrmann
+ *            2004-2015 (C) Kai-Uwe Behrmann
  *
  *  @brief    elektra dependent functions
  *  @internal
@@ -31,19 +31,21 @@ void oyDBClose (void);
 /* better use oyExportReset_(EXPORT_SETTING) */
 void oyCloseReal__(void);
 
-int     oyGetBehaviour_        (oyBEHAVIOUR_e type);
-int     oySetBehaviour_        (oyBEHAVIOUR_e type,
-                                int         behaviour);
-
 /* elektra key wrappers */
-char*   oyDBGetString_               ( const char        * key_name,
+char *   oyDBGetString_              ( const char        * key_name,
+                                       oySCOPE_e           scope,
                                        oyAlloc_f           allocFunc );
-int     oyDBSetString_               ( const char        * keyName,
-                                       const char* value, const char* comment);
-char*   oyDBSearchEmptyKeyname_        ( const char        * keyParentName );
-char ** oyDBKeySetGetNames_              ( const char        * key_parent_name,
-                                         int               * n );
-int     oyDBEraseKey_                    ( const char        * key_name );
+int      oyDBSetString_              ( const char        * keyName,
+                                       oySCOPE_e           scope,
+                                       const char        * value,
+                                       const char        * comment );
+char*    oyDBSearchEmptyKeyname_     ( const char        * keyParentName,
+                                       oySCOPE_e           scope );
+char **  oyDBKeySetGetNames_         ( const char        * key_parent_name,
+                                       oySCOPE_e           scope,
+                                       int               * n );
+int      oyDBEraseKey_               ( const char        * key_name,
+                                       oySCOPE_e           scope );
 
 
 #ifdef __cplusplus
