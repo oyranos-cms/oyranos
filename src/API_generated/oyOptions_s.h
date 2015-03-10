9297dbd7991bc6d263267faf2ffae30a2ef29fe3
Kai-Uwe Behrmann <ku.b@gmx.de>
2015-03-10 11:24:29 +0100

    * [sources]: add OY_MATCH_KEY flag

--- src/API_generated/oyOptions_s.h
+++ src/API_generated/oyOptions_s.h
@@ -51,6 +51,7 @@ typedef struct oyOptions_s oyOptions_s;
  *  @{ */
 #define OY_CREATE_NEW                  0x02        /**< create */
 #define OY_ADD_ALWAYS                  0x04        /**< no check for double occurencies; do not use for configurations */
+#define OY_MATCH_KEY                   0x08        /**< check for double occurencies of key name, ignoring the path */
 /* decode */
 #define oyToCreateNew_m(r)             (((r) >> 1)&1)
 #define oyToAddAlways_m(r)             (((r) >> 2)&1)
--- src/sources/Options.public.h
+++ src/sources/Options.public.h
@@ -2,6 +2,7 @@
  *  @{ */
 #define OY_CREATE_NEW                  0x02        /**< create */
 #define OY_ADD_ALWAYS                  0x04        /**< no check for double occurencies; do not use for configurations */
+#define OY_MATCH_KEY                   0x08        /**< check for double occurencies of key name, ignoring the path */
 /* decode */
 #define oyToCreateNew_m(r)             (((r) >> 1)&1)
 #define oyToAddAlways_m(r)             (((r) >> 2)&1)
