8f73ea99f4245c36826bc856d8776d8e30806a16
Kai-Uwe Behrmann <ku.b@gmx.de>
2015-05-05 21:01:29 +0200

    * [alpha]: remove double macro
    moved to oyProfile_s.h

--- src/include_private/oyranos_alpha.h
+++ src/include_private/oyranos_alpha.h
@@ -105,22 +105,6 @@ OYAPI int  OYEXPORT
 
 
 
-/** @typedef oyIO_t
-    parametric type as shorthand for IO flags \n
-
-    should fit into a 32bit type, usual unsigned int or uint32_t \n
-
-    ------ W R \n
-
-    R  no cache read \n
-    W  no cache write \n
-
- */
-#define OY_NO_CACHE_READ            0x01        /** read not from cache */
-#define OY_NO_CACHE_WRITE           0x02        /** write not from cache */
-/* decode */
-#define oyToNoCacheRead_m(r)        ((r)&1)
-#define oyToNoCacheWrite_m(w)       (((w) >> 1)&1)
 #if 0
 OYAPI oyPointer OYEXPORT
                    oyProfile_Align   ( oyProfile_s     * profile,
