00abbad43be8769abf99c7567f72445cc6ceaadc
Kai-Uwe Behrmann <ku.b@gmx.de>
2017-03-22 11:29:35 +0100

    * [conf]: fix at least Xmu detection

--- cmake/Modules/FindXmu.cmake
+++ cmake/Modules/FindXmu.cmake
@@ -1,6 +1,21 @@
+# Redistribution and use is allowed according to the terms of the BSD license.
+# Copyright (c) 2012-2014, Kai-Uwe Behrmann, <ku.b@gmx.de>
+
 find_package(PkgConfig)
 pkg_check_modules(XMU xmu)
 
+FIND_PATH(XMU_INCLUDE_DIRS NAMES Xmu.h
+	ONLY_CMAKE_FIND_ROOT_PATH
+	)
+
+FIND_LIBRARY(XMU_LIBRARY_DIR NAMES Xmu
+	ONLY_CMAKE_FIND_ROOT_PATH
+	)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Xmu
+	REQUIRED_VARS XMU_LIBRARY_DIR XMU_INCLUDE_DIRS
+	)
 
 if (XMU_FOUND)
     set(HAVE_XMU TRUE)
