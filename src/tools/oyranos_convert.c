58df834186445e1fc545cac0a368de69d8552a6d
Kai-Uwe Behrmann <ku.b@gmx.de>
2017-09-18 16:26:19 +0200

    * [tools]: fix non strict OpenMP syntax

--- src/tools/oyranos_convert.c
+++ src/tools/oyranos_convert.c
@@ -647,7 +647,7 @@ int main( int argc , char** argv )
         width = 8;
       buf = calloc(sizeof(uint16_t), size*width*size*width*3);
 
-#pragma omp parallel for private(in,a,b,j,)
+#pragma omp parallel for private(in,a,b,j)
       for(l = 0; l < size; ++l)
       {
         in[0] = floor((double) l / (size - 1) * 65535.0 + 0.5);
