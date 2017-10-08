d292f8ad5936e32576ccc47ee5da42dced9f799b
Kai-Uwe Behrmann <ku.b@gmx.de>
2017-10-08 11:03:59 +0200

    * [sources]: fix valgrind leak warning in oyRankMapFromJSON()

--- src/sources/Config.public_methods_definitions.c
+++ src/sources/Config.public_methods_definitions.c
@@ -1262,6 +1262,9 @@ OYAPI int  OYEXPORT oyRankMapFromJSON( const char        * json_text,
         }
       }
     }
+
+    oyjl_tree_free( json );
+    json = NULL;
   }
 
   if(error <= 0 && rank_map)
