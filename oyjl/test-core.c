5e59d8869cf658feb7286b5579ec6ffdf32f3998
Kai-Uwe Behrmann <ku.b@gmx.de>
2018-08-17 09:14:08 +0200

    * [oyjl]: put tree handling into OyjlCore
    It makes less sense to have a additional library. The dependency is a
    main argument for the libOyjl / libOyjlCore split.

--- oyjl/test-core.c
+++ oyjl/test-core.c
@@ -16,6 +16,7 @@
   TEST_RUN( testVersion, "Version matching", 1 ); \
   TEST_RUN( testI18N, "Internationalisation", 1 ); \
   TEST_RUN( testStringRun, "String handling", 1 ); \
+  TEST_RUN( testTree, "Tree handling", 1 );
 
 #include "oyjl_test.h"
 #include "oyjl.h"
@@ -165,6 +166,89 @@ oyjlTESTRESULT_e testStringRun ()
 }
 
 
+
+oyjlTESTRESULT_e testTree ()
+{
+  oyjlTESTRESULT_e result = oyjlTESTRESULT_UNKNOWN;
+
+  fprintf(stdout, "\n" );
+
+  int i;
+
+  oyjl_val value = 0;
+  oyjl_val root = 0;
+
+  value = oyjlTreeGetValue( NULL, OYJL_CREATE_NEW, "not/existing" );
+  if(!value)
+  { PRINT_SUB( oyjlTESTRESULT_SUCCESS,
+    "oyjlTreeGetValue( NULL, flags=OYJL_CREATE_NEW, xpath ) == NULL" );
+  } else
+  { PRINT_SUB( oyjlTESTRESULT_FAIL,
+    "oyjlTreeGetValue( NULL, flags=OYJL_CREATE_NEW, xpath ) == NULL" );
+  }
+
+  root = oyjlTreeNew( NULL );
+  if(root)
+  { PRINT_SUB( oyjlTESTRESULT_SUCCESS,
+    "oyjlTreeNew( NULL )                     " );
+  } else
+  { PRINT_SUB( oyjlTESTRESULT_FAIL,
+    "oyjlTreeNew( NULL )                     " );
+  }
+  oyjlTreeFree( root );
+
+  root = oyjlTreeNew( "new/tree/key" );
+  char * rjson = NULL; i = 0;
+  oyjlTreeToJson( root, &i, &rjson ); i = 0;
+  size_t len = strlen(rjson);
+  if(root)
+  { PRINT_SUB( oyjlTESTRESULT_SUCCESS,
+    "oyjlTreeNew( \"new/tree/key\" )       %d", (int)len );
+  } else
+  { PRINT_SUB( oyjlTESTRESULT_FAIL,
+    "oyjlTreeNew( \"new/tree/key\" )       %d", (int)len );
+  }
+  fprintf( zout, "%s\n", rjson );
+  myDeAllocFunc( rjson ); rjson = NULL;
+
+  value = oyjlTreeGetValue( root, 0, "new/tree/key" );
+#define VALUE "value-\"one\""
+  oyjlValueSetString( value, VALUE );
+  oyjlTreeToJson( root, &i, &rjson ); i = 0;
+  if(len < strlen(rjson))
+  { PRINT_SUB( oyjlTESTRESULT_SUCCESS,
+    "oyjlValueSetString( %s )         %d", VALUE, (int)strlen(rjson) );
+  } else
+  { PRINT_SUB( oyjlTESTRESULT_FAIL,
+    "oyjlValueSetString( %s )         %d", VALUE, (int)strlen(rjson) );
+  }
+  fprintf( zout, "%s\n", rjson );
+  len = strlen(rjson);
+  myDeAllocFunc( rjson ); rjson = NULL;
+
+  char * v = oyjlValueText(value, myAllocFunc);
+  if(v && strlen(v) == strlen(VALUE))
+  { PRINT_SUB( oyjlTESTRESULT_SUCCESS,
+    "oyjlValueText( \"new/tree/key\" ) = %s", v );
+  } else
+  { PRINT_SUB( oyjlTESTRESULT_FAIL,
+    "oyjlValueText( \"new/tree/key\" ) = %s", VALUE );
+  }
+  myDeAllocFunc(v);
+
+  oyjlTreeClearValue( root,"new/tree/key" );
+  oyjlTreeToJson( root, &i, &rjson ); i = 0;
+  if(!rjson)
+  { PRINT_SUB( oyjlTESTRESULT_SUCCESS,
+    "oyjlTreeClearValue( \"new/tree/key\" ) " );
+  } else
+  { PRINT_SUB( oyjlTESTRESULT_FAIL,
+    "oyjlTreeClearValue( \"new/tree/key\" ) " );
+  }
+  oyjlTreeFree( root );
+
+  return result;
+}
 
 
 /* --- end actual tests --- */
