f8f33462d778ff24b2c3b823c3fcfe0d81541c57
Kai-Uwe Behrmann <ku.b@gmx.de>
2018-08-26 21:32:32 +0200

    * [oJPG]: support write of ICC_PROFILE and comment markers

--- src/modules/color/modules/oyranos_cmm_oJPG.c
+++ src/modules/color/modules/oyranos_cmm_oJPG.c
@@ -936,6 +936,91 @@ oyCMMapi_s * ojpgApi4CmmCreateWrite  ( const char        * format )
   return (oyCMMapi_s*)cmm4;
 }
 
+/* Start - added from Marti Marias little cms library  */
+
+/*
+ * Since an ICC profile can be larger than the maximum size of a JPEG marker
+ * (64K), we need provisions to split it into multiple markers.  The format
+ * defined by the ICC specifies one or more APP2 markers containing the
+ * following data:
+ *	Identifying string	ASCII "ICC_PROFILE\0"  (12 bytes)
+ *	Marker sequence number	1 for first APP2, 2 for next, etc (1 byte)
+ *	Number of markers	Total number of APP2's used (1 byte)
+ *      Profile data		(remainder of APP2 data)
+ * Decoders should use the marker sequence numbers to reassemble the profile,
+ * rather than assuming that the APP2 markers appear in the correct sequence.
+ */
+
+#define ICC_MARKER  (JPEG_APP0 + 2)	/* JPEG marker code for ICC */
+#define ICC_OVERHEAD_LEN  14		/* size of non-profile data in APP2 */
+#define MAX_BYTES_IN_MARKER  65533	/* maximum data len of a JPEG marker */
+#define MAX_DATA_BYTES_IN_MARKER  (MAX_BYTES_IN_MARKER - ICC_OVERHEAD_LEN)
+
+
+/*
+ * This routine writes the given ICC profile data into a JPEG file.
+ * It *must* be called AFTER calling jpeg_start_compress() and BEFORE
+ * the first call to jpeg_write_scanlines().
+ * (This ordering ensures that the APP2 marker(s) will appear after the
+ * SOI and JFIF or Adobe markers, but before all else.)
+ */
+
+void
+write_icc_profile (j_compress_ptr cinfo,
+		   const JOCTET *icc_data_ptr,
+		   unsigned int icc_data_len)
+{
+  unsigned int num_markers;	/* total number of markers we'll write */
+  int cur_marker = 1;		/* per spec, counting starts at 1 */
+  unsigned int length;		/* number of bytes to write in this marker */
+
+  /* Calculate the number of markers we'll need, rounding up of course */
+  num_markers = icc_data_len / MAX_DATA_BYTES_IN_MARKER;
+  if (num_markers * MAX_DATA_BYTES_IN_MARKER != icc_data_len)
+    num_markers++;
+
+  while (icc_data_len > 0) {
+    /* length of profile to put in this marker */
+    length = icc_data_len;
+    if (length > MAX_DATA_BYTES_IN_MARKER)
+      length = MAX_DATA_BYTES_IN_MARKER;
+    icc_data_len -= length;
+
+    /* Write the JPEG marker header (APP2 code and marker length) */
+    jpeg_write_m_header(cinfo, ICC_MARKER,
+			(unsigned int) (length + ICC_OVERHEAD_LEN));
+
+    /* Write the marker identifying string "ICC_PROFILE" (null-terminated).
+     * We code it in this less-than-transparent way so that the code works
+     * even if the local character set is not ASCII.
+     */
+    jpeg_write_m_byte(cinfo, 0x49);
+    jpeg_write_m_byte(cinfo, 0x43);
+    jpeg_write_m_byte(cinfo, 0x43);
+    jpeg_write_m_byte(cinfo, 0x5F);
+    jpeg_write_m_byte(cinfo, 0x50);
+    jpeg_write_m_byte(cinfo, 0x52);
+    jpeg_write_m_byte(cinfo, 0x4F);
+    jpeg_write_m_byte(cinfo, 0x46);
+    jpeg_write_m_byte(cinfo, 0x49);
+    jpeg_write_m_byte(cinfo, 0x4C);
+    jpeg_write_m_byte(cinfo, 0x45);
+    jpeg_write_m_byte(cinfo, 0x0);
+
+    /* Add the sequencing info */
+    jpeg_write_m_byte(cinfo, cur_marker);
+    jpeg_write_m_byte(cinfo, (int) num_markers);
+
+    /* Add the profile data */
+    while (length--) {
+      jpeg_write_m_byte(cinfo, *icc_data_ptr);
+      icc_data_ptr++;
+    }
+    cur_marker++;
+  }
+}
+/* End - added from Marti Marias little cms library  */
+
 int          oyImage_WriteJPEG       ( oyImage_s         * image,
                                        const char        * filename,
                                        oyOptions_s       * options )
@@ -956,6 +1041,7 @@ int          oyImage_WriteJPEG       ( oyImage_s         * image,
   //int cchan_n = oyProfile_GetChannelsCount( prof );
   oyDATATYPE_e data_type = oyToDataType_m( pixel_layout );
   int stride = image_width * channels_n;
+  char * comment = (char*) oyOptions_FindString( options, "comment", 0 );;
 
   struct jpeg_compress_struct cinfo;
   /* This struct represents a JPEG error handler.  It is declared separately
@@ -1061,6 +1147,21 @@ int          oyImage_WriteJPEG       ( oyImage_s         * image,
    * Pass TRUE unless you are very sure of what you're doing.
    */
   jpeg_start_compress(&cinfo, TRUE);
+
+  /* 4b: Write ICC profile */
+  {
+    void * pmem = 0;
+    size_t psize = 0;
+    pmem = oyProfile_GetMem( prof, &psize, 0,0 );
+    write_icc_profile( &cinfo, (JOCTET*)pmem, psize );
+    /*jpeg_write_marker_APP( &cinfo, JPEG_APP0+2, (JOCTET*)"ICC_PROFILE", 12,
+                           (JOCTET*)pmem, psize ); -- appears not to work */
+    if(oy_debug)
+      ojpg_msg( oyMSG_DBG, (oyStruct_s*)image, OY_DBG_FORMAT_ "embedd profile: %lu", OY_DBG_ARGS_, psize);
+  }
+
+  if(comment)
+    jpeg_write_marker(&cinfo, JPEG_COM, (const JOCTET *)comment, strlen(comment));
 
   /* Step 5: while (scan lines remain to be written) */
   /*           jpeg_write_scanlines(...); */
