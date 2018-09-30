62b1ac363507269dbe59d0cb1001e1a01b75e5f9
Kai-Uwe Behrmann <ku.b@gmx.de>
2018-09-30 20:21:04 +0200

    * [templates]: improve FilterCore object info robustness

--- src/templates/objects_conversion/oyFilterCore_s_.template.c
+++ src/templates/objects_conversion/oyFilterCore_s_.template.c
@@ -9,19 +9,19 @@
 
 {% block customStaticMessage %}
 {{ block.super }}
-  if(type == oyNAME_NICK && (flags & 0x01))
+  if(type == oyNAME_NICK && (flags & 0x01) && s->category_)
   {
     sprintf( &text[strlen(text)], "%s",
              s->category_
            );
   } else
-  if(type == oyNAME_NAME)
+  if(type == oyNAME_NAME && (s->category_ || s->registration_))
     sprintf( &text[strlen(text)], "%s %s",
-             s->category_, s->registration_
+             s->category_?s->category_:"", s->registration_?s->registration_:""
            );
   else
-  if((int)type >= oyNAME_DESCRIPTION)
+  if((int)type >= oyNAME_DESCRIPTION && (s->category_ || s->registration_))
     sprintf( &text[strlen(text)], "category: %s\nreg: %s",
-             s->category_, s->registration_
+             s->category_?s->category_:"", s->registration_?s->registration_:""
            );
 {% endblock %}
