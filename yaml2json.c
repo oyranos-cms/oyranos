3d22557af82eae4251746e0a1f8b5b024dfa7470
Kai-Uwe Behrmann <ku.b@gmx.de>
2019-01-08 23:02:35 +0100

    * [oyjl]: simplify tool by using internal YAML API

--- yaml2json.c
+++ yaml2json.c
@@ -1,5 +1,3 @@
-#include <yaml.h>
-
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -8,103 +6,10 @@
 #endif
 #include <assert.h>
 
-#include <stdio.h>
-#include <yaml.h>
+#include "oyjl_version.h"
 #include "oyjl.h"
 
-
-int getCount( yaml_node_t * n )
-{
-  int i = 0;
-  switch( n->type )
-  {
-    case YAML_SCALAR_NODE: i = n->data.scalar.length; break;
-    case YAML_SEQUENCE_NODE: i = n->data.sequence.items.top - n->data.sequence.items.start; break;
-    case YAML_MAPPING_NODE: i = n->data.mapping.pairs.top - n->data.mapping.pairs.start; break;
-    default: break;
-  }
-  return i;
-}
-
-int getId( yaml_node_t * n, int index, int key )
-{
-  int id = 0;
-  if( n->type == YAML_SEQUENCE_NODE )
-    id = n->data.sequence.items.start[index];
-
-  if( n->type == YAML_MAPPING_NODE )
-  {
-    if(key == 1)
-      id = n->data.mapping.pairs.start[index].key;
-    else
-      id = n->data.mapping.pairs.start[index].value;
-  }
-
-  return id;
-}
-
-int number_detection = 1;
-int readNode( yaml_document_t * doc, yaml_node_t * node, int is_key, char ** json )
-{
-  int error = 0;
-  int count = getCount( node ), i;
-  if( node->type == YAML_SCALAR_NODE )
-  {
-    char * t = (char*)node->data.scalar.value, * tmp = NULL;
-    if(t && strstr(t, ":\\ "))
-      t = tmp = oyjlStringReplace( t, ":\\ ", ": ", 0, 0);
-    if(t)
-    {
-      double d;
-      int err = -1;
-      if(number_detection && is_key != 1)
-        err = oyjlStringToDouble( t, &d );
-      if(err == 0)
-        oyjlStringAdd( json, 0,0, "%s", t );
-      else
-        oyjlStringAdd( json, 0,0, "\"%s\"", t );
-    }
-    if(tmp) free(tmp);
-  }
-  if( node->type == YAML_SEQUENCE_NODE )
-  {
-    oyjlStringAdd( json, 0,0, "[");
-    for(i = 0; i < count && !error; ++i)
-    {
-      int id = getId( node, i, 0 );
-      yaml_node_t * n =
-      yaml_document_get_node( doc, id );
-      error = readNode(doc, n, 0, json);
-      if(i < count - 1) oyjlStringAdd( json, 0,0, ",");
-    }
-    oyjlStringAdd( json, 0,0, "]");
-  }
-  if( node->type == YAML_MAPPING_NODE )
-    for(i = 0; i < count; ++i)
-    {
-      int key_id = getId( node, i, 1 );
-      int val_id = getId( node, i, 0 );
-      yaml_node_t * key =
-      yaml_document_get_node( doc, key_id );
-      yaml_node_t * val =
-      yaml_document_get_node( doc, val_id );
-
-      if(i == 0) oyjlStringAdd( json, 0,0, "{");
-
-      error = readNode(doc, key, 1, json);
-      if( key->type == YAML_SCALAR_NODE &&
-          !error )
-      {
-        oyjlStringAdd( json, 0,0, ":");
-      }
-
-      error = readNode(doc, val, 0, json);
-      if(i < count - 1) oyjlStringAdd( json, 0,0, ",");
-      else if( i == count - 1 ) oyjlStringAdd( json, 0,0, "}");
-    }
-  return error;
-}
-
+int number_detection = 0;
 int main(int argc, char *argv[])
 {
     int number;
@@ -126,13 +31,12 @@ int main(int argc, char *argv[])
     for (number = start + 1; number < argc && !error; number ++)
     {
         FILE *fp;
-        yaml_parser_t parser;
-        yaml_document_t document;
-        yaml_node_t * root = NULL;
+
         char * json = NULL, * text = NULL;
-        oyjl_val jroot;
+        oyjl_val jroot = NULL;
         char error_buffer[256] = {0};
         int level = 0;
+        int size = 0;
 
         if(strcmp(argv[number],"-") == 0)
           fp = stdin;
@@ -140,23 +44,17 @@ int main(int argc, char *argv[])
           fp = fopen(argv[number], "rb");
         assert(fp);
 
-        assert(yaml_parser_initialize(&parser));
+        text = oyjlReadFileStreamToMem( fp, &size );
 
-        yaml_parser_set_input_file(&parser, fp);
+        jroot = oyjlTreeParseYaml( text, number_detection ? OYJL_NUMBER_DETECTION : 0, error_buffer, 256 );
 
-        yaml_parser_load(&parser, &document);
-        root = yaml_document_get_root_node(&document);
-        error = readNode( &document, root, 1, &json );
+        if(error_buffer[0] != 0)
+          fprintf( stderr, "%s: %s\n", argv[number], "" );
 
         /* reformat */
-        jroot = oyjlTreeParse( json, error_buffer, 256 );
-        if(error_buffer[0] != '\000')
-            fprintf(stderr, "ERROR:\t\"%s\"\n", error_buffer);
-        oyjlTreeToJson( jroot, &level, &text );
-        puts( text );
-
-        yaml_parser_delete(&parser);
-        yaml_document_delete(&document);
+        oyjlTreeToJson( jroot, &level, &json );
+        if( json )
+          puts( json );
 
         if(fp != stdin)
           assert(!fclose(fp));
