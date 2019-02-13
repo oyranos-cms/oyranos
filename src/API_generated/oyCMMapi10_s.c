491733bff6ba67a85cf9798e6797ee4748eceec5
Kai-Uwe Behrmann <ku.b@gmx.de>
2019-02-13 08:09:53 +0100

    * [sources]: add oyCMMapi10_Create() API
    ... for dynamic options handle module creation

--- src/API_generated/oyCMMapi10_s.c
+++ src/API_generated/oyCMMapi10_s.c
@@ -181,6 +181,69 @@ OYAPI oyPointer_s * OYEXPORT
 
 
 /* Include "CMMapi10.public_methods_definitions.c" { */
+/** Function    oyCMMapi10_Create
+ *  @memberof   oyCMMapi10_s
+ *  @brief      Custom CMMapi10 constructor
+ *
+ *  @param         init                custom initialisation
+ *  @param         msg_set             message function setter
+ *  @param         registration        the modules @ref registration string,
+ *  it shall contain the command name, so it can be selected
+ *  @param         version             module version
+ *  - 0: major - should be stable for the live time of a filter
+ *  - 1: minor - mark new features
+ *  - 2: patch version - correct errors
+ *  @param         module_api          tell compatibility with Oyranos API
+ *  - 0: last major Oyranos version during development time, e.g. 0
+ *  - 1: last minor Oyranos version during development time, e.g. 9
+ *  - 2: last Oyranos patch version during development time, e.g. 5
+ *  @param         getText             for filter help descriptions;
+ *  It shall at least contain "can_handle", "help" and the command name as
+ *  used in the registration string
+ *  @param         texts               types for getText member
+ *  @param         oyMOptions_Handle   the function pointer to do the work
+ *  @param         object              a optional object
+ *
+ *  @version Oyranos: 0.9.7
+ *  @since   2019/02/07 (Oyranos: 0.9.7)
+ *  @date    2019/02/07
+ */
+OYAPI oyCMMapi10_s*  OYEXPORT
+                   oyCMMapi10_Create ( oyCMMInit_f         init,
+                                       oyCMMMessageFuncSet_f msg_set,
+                                       const char        * registration,
+                                       int32_t             version[3],
+                                       int32_t             module_api[3],
+                                       oyCMMGetText_f      getText,
+                                       const char       ** texts,
+                                       oyMOptions_Handle_f oyMOptions_Handle,
+                                       oyObject_s          object )
+{
+  oyCMMapi10_s_ * api10 = (oyCMMapi10_s_*) oyCMMapi10_New( object ); 
+  if(!api10)
+  {
+    WARNc_S(_("MEM Error."));
+    return NULL;
+  }
+
+  oyCMMapi_Set( (oyCMMapi_s*) api10, init, msg_set, registration,
+                version, module_api );
+
+  api10->getText = getText;
+  {
+    int n = 0, i;
+    char ** list;
+    while(texts && texts[n]) ++n;
+    list = oyAllocateFunc_(sizeof(char*)*(n+1));
+    for(i = 0; i < n; ++i)
+      list[i] = oyjlStringCopy(texts[i], oyAllocateFunc_);
+    api10->texts = list;
+  }
+  api10->oyMOptions_Handle = oyMOptions_Handle;
+
+  return (oyCMMapi10_s*) api10;
+}
+
 
 /* } Include "CMMapi10.public_methods_definitions.c" */
 
---
+++ src/sources/CMMapi10.public_methods_declarations.h
@@ -0,0 +1,56 @@
+#include "oyOptions_s.h"
+/** typedef  oyMOptions_Handle_f
+ *  @brief   generic data exchange
+ *  @ingroup module_api
+ *  @memberof oyCMMapi10_s
+ *
+ *  The provided data passed with oyOptions_s shall be processed according to
+ *  the rules of a given command. The result is again a generic oyOptions_s
+ *  object.
+ *
+ *  This module type works similiar to the device modules, which are defined in
+ *  oyCMMapi8_s. While the module interface is simple it has minimal meaning to
+ *  the intented work or data exchange. The rules are to be defined by the
+ *  communicating parties, the host and the module. They use the plug-in
+ *  mechanism only for their convinience.
+ *
+ *  For speed up the "can_handle.xxx" command must be implemented. "xxx" means
+ *  in the previous command string the intented command to be performed. The
+ *  "can_handle" command shall not produce any output in result, but shall
+ *  indicate with zero that it could handle the request. This command is
+ *  intented to lightwight select a capable module.
+ *
+ *  @param[in]     options             the input data; It is typical not
+ *                                     changed.
+ *  @param[in]     command             a string describing the intention of the
+ *                                     caller; Allowed are alphanumerical signs.
+ *                                     lower bar '_', minus '-', plus '+' and
+ *                                     point '.' . other signs including white
+ *                                     space must not occure.
+ *                                     Strings are separated by point '.' .
+ *                                     The first string is the actual command.
+ *                                     The following strings are merely
+ *                                     modifiers.
+ *  @param[out]    result              the created data
+ *  @return                            0 - indifferent, >= 1 - error,
+ *                                     <= -1 - issue,
+ *                                     + a message should be sent
+ *
+ *  @version Oyranos: 0.1.10
+ *  @since   2009/12/11 (Oyranos: 0.1.10)
+ *  @date    2009/12/11
+ */
+typedef int  (*oyMOptions_Handle_f)  ( oyOptions_s       * options,
+                                       const char        * command,
+                                       oyOptions_s      ** result );
+#include "oyCMMapi_s.h"
+OYAPI oyCMMapi10_s*  OYEXPORT
+                   oyCMMapi10_Create  ( oyCMMInit_f         init,
+                                       oyCMMMessageFuncSet_f msg_set,
+                                       const char        * registration,
+                                       int32_t             version[3],
+                                       int32_t             module_api[3],
+                                       oyCMMGetText_f      getText,
+                                       const char       ** texts,
+                                       oyMOptions_Handle_f oyMOptions_Handle,
+                                       oyObject_s          object );
