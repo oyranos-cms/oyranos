ceb7d18f96d1badfcf3efc150e232640919d208f
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-01-13 10:07:56 +0100

    * [templates]: put static msg into right file

--- src/templates/module_api/oyCMMui_s_.template.c
+++ src/templates/module_api/oyCMMui_s_.template.c
@@ -1,4 +1,43 @@
 {% extends "Base_s_.c" %}
+{% block customStaticMessage %}
+{{ block.super }}
+  /* allocate enough space */
+  int l = 0;
+  if(s->options)
+    l = strlen(s->options);
+  if(oy_{{ class.baseName|lower }}_msg_text_n_ < l+80)
+  {
+    oyDeAlloc_f dealloc = oyDeAllocateFunc_;
+    if(oy_{{ class.baseName|lower }}_msg_text_ && oy_{{ class.baseName|lower }}_msg_text_n_)
+      dealloc( oy_{{ class.baseName|lower }}_msg_text_ );
+    oy_{{ class.baseName|lower }}_msg_text_n_ = l+80;
+    oy_{{ class.baseName|lower }}_msg_text_ = alloc(oy_{{ class.baseName|lower }}_msg_text_n_);
+    if(oy_{{ class.baseName|lower }}_msg_text_)
+      oy_{{ class.baseName|lower }}_msg_text_[0] = '\000';
+    else
+      return "Memory Error";
+
+    if(!(flags & 0x01))
+      sprintf(oy_{{ class.baseName|lower }}_msg_text_, "%s%s", oyStructTypeToText( s->type_ ), type != oyNAME_NICK?" ":"");
+  }
+
+  if(type == oyNAME_NICK && (flags & 0x01))
+  {
+    sprintf( &oy_{{ class.baseName|lower }}_msg_text_[strlen(oy_{{ class.baseName|lower }}_msg_text_)], "%s",
+             s->category
+           );
+  } else
+  if(type == oyNAME_NAME)
+    sprintf( &oy_{{ class.baseName|lower }}_msg_text_[strlen(oy_{{ class.baseName|lower }}_msg_text_)], "category: %s",
+             s->category
+           );
+  else
+  if((int)type >= oyNAME_DESCRIPTION)
+    sprintf( &oy_{{ class.baseName|lower }}_msg_text_[strlen(oy_{{ class.baseName|lower }}_msg_text_)], "category: %s options:\n%s",
+             s->category,
+             s->options
+           );
+{% endblock %}
 
 {% block refCount %}
   {
@@ -7,7 +46,7 @@
 
     /* references from members has to be substracted
      * from this objects ref count */
-    if(oyObject_GetRefCount( s->oy_ ) > (int)(ui_p + observer_refs))
+    if(oyObject_GetRefCount( s->oy_ ) > (int)(parent_refs + ui_p + observer_refs*2))
       return 0;
   }
 {% endblock %}
