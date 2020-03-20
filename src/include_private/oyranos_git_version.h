97ace32f8d91d8f1b07a04ef76db531a36a2a630
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-03-20 07:23:52 +0100

    * [oyjl]: fix C++ compile error of OYJL_TEST_WRITE_RESULT macro

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2159-g33f7378a"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2160-g561d9d88"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
