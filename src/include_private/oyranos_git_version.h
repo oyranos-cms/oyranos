561d9d8813796b81e0225b968446444928402457
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-03-20 07:23:16 +0100

    * [oyjl]: improve parser error message in oyjl tool

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2158-ge8b3ba14"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2159-g33f7378a"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
