579c4bd6447772098054811175ba67a4866dc2df
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-03-27 07:30:14 +0100

    * [oyjl]: put sources guard around ancestor code
    This code is inherited from a old yajl_tree implementation.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2187-g707f9eba"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2187-gfcff7f1c"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
