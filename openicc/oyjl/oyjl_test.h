a0ced8f780726cd61143309fcafb70903d52b960
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-04-18 08:01:39 +0200

    * [oyjl]: fix long names in OYJL_TEST_WRITE_RESULT macro

--- openicc/oyjl/oyjl_test.h
+++ openicc/oyjl/oyjl_test.h
@@ -2,7 +2,7 @@
  *
  *  Oyranos is an open source Color Management System 
  *
- *  Copyright (C) 2004-2019  Kai-Uwe Behrmann
+ *  Copyright (C) 2004-2020  Kai-Uwe Behrmann
  *
  *  @brief    Oyranos test suite
  *  @internal
@@ -402,16 +402,16 @@ int  oyjlWriteTestFile               ( const char        * filename,
  *  @param         suffix              file name suffix
  */
 #define OYJL_TEST_WRITE_RESULT( mem, size, hint, suffix ) if(mem) { \
-  char * fn = (char*)malloc(64); \
+  char * fn = (char*)malloc((hint?strlen(hint):0)+64); \
   sprintf( fn, "%s-%d-%d-%s-%s.%s", OYJL_TEST_NAME, oyjl_test_number, oy_test_current_sub_count, hint?hint:"", oyjlTestResultToString(oy_test_last_result,0), suffix ); \
   oyjlWriteTestFile( fn, mem, size ); \
   if(verbose) fprintf(zout, "%s\n", fn); \
   if(oy_test_last_result == oyjlTESTRESULT_FAIL) { \
-    char * fns = (char*)malloc(64); \
+    char * fns = (char*)malloc((hint?strlen(hint):0)+64); \
     sprintf( fns, "%s-%d-%d-%s-%s.%s", OYJL_TEST_NAME, oyjl_test_number, oy_test_current_sub_count, hint?hint:"", oyjlTestResultToString(oyjlTESTRESULT_SUCCESS,0), suffix ); \
     FILE * fp = fopen(fns, "r"); \
     if(fp && strcmp(suffix, "txt") == 0) { \
-      char * diff = (char*)malloc(128); \
+      char * diff = (char*)malloc((hint?strlen(hint)*2:0)+128); \
       sprintf( diff, "diff -aur %s %s", fns, fn ); \
       system(diff); \
       free(diff); \
