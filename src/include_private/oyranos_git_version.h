3fb264a5204f7581c6d0ee9039feb42cb66ab4b5
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-04-25 07:41:43 +0200

    * [oyjl]: print only synopsis help on completely missing args
    The whole help text might be too long and hiding the top error info.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2237-g05c01f3b"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2238-gff4060b4"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
