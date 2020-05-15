353d7b3fc6d34702b63ce611afc62409f3e2e4e8
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-05-15 11:23:02 +0200

    * [oyjl]: use pkg-config only for path hints in FindYajl.cmake

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2262-g6bd30530"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2263-g0e08650a"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
