8ae5537f304539e9f304fdd243560e4d4ff97dad
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-05-18 11:02:11 +0200

    * [oyjl]: build oyjl-args-qml on Android
    ... not (yet) as APK

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2270-gad17f8a0"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2271-g7a6468f8"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
