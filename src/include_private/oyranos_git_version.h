5b827e111f9ffb5d12e17e1227b374e608507a8f
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-05-26 10:10:45 +0200

    * [oyjl]: use oyjlMessage_p for error in oyjl tool

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2274-g50c59e2c"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2275-g1c0dcbe5"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
