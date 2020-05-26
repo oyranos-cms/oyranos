47f909cc8568c987b8f27b1240748ee8f7175128
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-05-26 10:16:11 +0200

    * [openicc]: reenable dynamic link
    was broken by commit #7522146e

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2276-g5b827e11"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2277-g5984d0c5"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
