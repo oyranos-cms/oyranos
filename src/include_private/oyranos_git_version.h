50bc2c08fe8f66cb88f3062e5918b49e0738ebf5
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-05-27 12:20:58 +0200

    * [oyjl]: fix clang compile warning in oyjlOptions_New()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2277-g5984d0c5"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2278-g47f909cc"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
