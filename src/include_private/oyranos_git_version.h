0336b592bfdb45673f7b4dd0d339f025a8c3c934
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-06-22 21:12:06 +0200

    * [oyjl]: add pkg-config file

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2310-ga2bd9e23"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2311-g69404fbc"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
