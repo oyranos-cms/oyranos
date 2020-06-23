001220af184ec5d316806784eb4dbaa66faccc0c
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-06-23 13:44:08 +0200

    * [oyjl]: link all dependencies into libOyjl
    fixes compiling -lOyjl without specified yaml/xml2/yajl

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2311-g69404fbc"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2312-g0336b592"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
