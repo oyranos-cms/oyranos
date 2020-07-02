fa534a8e91ad9c99f0842b5ecfb792327c09df29
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-07-02 12:11:43 +0200

    * [sources]: fix oyEstimateTemperature() compile warning

--- src/templates/objects_profile/oyProfile_s.template.c
+++ src/templates/objects_profile/oyProfile_s.template.c
@@ -5,6 +5,7 @@
 #include "oyranos_devices_internal.h"
 #include "oyranos_cache.h"
 #include "oyranos_color.h"
+#include "oyranos_color_internal.h"
 #include "oyranos_io.h"
 #include "oyranos_icc.h"
 #include "oyConfig_s_.h"
