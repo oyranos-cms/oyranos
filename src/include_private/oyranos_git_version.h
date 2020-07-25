19d714161c004e73c0c693fe84ea398ed1b31446
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-07-25 08:15:31 +0200

    * [conf]: fix libm case
    introduced recently in #3bf7cb59

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2338-g5ebda47f"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2339-g4c1e253c"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
