8b865f592c32420ace9f57d52916c0dd9845d19c
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-07-28 13:11:44 +0200

    * [oyjl]: add public oyjlLanguage() oyjlCountry()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2341-g0fa9ba6a"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2342-gec49846f"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
