cf13ca3bdfa047548a51c476c9076d78debef035
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-07-29 12:39:35 +0200

    * [oyjl]: add oyjlLang() API
    ... for statically storing locale

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2347-gc8027388"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2348-gf3afa598"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
