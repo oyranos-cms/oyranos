c80273884b72ffc03c9287621dcab13dbd93d5c9
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-07-29 12:36:29 +0200

    * [oyjl]: simplify non argument case for VA macro

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2345-gc35b00ee"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2346-ge0087e24"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
