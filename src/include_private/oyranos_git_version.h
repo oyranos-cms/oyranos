87587e035748f18d179c37d04c7436df8b5b2d7a
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-08-18 06:59:24 +0200

    * [oyjl]: fix release of stored data in oyjlLang()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2362-gbf18f180"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2363-g67c22851"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
