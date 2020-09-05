5d62115a38c80d19e93549718cd429a2112c0c12
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-09-05 08:12:21 +0200

    * [conf]: extract multiple languages to JSON

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2364-g87587e03"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2367-g1090313b"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
