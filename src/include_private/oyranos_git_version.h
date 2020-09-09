0ba0133c6060f1cd070e5584d58ad40e455e0262
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-09-09 21:48:57 +0200

    * [oyjl]: print tick for double options help text

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2371-g2dd5d4a9"  /**< git describe --always HEAD */ 
+#define OY_GIT_VERSION                 "0.9.6-2372-g18f8d537"  /**< git describe --always HEAD */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
