c6b23f592e434aa5e2db3ceedc3812a7e36efd8d
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-11-02 07:46:18 +0100

    * [oyjl]: fix args error repetition
    must come from last week changes

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2409-gdde1eeb4-2020-10-31"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2410-g7eb14788-2020-11-01"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
