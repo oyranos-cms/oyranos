e4c6bf6d3ad59bd3426bd0bae40e428c3f589a08
Kai-Uwe Behrmann <ku.b@gmx.de>
2020-11-14 11:17:42 +0100

    * [tools]: update generated bash completions

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2415-g95197a1b-2020-11-14"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2416-g0a1c1036-2020-11-14"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
