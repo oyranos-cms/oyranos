f8a33e93d7c94050ac3b08338cfe50620768d26e
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-02-02 16:51:46 +0100

    * [oyjl]: translate help completion dynamicaly
    The completion files contain only static translation. With
    oyjlOPTIONTYPE_FUNCTION as shown in oyjl.c the completion list
    is generated dynamicaly and thus potentially translated.

--- openicc/oyjl/oyjl.c
+++ openicc/oyjl/oyjl.c
@@ -137,7 +137,7 @@ int myMain( int argc, const char ** argv )
     {"oiwi", 0,                          "S","man-see_also",  NULL,     _("SEE ALSO"),NULL,                      NULL, NULL,
         oyjlOPTIONTYPE_CHOICE,   {.choices.list = (oyjlOptionChoice_s*) oyjlStringAppendN( NULL, (const char*)S_choices, sizeof(S_choices), malloc )}, oyjlNONE,      {}},
     /* default options -h and -v */
-    {"oiwi", OYJL_OPTION_FLAG_ACCEPT_NO_ARG, "h", "help", NULL,NULL, NULL, NULL,NULL, oyjlOPTIONTYPE_CHOICE, {0}, oyjlSTRING, {.s=&help} },
+    {"oiwi", OYJL_OPTION_FLAG_ACCEPT_NO_ARG, "h", "help", NULL,NULL, NULL, NULL,NULL, oyjlOPTIONTYPE_FUNCTION, {0}, oyjlSTRING, {.s=&help} },
     {"oiwi", 0, NULL, "synopsis", NULL, NULL, NULL,      NULL,     NULL, oyjlOPTIONTYPE_NONE, {0}, oyjlNONE, {0} },
     {"oiwi", 0, "v","verbose",NULL,_("Verbose"),_("increase verbosity"), NULL, NULL, oyjlOPTIONTYPE_NONE,{0},oyjlINT,{.i=&verbose}},
     /* The --render option can be hidden and used only internally. */
