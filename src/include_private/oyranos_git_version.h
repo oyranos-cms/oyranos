b1a37a916653c25b8bca6825965552f1ee22162e
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-02-26 09:19:08 +0100

    * [oyjl]: update internal UI date

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2432-g651ce414-2021-02-02"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2433-gc5d8ecfa-2021-02-10"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
