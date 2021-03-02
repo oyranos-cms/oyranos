ae6014e195819029e1c6be3f03e1f841c7913e93
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-03-02 18:00:33 +0100

    * [oyjl]: detect "execinfo.h"

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2436-ga5dc1068-2021-02-26"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2438-g4bf2cbd1-2021-03-02"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
