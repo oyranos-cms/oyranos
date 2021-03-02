fd5c0001a98e0b466409704f2b3fa32a4edd6a68
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-03-02 20:03:59 +0100

    * [oyjl]: allow for sub command option args
    Lifts the restriction of single argless mandatory option for sub command groups.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2439-g0b006b62-2021-03-02"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2440-g37a11cba-2021-03-02"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
