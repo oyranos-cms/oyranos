2d436cb762fb836f211a171ff597d68e947ce8a0
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-03-03 08:52:37 +0100

    * [oyjl]: improve subcommand error detection

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2441-gfd5c0001-2021-03-02"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2442-g5294d453-2021-03-02"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
