b5f7c5d36090f383ab8f04bed5a5878bf678786e
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-04-13 09:57:06 +0200

    * [oyjl]: print verbose message for OYJL_DEBUG_NODE detection

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2456-g5b93c0a9-2021-04-13"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2457-gf3e1d081-2021-04-13"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
