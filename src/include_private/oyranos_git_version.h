5b93c0a9c4c35dbabfaccdd86547f6f8fc2857b8
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-04-13 09:54:17 +0200

    * [oyjl]: use diff for all OYJL_TEST_WRITE_RESULT suffixes
    close mem leak

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2454-gc8786533-2021-04-08"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2455-gc854e069-2021-04-08"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
