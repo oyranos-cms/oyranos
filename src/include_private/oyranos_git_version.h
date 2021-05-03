47ae71274a259ddc01e5a4f3238447cb7b8a0403
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-05-03 14:59:24 +0200

    * [oyjl]: omit difficult synopsis format

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2463-g763c07b5-2021-04-30"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2464-g21ec919d-2021-04-30"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
