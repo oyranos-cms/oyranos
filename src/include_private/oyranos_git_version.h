d69c4117987ab969cd741a1cea0f959cd7f372b6
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-05-14 07:06:18 +0200

    * [conf]: update devel year

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2467-gfd1ccb0c-2021-05-03"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2468-g574c5472"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
