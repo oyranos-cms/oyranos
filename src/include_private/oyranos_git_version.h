3dcc3fe1b12a6ead569211a895c8d0211e18d3ae
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-05-26 13:29:24 +0200

    * [oyjl]: add oyjl_internal.h.in for OYJL_SOURCEDIR
    ... and OYJL_BUILDDIR. These variables are used to relyably test
    command line tools output.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2485-gecbe30b5-2021-05-26"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2485-gccf17adb-2021-05-26"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
