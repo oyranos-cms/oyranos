5258ba823fd354a6d3cb37651dd442082e9da24e
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-05-26 13:16:58 +0200

    * [oyjl]: fix memory overrun in oyjlPrintTime()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2481-g560d4600-2021-05-26"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2482-g46395cb4-2021-05-26"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
