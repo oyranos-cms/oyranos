560d460047f0f75561f4849fde62783195ef89ec
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-05-26 13:07:45 +0200

    * [oyjl]: fix oyjlTranslate() matching
    Previously the function matched partitial occurences.
    The complete term is required to match with this change.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2478-ga6cc02fa-2021-05-21"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2480-gff5c85c1-2021-05-21"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
