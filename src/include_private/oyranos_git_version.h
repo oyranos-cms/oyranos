35990178defa5ffd8fe5b312abd424f651cbad86
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-06-08 20:20:47 +0200

    * [oyjl]: add oyjl-args --test option for UI JSON

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2498-gc1497365-2021-06-04"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2499-g21c6d205-2021-06-08"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
