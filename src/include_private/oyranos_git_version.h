d611d1b891fadd42ea11aefc8d1927346b6d8607
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-06-12 19:51:38 +0200

    * [oyjl]: support base args with oyjlNONE variable
    for prog -V -v options
    for -h is was alread implemented

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2515-g01e90991-2021-06-12"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2515-g76603bf1-2021-06-12"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
