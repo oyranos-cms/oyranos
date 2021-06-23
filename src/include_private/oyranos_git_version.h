0f3d96666eab615f458ef4c8449309ba0b8bda0d
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-06-23 10:20:36 +0200

    * [oyjl]: do not generate model entries on get in QML

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2525-g91e6ee11-2021-06-19"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2526-g0f3f2b62-2021-06-23"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
