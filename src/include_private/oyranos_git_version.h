e617bae59c6044d66010ac5659d2a7a712b04e37
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-06-29 09:27:25 +0200

    * [oyjl]: check for no args in oyjlManAddOptionToGroupList_()
    ... and test

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2527-g0f3d9666-2021-06-23"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2528-g3cb19403-2021-06-23"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
