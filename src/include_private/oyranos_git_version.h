781fa3f33e3791b28db4aa6f8aeffbaf066db8a8
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-06-30 11:29:40 +0200

    * [oyjl]: add OYJL_GET_RESULT_XXX macros
    They can be used inside oyjlOPTIONTYPE_FUNCTION callbacks to obtain
    all options results.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2528-g3cb19403-2021-06-23"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2529-ge617bae5"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
