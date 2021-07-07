706c88531584934593af1e7e71112b4242666401
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-07-07 10:14:41 +0200

    * [oyjl]: add OyjlArgsCli module
    It renders -X json files on the command line, analog to OyjlArgsQml.
    The module is not interactive. The use is to more easily compare QML
    output.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2539-g698c9176-2021-07-07"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2540-gea02add9-2021-07-07"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
