42f47ff0bce3794722c6bfbefc5801f6172f1073
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-07-07 09:49:34 +0200

    * [oyjl]: docu QML purpose more specifically

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2533-g40a059e4-2021-07-04"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2534-g209da105-2021-07-07"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
