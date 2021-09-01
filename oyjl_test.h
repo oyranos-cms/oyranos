41e90a28557ea72b23d0b35a4cc5d23461ec081f
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-09-01 12:02:41 +0200

    * [oyjl]: show diff as well for XFAIL test results

--- oyjl_test.h
+++ oyjl_test.h
@@ -407,7 +407,7 @@ int  oyjlWriteTestFile               ( const char        * filename,
   sprintf( fn, "%s-%d-%d-%s-%s.%s", OYJL_TEST_NAME, oyjl_test_number, oy_test_current_sub_count, hint?hint:"", oyjlTestResultToString(oy_test_last_result,0), suffix ); \
   oyjlWriteTestFile( fn, mem, size ); \
   if(verbose) fprintf(zout, "%s\n", fn); \
-  if(oy_test_last_result == oyjlTESTRESULT_FAIL) { \
+  if(oy_test_last_result == oyjlTESTRESULT_FAIL || oy_test_last_result == oyjlTESTRESULT_XFAIL) { \
     char * fns = (char*)malloc((hint?strlen(hint):0)+64); \
     sprintf( fns, "%s-%d-%d-%s-%s.%s", OYJL_TEST_NAME, oyjl_test_number, oy_test_current_sub_count, hint?hint:"", oyjlTestResultToString(oyjlTESTRESULT_SUCCESS,0), suffix ); \
     FILE * fp = fopen(fns, "r"); \
