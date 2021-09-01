3232ad068125172b94fbd5c3f2d9cd4f9cce4c45
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-09-01 12:02:41 +0200

    * [oyjl]: show diff as well for XFAIL test results

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2551-gf3de5280-2021-07-14"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2553-gd102c5df-2021-09-01"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
