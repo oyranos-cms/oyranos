0358481230ab0bc1b7821f57390e3f9b7a230cf5
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-10-12 10:39:12 +0200

    * [oyjl]: fix debug warnings in oyjl_macros.h

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2556-g6b01fffb-2021-10-12"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2557-gb40e7796-2021-10-12"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
