b40e7796e34a74517fa700dfdb518adbc9c3dafc
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-10-12 10:36:58 +0200

    * [oyjl]: use oyjlTranslate() in case of missed gettext
    support OYJL_SKIP_TRANSLATE for omitting oyjlTranslate()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2555-g4dbc5b74"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2556-g6b01fffb-2021-10-12"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
