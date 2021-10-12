d26cdd5fa11fb2ef710c6c2385e6d2959d9112b8
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-10-12 10:46:05 +0200

    * [oyjl]: support true/false detection in oyjlParseXML()
    Support plain load of oiJS static Oyjl json.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2558-g03584812-2021-10-12"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2559-gfc7859d4-2021-10-12"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
