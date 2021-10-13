b209b999ba52bc845d20f7a57378ff7c0ccf62e6
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-10-13 18:21:32 +0200

    * [oyjl]: generate liboyjl.i18n.c
    add make check-fast target

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2576-gae302db1-2021-10-13"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2577-gd1a1ad0b-2021-10-13"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
