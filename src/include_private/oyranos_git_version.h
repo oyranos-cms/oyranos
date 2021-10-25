bbccc6d72abab934b677413b776b881228d43a3d
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-10-25 10:31:00 +0200

    * [oyjl]: use oyjlLang() API to initialise oyjl tool

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2598-g469531b1-2021-10-25"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2599-gb7017433-2021-10-25"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
