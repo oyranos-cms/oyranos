c61bd864f0a66cdad39dc79e63ffd0c13fb29651
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-10-25 10:39:18 +0200

    * [oyjl]: fix locale issue while generating docu in cmake

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2600-gbbccc6d7-2021-10-25"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2601-g4e49f9d0-2021-10-25"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
