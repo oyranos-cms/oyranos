d76e5fb4f62216de39fedd78b054422a81a1aa34
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-09 10:56:31 +0100

    * [conf]: fix LANG related warning during docu generation

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2615-g3610591e-2021-11-09"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2616-gd32a2e66-2021-11-09"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
