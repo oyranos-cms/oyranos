3371cb8ef4fb9bd1e325564099db3da0abfd912b
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-11 18:57:20 +0100

    * [openicc]: fix cmake warning about too low version

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2622-g6c7779c7-2021-11-11"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2623-g69496f55-2021-11-11"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
