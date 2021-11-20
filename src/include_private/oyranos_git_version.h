e341b37d35750e646f7aabe73b6ff32effebbb82
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-20 11:26:31 +0100

    * [oyjl]: fix compiling wit helder Qt

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2624-g3371cb8e-2021-11-11"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2625-g704a54e1"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
