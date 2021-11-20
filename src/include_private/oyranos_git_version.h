ee8e9cbfc0912edb0206c929331c75f1e1f7766f
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-20 11:47:14 +0100

    * [oyjl]: support FORCE_NO_COLORTERM in oyjl_test.h

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2630-gde1a4cdb-2021-11-20"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2631-g728402f8-2021-11-20"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
