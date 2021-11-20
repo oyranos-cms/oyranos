7dc1df44e1de25a90321b0b4f0aeabbaf3ce7f0b
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-20 11:51:20 +0100

    * [oyjl]: support formatted synopsis in OyjlArgsCli

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2632-gee8e9cbf-2021-11-20"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2633-gf734f493-2021-11-20"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
