fbd559b01e74a0f5dda4a38911ab9f46d7b23f93
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-21 07:44:31 +0100

    * [oyjl]: compile fix OyjlArgsCli declare inside for()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2640-g856edd77-2021-11-20"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2641-g6c45019e-2021-11-21"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
