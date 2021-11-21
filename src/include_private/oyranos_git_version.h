6c45019ebcf9e466ae39439cf5ea67edbc77eb83
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-21 07:43:15 +0100

    * [oyjl]: fix nullptr use in GCC < 4.6.0

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2639-g3177b3d9-2021-11-20"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2640-g856edd77-2021-11-20"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
