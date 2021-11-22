a88c98a47a4bba18d47dfc3adeb8a68c446ed57c
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-22 09:43:42 +0100

    * [oyjl]: compile openicc code into QML if libopenicc is missing

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2642-gfbd559b0-2021-11-21"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2645-g1532a5f3-2021-11-22"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
