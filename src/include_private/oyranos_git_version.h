cd83cac05437420820fdb65c733f4ee30e654ee6
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-22 10:09:15 +0100

    * [i18n]: update

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2647-gd9d2cde0-2021-11-22"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2648-gbac505d0-2021-11-22"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
