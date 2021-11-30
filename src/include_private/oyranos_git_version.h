3eb6717a7fb0ac28ac9cb0f637606acd85ed8d11
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-11-30 10:45:28 +0100

    * [conf]: fix build issue with non local libOyjl

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2659-g26593dd3-2021-11-30"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2660-ga393062d"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
