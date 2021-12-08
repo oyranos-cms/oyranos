36bc9a947a504e5fccf99ad4cfe98b184f09063e
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-12-08 11:38:47 +0100

    * [oyjl]: support oyjlOption_s::flags|OYJL_NO_OPTIMISE
    ... for oyjlOptionsResultValueCopy_() in oyjlOptions_Parse() and skip
    quotation mark removal

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2664-gb2581da6-2021-12-07"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2665-g79f5f8ed-2021-12-08"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
