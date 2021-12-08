f6d66c155d85557384c6cdccd9aab609e395185c
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-12-08 11:46:31 +0100

    * [oyjl]: fix pkg-config line break glitch for test compilation

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2666-g36bc9a94-2021-12-08"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2667-gc26b6d3b-2021-12-08"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
