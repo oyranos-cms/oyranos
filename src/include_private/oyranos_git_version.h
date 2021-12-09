293e0d6567298e071815fa12d69004b3ee1966a3
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-12-09 17:38:17 +0100

    * [oyjl]: fix writing of '#' args in oyjlUiJsonToCode()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2670-ge6b31196-2021-12-09"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2671-g106040bb-2021-12-09"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
