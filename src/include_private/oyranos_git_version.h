5ce75bd127ec23f2eed0400769984880b554201d
Kai-Uwe Behrmann <ku.b@gmx.de>
2021-12-20 11:02:03 +0100

    * [oyjl]: detect long-args in QML hasArg()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2681-g548cbab42-2021-12-20"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2682-g637d41ae0-2021-12-20"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
