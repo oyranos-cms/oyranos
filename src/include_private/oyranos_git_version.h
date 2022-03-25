d6fe059e27056ba23b59a3a8b2f5ccadc2815c78
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-03-25 07:06:44 +0100

    * [oyjl]: update docu year

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2703-g64623debe"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2704-g7a34c27e6-2022-03-25"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
