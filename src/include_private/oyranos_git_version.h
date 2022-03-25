950f140fb7579a20315690758eb846d054dca0dd
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-03-25 07:37:24 +0100

    * [oyjl]: fix enum interference of oyjlOPTIONSTYLE_XXX
    ... for oyjlOption_PrintArg_() and friends

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2704-g7a34c27e6-2022-03-25"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2706-ge460aa76f-2022-03-25"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
