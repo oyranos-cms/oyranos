f824982b5f2be60d53264b9a3c05bf210f3ac291
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-04-03 14:44:25 +0200

    * [oyjl]: highlight oyjlTree_ToJson() output

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2711-g6918e7ba3-2022-04-03"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2712-gdb5dcb3fb-2022-04-03"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
