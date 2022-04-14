8b20f7edb6e40ea3f09b3462fd55c9adb0044435
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-04-14 08:04:03 +0200

    * [build]: fix bootstrap build of oy-convert

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2716-g09d47b78c-2022-04-07"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2717-g958e37ca6"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
