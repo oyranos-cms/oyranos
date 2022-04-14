b1d20b27313d66ed17964aafa076775ee3b77ee6
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-04-14 08:06:13 +0200

    * [oyjl]: add link symbol for static core lib

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2718-g8b20f7edb-2022-04-14"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2719-gba5ec9749-2022-04-14"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
