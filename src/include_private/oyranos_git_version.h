818b9029e71569bce1dc06098abacbeaab5b3c6b
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-04-14 08:12:41 +0200

    * [conf]: add messages for -- Use internal: libXXX

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2722-gd6d4b7817-2022-04-14"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2723-g9634d0501-2022-04-14"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
