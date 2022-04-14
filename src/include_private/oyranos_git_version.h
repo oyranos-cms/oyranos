d6d4b78174bc0acd0aa79a00a33d2cc0ef580175
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-04-14 08:10:47 +0200

    * [oyjl]: parse PO files inside oy-translate

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2721-g5ef3409d6-2022-04-14"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2721-gc717f7f42-2022-04-14"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
