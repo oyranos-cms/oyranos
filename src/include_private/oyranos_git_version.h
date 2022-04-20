ec55c85a5058698c73e4db5da9265c70ac10a3d3
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-04-20 09:31:31 +0200

    * [generated]: use plain JSON for hash

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2730-g16cab5239-2022-04-20"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2731-g9379b8e60-2022-04-20"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
