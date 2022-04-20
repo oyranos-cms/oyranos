0b5aa616981ee0a324dc287a5e089c04fb1af012
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-04-20 09:33:42 +0200

    * [build]: add test-2-fast target
    ... for faster checking during development

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2731-g9379b8e60-2022-04-20"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2732-gec55c85a5-2022-04-20"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
