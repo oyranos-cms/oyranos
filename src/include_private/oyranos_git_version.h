82ee2a8d4fb7e31c94204ec03454807b3043bf6d
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-05-04 08:25:58 +0200

    * [oyjl]: change OYJL_XML define
    ... to not interference with oyjlUI_STATE_VERBOSE

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2751-g73d6ed81b-2022-05-04"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2752-g0f7f366f7-2022-05-04"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
