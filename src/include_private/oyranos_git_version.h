1116622f5aaa3540f311557142bef10a31bb9818
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-05-07 09:24:50 +0200

    * [oyjl]: add warning for -h wrong variable_type and oyjlOPTIONTYPE_CHOICE
    Then -h=- without oyjlSTRING would not work as expected.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2756-gcd00fad61-2022-05-06"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2757-g2e082e4cc-2022-05-06"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
