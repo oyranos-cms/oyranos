9585669b8e16aca06fcb0de350e7ab58b1a20a17
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-05-07 09:26:36 +0200

    * [tools]: fix oy-monitor-white-point -h variable_type

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2757-g2e082e4cc-2022-05-06"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2758-g1116622f5-2022-05-07"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
