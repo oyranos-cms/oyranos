f8abf46fdacc4391548fecb35bdb64bd1d36a695
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-05-20 07:12:56 +0200

    * [tools]: fix option value type in oy-m-w-pt

--- src/tools/oyranos-monitor-white-point.c
+++ src/tools/oyranos-monitor-white-point.c
@@ -466,7 +466,7 @@ int myMain( int argc , const char** argv )
          set = 0.0;
   int daemon = -1;
   int check = 0;
-  int help = 0;
+  const char * help = NULL;
   int verbose = 0;
   int version = 0;
   int state = 0;
@@ -537,7 +537,7 @@ int myMain( int argc , const char** argv )
     {"oiwi", 0, "X", "export", NULL, NULL, NULL, NULL, NULL, oyjlOPTIONTYPE_CHOICE, {.choices.list = NULL}, oyjlSTRING, {.s=&export} },
     /* The --render option can be hidden and used only internally. */
     {"oiwi", OYJL_OPTION_FLAG_EDITABLE, "R", "render", NULL, NULL,  NULL,  NULL, NULL, oyjlOPTIONTYPE_CHOICE, {0}, oyjlSTRING, {.s=&render} },
-    {"oiwi", OYJL_OPTION_FLAG_ACCEPT_NO_ARG, "h", "help",NULL,NULL,NULL,NULL, NULL, oyjlOPTIONTYPE_CHOICE, {0}, oyjlSTRING, {.i=&help} },
+    {"oiwi", OYJL_OPTION_FLAG_ACCEPT_NO_ARG, "h", "help",NULL,NULL,NULL,NULL, NULL, oyjlOPTIONTYPE_CHOICE, {0}, oyjlSTRING, {.s=&help} },
     {"oiwi", 0, NULL,"synopsis",NULL, NULL,         NULL,         NULL, NULL, oyjlOPTIONTYPE_NONE, {0}, oyjlNONE, {0} },
     {"oiwi", 0, "v", "verbose", NULL, _("verbose"), _("verbose"), NULL, NULL, oyjlOPTIONTYPE_NONE, {0}, oyjlINT, {.i=&verbose} },
     {"oiwi", 0, "V", "version", NULL, _("version"), _("Version"), NULL, NULL, oyjlOPTIONTYPE_NONE, {0}, oyjlINT, {.i=&version} },
