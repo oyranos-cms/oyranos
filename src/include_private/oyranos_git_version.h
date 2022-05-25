de6db81ae90b2a4b6a92f4ace261f7b96b0b8217
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-05-25 10:00:48 +0200

    * [oyjl]: adapt json/xml/yaml highlight in QML to oyjlCore

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2778-g99265332a-2022-05-24"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2779-g884036b22-2022-05-25"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
