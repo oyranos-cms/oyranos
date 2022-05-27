d90fa336ce625cbc1f33906688a1d5829a30da2b
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-05-27 15:34:13 +0200

    * [conf]: update OYRANOS_DEVEL_YEAR macro

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2779-g884036b22-2022-05-25"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2781-g2e04afebd-2022-05-27"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
