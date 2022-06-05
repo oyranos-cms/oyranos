caad5c363b14c0001dbfd0c4fbec738f206b1868
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-06-05 11:00:45 +0200

    * [oyjl]: shorten test case
    The LOCPATH variable affect only test case 1.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2795-g14795aaf5-2022-06-02"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2797-gebbdadd6a-2022-06-05"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
