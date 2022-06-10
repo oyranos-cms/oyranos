5aa32df19deea4d38ae4af310d3933ca66b2d201
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-06-10 11:42:09 +0200

    * [oyjl]: fix conversion of synopsis to HTML in QML

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2807-g56868aff0-2022-06-07"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2808-g78a2cb64b-2022-06-09"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
