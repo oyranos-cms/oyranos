522142aa2e0b4993114a3370d2996ec30273e8b2
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-06-18 11:01:20 +0200

    * [oyjl]: add oyjlTB() line to failed cases in oyjl_test.h

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2824-ge935f5310"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2825-ge620afe95-2022-06-17"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
