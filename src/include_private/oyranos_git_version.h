fe184f1575e9f0502e843731df7cbdbae51df9e3
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-07-07 20:23:38 +0200

    * [oyjl]: fix OYJL_CREATE_VA_STRING macro variable

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2827-gdbbef727d"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2828-gb87539919-2022-06-21"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
