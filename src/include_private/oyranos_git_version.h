74ebda5987c50e1f4637980fd411dca5e6518fac
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-07-08 20:06:45 +0200

    * [oyjl]: fix doxy icon

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2829-gfe184f157-2022-07-07"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2830-g3a03a8e69-2022-07-07"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
