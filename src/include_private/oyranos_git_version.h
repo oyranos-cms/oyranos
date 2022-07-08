35dcd2b39f63c973285c609a357f88867c6213df
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-07-08 21:03:28 +0200

    * [oyjl]: fix test for i18n

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2835-g974b4fc11-2022-07-08"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2836-g2004dbd9e-2022-07-08"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
