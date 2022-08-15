b0b7c4a22fd47a724f8f83bf93f0fd423b6a3d36
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-08-15 19:10:19 +0200

    * [oyjl]: oyjl_test.h resolve conflict with OyjlCore oyjlSeconds()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2850-g601456bc2-2022-08-15"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2851-gb65e3c1b4-2022-08-15"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
