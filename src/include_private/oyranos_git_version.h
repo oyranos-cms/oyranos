7c6d657ba09faf668be3cfe997d4164cb80983a6
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-09-14 13:12:23 +0200

    * [oyjl]: suggest cmake in main docu, configure is untested
    update for openSUSE Leap 15.4 package names

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2867-gaa358de30-2022-09-14"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2868-g84e0f6223-2022-09-14"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
