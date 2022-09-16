1ae70f01fd28dc33909f4cf808944e1c062893a2
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-09-16 19:35:21 +0200

    * [oyjl]: add OYJL_E macro for non empty strings
    ... in e.g. printf(%s) format arguments.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2869-g7c6d657ba-2022-09-14"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2870-g062d21c39-2022-09-14"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
