52a9b4ebd78f1842d3e58b5dc045384917df3066
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-10-03 09:09:45 +0200

    * [oyjl]: clean OYJL_E definition
    ... and use the OYJL_E version from oyjl_macros.h

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2881-g17c133ae6-2022-10-03"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2882-g22a61908f-2022-10-03"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
