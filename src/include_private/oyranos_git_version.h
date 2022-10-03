85f2b719db6de7873de971ddd23a2d5f26299e47
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-10-03 14:54:40 +0200

    * [oyjl]: support -R=web:help

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2885-g8e12c8b5c-2022-10-03"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2886-gfdecaad74-2022-10-03"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
