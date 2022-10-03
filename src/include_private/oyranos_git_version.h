8e12c8b5c99bab331387a82b954665b64c3edbe3
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-10-03 12:52:37 +0200

    * [oyjl]: implement -R=web:security=lazy by oyjlReadFunction()
    Show missing group elements from mandatory options set.
    
    Render type="double" options as form::input::range.
    
    Clean more leaked memory.
    
    Pass various variables to MHD_start_daemon().
    
    The static oyjlArgsRender() part is not much checked, as
    the version in liboyjl-args-qml-static is primarily developed.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2883-g52a9b4ebd-2022-10-03"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2884-g4f8fb6204-2022-10-03"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
