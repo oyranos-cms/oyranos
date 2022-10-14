78588fd0f7ba80114b48ca778085188ab4351032
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-10-14 17:06:01 +0200

    * [oyjl]: show range error for double args

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2886-gfdecaad74-2022-10-03"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2887-g85f2b719d-2022-10-03"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
