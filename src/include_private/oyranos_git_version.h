465400e0291f8d0c3ac008768c0804628e62a62f
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-10-14 17:23:25 +0200

    * [oyjl]: add oyjlStringListAdd(...) API

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2887-g85f2b719d-2022-10-03"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2888-g78588fd0f-2022-10-14"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
