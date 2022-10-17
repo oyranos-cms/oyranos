857a67c8597d93d7fe6b6b8f4db12f7c986dbdc1
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-10-17 15:08:18 +0200

    * [oyjl]: support -R=web:check,no_above,allow_/usr/share
    implement JS alike oyjlStringStartsWith()
    
    implement oyjlStringListFind() with different styles of matching,
    default: strict by strcmp(), OYJL_CASE_COMPARE and OYJL_LAZY for
    strstr(). A fuzzy option for missing letters like in web searches
    would be nice too.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2898-g0a9e4c253-2022-10-15"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2899-gaa97ca840-2022-10-17"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
