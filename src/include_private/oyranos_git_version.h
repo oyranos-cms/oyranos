339c23d0ce7d32da903085e0c30bb7b323f7032b
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-10-24 09:44:42 +0200

    * [oyjl]: change oyjlRegExpFind() API

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2905-gd6d7d4430-2022-10-17"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2906-gc5c67ef0a-2022-10-24"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
