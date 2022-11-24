62b0c24401e0022b7c1abf557456af851c33fe06
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-11-24 10:12:19 +0100

    * [oyjl]: oyjlExtraManSection_() bold first "SEE AS WELL" entry

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2913-gc82f8304a-2022-10-27"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2915-gaa4d25809-2022-11-24"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
