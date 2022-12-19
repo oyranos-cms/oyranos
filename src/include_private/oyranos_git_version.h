97d4e581850f7750ebfd7582176d520db95555a2
Kai-Uwe Behrmann <ku.b@gmx.de>
2022-12-19 10:54:59 +0100

    * [oyjl]: guard libmicrohttpd linking in QML

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2924-gbeaf5056f-2022-12-17"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2925-geecfd62ec-2022-12-17"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
