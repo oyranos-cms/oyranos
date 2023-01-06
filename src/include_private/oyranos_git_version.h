9a0a018d1ecb6910abaf17b7ac8db1e45787ec3d
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-01-06 09:04:27 +0100

    * [oyjl]: fix oyjlStr_AppendN()/oyjlStr_Push() with empty

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2929-g64dc140e2-2023-01-01"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2930-g5125f8f82-2023-01-01"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
