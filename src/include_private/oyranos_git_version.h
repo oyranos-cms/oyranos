8b8f627c34cdd48ef438cea61dc469e176b5ef59
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-01-09 08:28:37 +0100

    * [oyjl]: fix realloc in oyjlPrintSubProfiling()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2930-g5125f8f82-2023-01-01"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2931-g9a0a018d1-2023-01-06"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
