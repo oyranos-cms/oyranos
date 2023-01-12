ba56e9ccb6d7ad46783247dfb19129633f6916fe
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-01-12 17:17:43 +0100

    * [oyjl]: add oyjlStringColor() API

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2934-g6c94a3a29-2023-01-09"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2935-g7c3262225"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
