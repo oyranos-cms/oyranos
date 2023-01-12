76f69ab996fafc94b6aa4ae019d37f5e85709977
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-01-12 17:25:50 +0100

    * [oyjl]: remove local instance and use public API

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2935-g7c3262225"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2936-gba56e9ccb-2023-01-12"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
