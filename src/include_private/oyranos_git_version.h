9ba80eb826e8061767ca75cad0ac973d44b9352b
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-01-18 14:13:56 +0100

    * [oyjl]: fix message string release

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2949-gc599b2853-2023-01-17"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2950-g2ef67b99f-2023-01-17"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
