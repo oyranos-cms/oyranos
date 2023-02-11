670e0efbd0bea89674fdd3c23e71eb9de8a5f562
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-02-11 13:23:45 +0100

    * [oyjl]: compile oyjlArgsRender() for static library
    ... in order to load inbuild versions of OyjlArgsXxx by liboyjl-static.

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2950-g2ef67b99f-2023-01-17"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2951-g9ba80eb82-2023-02-11"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
