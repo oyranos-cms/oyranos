cd78ba682b79b59d1873b8189c84052da1189a5f
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-02-11 13:30:49 +0100

    * [oyjl]: build OyjlArgsXxx modules for sub builds
    detect libmicrohttps especially carefull
    
    use bootstraped tools to run the sub build process

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2951-g9ba80eb82-2023-02-11"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2952-g670e0efbd-2023-02-11"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
