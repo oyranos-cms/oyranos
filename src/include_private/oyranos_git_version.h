9b0df5d75695754f02a264a281f001f8d356c5df
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-02-11 13:32:57 +0100

    * [oyjl]: fix config path for sub build of OyjlArgsQml

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2952-g670e0efbd-2023-02-11"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2953-gcd78ba682-2023-02-11"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
