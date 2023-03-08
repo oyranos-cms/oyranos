a24993976cc0a76de3e1ab3830eec1182df05a68
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-03-08 08:12:59 +0100

    * [oyjl]: reactivate mini-app-example -R option

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2957-gf406c35db-2023-02-13"  /**< git describe --always HEAD */
+#define OY_GIT_VERSION                 "0.9.6-2958-g858258758"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
