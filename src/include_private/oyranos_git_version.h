97ecfcc41b0776127b71d58005fc5bb3df895f02
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-03-08 08:30:31 +0100

    * [oyjl]: optimise oyjlTreeToCsv()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2959-ga24993976-2023-03-08"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2960-g875955470-2023-03-08"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
