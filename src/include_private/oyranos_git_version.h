4477e00836388f609b83f105947c161612c0ddb8
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-03-21 09:01:53 +0100

    * [oyjl]: load all remaining functions from libOyjlCore.so
    demangle C++ function names in oyjlBT()

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2961-g97ecfcc41-2023-03-08"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2962-g4985e548b"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
