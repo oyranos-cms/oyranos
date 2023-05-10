3669307479a955327b78c08a81f67c6694c742c6
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-05-10 19:48:20 +0200

    * [oyjl]: parse markup JSON in OyjlArgsQml

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2967-gfd2f67ed6-2023-03-21"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2968-g12a98e98a-2023-03-26"  /**< git describe --always HEAD  + date */ 
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
