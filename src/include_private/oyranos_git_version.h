3cd02be8958b4ec262d2ee64a872a2402c9035cc
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-05-20 21:15:29 +0200

    * [build]: install oyjl gmo files
    ... in case of internal libOyjl build

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-2977-g782f9dceb-2023-05-18"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-2978-g69e628c11-2023-05-19"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
