ea0bca5878c7c4cfc420c3975289fdccde6a2100
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-05-23 12:46:00 +0200

    * [tools]: inj́ect defaults group properties

--- oyjl_git_version.h
+++ oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "2984-g635c15474-2023-05-23"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "2985-g71ed4e326-2023-05-23"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
