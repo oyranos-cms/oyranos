c0eec8540dc226a49abc751da1d1cc7ca63c6431
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-11-29 16:25:21 +0100

    * [oyjl]: accept --render only option
    Check -R for option string matching "render" and search otherwise for a
    --render option to pass to oyjlArgsRender() API.

--- openicc/oyjl/oyjl_git_version.h
+++ openicc/oyjl/oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3002-g0137d8c38-2023-11-28"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3003-g9283973e9-2023-11-28"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
