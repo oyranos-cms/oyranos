771aebdc35d2da0511ba20f17fac9b723059bb28
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-12-29 20:00:28 +0100

    * [oyjl]: be more relaxed on missing args for oyjlInitLanguageDebug()

--- openicc/oyjl/oyjl_git_version.h
+++ openicc/oyjl/oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3010-gfb52d7fd7-2023-12-29"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3011-gb545226a4-2023-12-29"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
