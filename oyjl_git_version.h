b9d70b67c0e8007c436726ea63fd17d35b7282ff
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-12-29 19:58:48 +0100

    * [oyjl]: support oyjlStringStartsWith(OYJL_COMPARE_CASE)
    Its a API change.

--- oyjl_git_version.h
+++ oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3009-g78006275d-2023-12-29"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3010-gfb52d7fd7-2023-12-29"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
