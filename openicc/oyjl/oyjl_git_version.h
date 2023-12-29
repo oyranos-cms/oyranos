78006275df94fe181af4e5dab1a021f73bf3f5e5
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-12-29 19:54:00 +0100

    * [oyjl]: rename oyjlUiJsonToCode to oyjlUiExportToCode
    add oyjlUiJsonSetOption() API for resolving options in renderers

--- openicc/oyjl/oyjl_git_version.h
+++ openicc/oyjl/oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3007-g4274ba0cd-2023-12-23"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3008-gaa267463a-2023-12-23"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
