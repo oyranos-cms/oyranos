b7ab735edcc4c9ecfb9c589748087548d1a359eb
Kai-Uwe Behrmann <ku.b@gmx.de>
2023-12-31 07:40:43 +0100

    * [oyjl]: improve gui behaviour of oyjl tool

--- oyjl_git_version.h
+++ oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3012-g771aebdc3-2023-12-29"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3013-gd1b75c46c-2023-12-29"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
