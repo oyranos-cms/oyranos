5c4d4e84052f0a652a1564801ee1bc23d805b358
Kai-Uwe Behrmann <ku.b@gmx.de>
2024-01-30 21:10:37 +0100

    * [conf]: detect libraw and Exiv2 versions

--- src/include_private/oyranos_git_version.h
+++ src/include_private/oyranos_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYRANOS_GIT_VERSION_H
 #define OYRANOS_GIT_VERSION_H
 
-#define OY_GIT_VERSION                 "0.9.6-3039-g924119c8d-2024-01-30"  /**< git describe --always HEAD  + date */ 
+#define OY_GIT_VERSION                 "0.9.6-3040-g2449438c3"  /**< git describe --always HEAD */
 
 
 #endif /*OYRANOS_GIT_VERSION_H*/
