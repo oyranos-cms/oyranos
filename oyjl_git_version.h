5c7360b5a9fb097df21636ebd1328f3ca50478b9
Kai-Uwe Behrmann <ku.b@gmx.de>
2024-01-31 21:40:03 +0100

    * [oyjl]: support TERM=linux oyjlTermColorInit()

--- oyjl_git_version.h
+++ oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3041-g5c4d4e840-2024-01-30"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3042-gfcc9a544e-2024-01-31"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
