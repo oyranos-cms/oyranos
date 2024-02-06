d4591e4add6e131887b87f764798d3e8d0995a13
Kai-Uwe Behrmann <ku.b@gmx.de>
2024-02-06 07:54:24 +0100

    * [oyjl]: link OyjlArgsWeb with oyjl-extra-static

--- oyjl_git_version.h
+++ oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3042-gfcc9a544e-2024-01-31"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3046-g5b56dec4a-2024-01-31"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
