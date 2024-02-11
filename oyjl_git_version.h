9e23efb43a10968e449c592677217786bdeac28c
Kai-Uwe Behrmann <ku.b@gmx.de>
2024-02-11 12:15:16 +0100

    * [oyjl]: add detected start=instant to setCommands in libOyjlArgsQml
    it is a pass through

--- oyjl_git_version.h
+++ oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3050-g8b70694b4-2024-02-11"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3051-gd971c3191-2024-02-11"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
