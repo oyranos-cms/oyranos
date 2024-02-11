f8a4ba2719d4c568c8ed9a0d50a502164a752fcb
Kai-Uwe Behrmann <ku.b@gmx.de>
2024-02-11 13:09:33 +0100

    * [oyjl]: use interactiveCallback("oyjl-history") for -R=qml:start=instant
    The "oyjl-history" key is already used by the history feature.

--- oyjl_git_version.h
+++ oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3054-gb2281a674-2024-02-11"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3055-g98677702b-2024-02-11"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
