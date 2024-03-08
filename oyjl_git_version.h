1dcc5cc7d526c4371fbe28a3d5fb9d467903e702
Kai-Uwe Behrmann <ku.b@gmx.de>
2024-03-08 09:57:07 +0100

    * [oyjl]: use oyjlTreeSortStrings() for oyjl-translate -a

--- oyjl_git_version.h
+++ oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "3073-g7a5eafe43-2024-03-06"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "3074-g2a14bff2f-2024-03-08"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
