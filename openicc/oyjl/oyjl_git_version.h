a24cb60e4e3265d8ee9d5868d9e0ce5b584101ac
Kai-Uwe Behrmann <ku.b@gmx.de>
2024-05-05 11:08:56 +0200

    * [oyjl]: remove libOpenIcc dependency
    Originally the OpenICC DB night property was used to switch the color
    sheme. oyranos-night-manager has now a color sheme selection included,
    which does better and for the whole desktop.

--- openicc/oyjl/oyjl_git_version.h
+++ openicc/oyjl/oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "4174-g0243c8adb-2024-05-04"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "4175-g91981bc84-2024-05-05"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
