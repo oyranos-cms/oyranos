fb4c603c95124c4fb0b9eae08906d430b545cc90
Kai-Uwe Behrmann <ku.b@gmx.de>
2024-05-07 11:36:44 +0200

    * [oyjl]: sync OyjlTest color term detection with OyjlArgs one

--- openicc/oyjl/oyjl_git_version.h
+++ openicc/oyjl/oyjl_git_version.h
@@ -4,7 +4,7 @@
 #ifndef OYJL_GIT_VERSION_H
 #define OYJL_GIT_VERSION_H
 
-#define OYJL_GIT_VERSION               "4179-gd246f8676-2024-05-06"  /**< git describe --always HEAD  + date */ 
+#define OYJL_GIT_VERSION               "4180-g5792c746d-2024-05-06"  /**< git describe --always HEAD  + date */ 
 
 #endif /*OYJL_GIT_VERSION_H*/
 
